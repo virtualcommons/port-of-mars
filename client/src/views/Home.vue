dc7bdade71db30e534187e84814f61fb98f1fe54
chrstngyn <Christine.N.Nguyen@asu.edu>
2019-10-02 16:31:24 -0700

    remove extraneous files and associated code routing is not necessary for now

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,29 +1,21 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
-import { Socket } from 'vue-socket.io-extended';
+    import { Component, Vue } from 'vue-property-decorator';
+    import { Socket } from 'vue-socket.io-extended';
 
-@Component({
-  components: {
-    HelloWorld,
-  }
-})
-export default class Home extends Vue {
-  @Socket('joinGame')
-  onJoinGame(data: unknown) {
-    console.log(data);
-  }
-
-  mounted() {
-    console.log((this as any).$socket);
-    (this as any).$socket.client.emit('joinGame', { my: 'data' }); 
-  }
-}
+    @Component
+    export default class Home extends Vue {
+        @Socket('joinGame')
+        onJoinGame(data: unknown) {
+            console.log(data);
+        }
+        mounted() {
+            console.log((this as any).$socket);
+            (this as any).$socket.client.emit('joinGame', { my: 'data' });
+        }
+    }
 </script>
