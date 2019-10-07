11660d776126dd4f7894731992522d8a39142fcb
chrstngyn <Christine.N.Nguyen@asu.edu>
2019-10-07 15:20:10 -0700

    node version bumps + added eslint

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import VueSocketIOExt from 'vue-socket.io-extended';
-import App from './App.vue';
 import io from 'socket.io-client';
+import App from './App.vue';
 import router from './router';
 import store from './store';
 
