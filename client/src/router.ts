21159c9a6ccc99a1a9031b0b5e7e84f730a23f52
chrstngyn <Christine.N.Nguyen@asu.edu>
2019-10-15 13:10:23 -0700

    removed extraneous code and files

--- client/src/router.ts
+++ client/src/router.ts
@@ -1,16 +1,16 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from './views/Home.vue';
+// import Home from './views/Home.vue';
 
 Vue.use(Router);
 
 export default new Router({
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home,
-    },
+    // {
+    //   path: '/',
+    //   name: 'home',
+    //   component: Home,
+    // },
     // {
     //   path: '/about',
     //   name: 'about',
