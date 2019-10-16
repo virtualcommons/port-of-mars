e589917acfface95e7c744f8d6f62d206b866c63
Nick Moran <nick42791@gmail.com>
2019-10-16 13:08:57 -0700

    [refactor] Moved Clouds and background to LoginPage component from App.

--- client/src/views/LoginPage.vue
+++ client/src/views/LoginPage.vue
@@ -1,5 +1,10 @@
 <template>
-    <LoginScreen />
+    <div>
+        <div class="stars"></div>
+        <div class="twinkling"></div>
+        <div class="clouds"></div>
+        <LoginScreen />
+    </div>
 </template>
 
 <script lang="ts">
@@ -14,3 +19,68 @@
 
     export default class LoginPage extends Vue {};
 </script>
+
+<style>
+  /* Background - Copyright (c) 2019 by WebSonick (https://codepen.io/WebSonick/pen/vjmgu) */
+  * {
+    margin: 0;
+    padding: 0;
+  }
+
+  @keyframes move-twink-back {
+    from { background-position: 0 0; }
+    to { background-position: -10000px 5000px; }
+  }
+
+  @keyframes move-clouds-back {
+    from { background-position: 0 0; }
+    to { background-position: -10000px 5000px; }
+  }
+
+  .stars, .twinkling, .clouds {
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+  }
+
+  .stars {
+    background:#000000 url(http://www.script-tutorials.com/demos/360/images/stars.png) repeat top center;
+    z-index: -2;
+  }
+
+  .twinkling {
+    background: transparent url(http://www.script-tutorials.com/demos/360/images/twinkling.png) repeat top center;
+    z-index: -1;
+    animation: move-twink-back 500s linear infinite;
+  }
+
+  .clouds {
+    background: transparent url(http://www.script-tutorials.com/demos/360/images/clouds3.png) repeat top center;
+    z-index: 0;
+    animation: move-clouds-back 500s linear infinite;
+
+  }
+
+  /* ==============================================================================================
+    End Background - Copyright (c) 2019 by WebSonick (https://codepen.io/WebSonick/pen/vjmgu)
+================================================================================================= */
+
+  /*html {*/
+  /*  font-size: 16px;*/
+  /*}*/
+
+  html, body, .game {
+    height: 100%;
+  }
+
+  body {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  }
+
+  .game {
+    display: flex;
+    flex-flow: row nowrap;
+  }
+</style>
