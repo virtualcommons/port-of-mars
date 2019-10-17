041279163df1e815139b3ceb0fa79adc21c13d65
chrstngyn <Christine.N.Nguyen@asu.edu>
2019-10-17 13:04:55 -0700

    Refactor: dynamic multi-layout system
    create folder src/layouts
    use Vuex to manage state of application
    use VueRouter to display router view on app
    See: https://levelup.gitconnected.com/multiple-layouts-for-vue-spa-app-fafda6b2bfc7

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -5,10 +5,17 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-
+    layout: 'primary-layout',
   },
   mutations: {
-
+    SET_LAYOUT(state, payload) {
+      state.layout = payload;
+    },
+  },
+  getters: {
+    layout(state) {
+      return state.layout;
+    }
   },
   actions: {
 
