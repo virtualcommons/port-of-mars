c0a88104e0163d8cb9a463d17199505d80dd74c1
chrstngyn <Christine.N.Nguyen@asu.edu>
2019-10-28 09:41:48 -0700

    feat: create dynamic layout components
    rename PrimaryLayout --> DefaultLayout
    added TutorialLayout
    registered layout components at entry point of app
    add meta keys for Login and GameDashboard pages
    create computed property to change layout dynamically based on route

--- client/src/main.ts
+++ client/src/main.ts
@@ -6,10 +6,20 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 
+// imports: layouts
+import DefaultLayout from '@/layouts/DefaultLayout.vue';
+import TutorialLayout from '@/layouts/TutorialLayout.vue';
+
 // const socket = io('http://localhost:3005');
 // Vue.use(VueSocketIOExt, socket);
 
 Vue.use(Vuex);
+
+Vue.config.productionTip = false;
+
+// register layouts globally at entry point of app
+Vue.component('default-layout', DefaultLayout);
+Vue.component('tutorial-layout', TutorialLayout);
 
 Vue.config.productionTip = false;
 
