d41c206d3cb91bc5da6dcb3159d0a9f63405deeb
chrstngyn <Christine.N.Nguyen@asu.edu>
2019-10-29 15:13:40 -0700

    refactor: remove VueTour imports from App entry point
    move VueTour imports to TutorialLayout

--- client/src/main.ts
+++ client/src/main.ts
@@ -2,27 +2,14 @@ import Vue from 'vue';
 import VueSocketIOExt from 'vue-socket.io-extended';
 // import io from 'socket.io-client';
 import Vuex from 'vuex';
-import VueTour from 'vue-tour';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
-// imports: layouts
-import DefaultLayout from '@/layouts/DefaultLayout.vue';
-import TutorialLayout from '@/layouts/TutorialLayout.vue';
-
 // const socket = io('http://localhost:3005');
 // Vue.use(VueSocketIOExt, socket);
 
-require('vue-tour/dist/vue-tour.css');
-
-Vue.use(Vuex, VueTour);
-
-Vue.config.productionTip = false;
-
-// register layouts globally at entry point of app
-Vue.component('default-layout', DefaultLayout);
-Vue.component('tutorial-layout', TutorialLayout);
+Vue.use(Vuex);
 
 Vue.config.productionTip = false;
 
