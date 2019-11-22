1f55a77992fd7ba91f107427712faf797ae3228f
Nick Moran <nick42791@gmail.com>
2019-11-22 11:36:32 -0700

    [feat] status bar colors, player score works, investments change color

---
+++ client/src/store/mutations.ts
@@ -0,0 +1,116 @@
+import { ChatMessageData } from 'shared/types';
+import { BaseInvestmentCosts, GetAccomplishmentsByPerson, buyAccomplishment } from "@/models";
+import * as _ from 'lodash';
+
+export default {
+  SET_ACCS(state: any, payload: any) {
+    // payload is an array of numbers
+    // for all the numbers
+    // activecards.push(Data(number))
+  },
+  /**
+   * SET_LAYOUT() mutation
+   * Changes the state of the layout state.
+   * @param state The state of the application.
+   * @param payload The string value of layout.
+   *
+   */
+
+  // game updates
+  SET_GAME_PHASE(state: any, payload: string) {
+    state.gamePhase = payload;
+    if (payload === 'Upkeep') {
+      state.round += 1;
+    }
+    if (payload === 'Trading') {
+      state.localInvestments.confirmInvestments();
+    }
+  },
+  SET_EVENTS_FOR_ROUND(state: any, payload: any) {
+    state.gameEvents = payload;
+    // console.log(state.gameEvents);
+  },
+  CHANGE_UPKEEP_AMOUNT(state: any, payload: number) {
+    if (state.upkeep - payload >= 0) {
+      state.upkeep -= payload;
+    }
+  },
+  ADD_TO_CHAT(state: any, payload: ChatMessageData) {
+    state.chat.push(payload);
+  },
+  CHANGE_LOCAL_INVESTMENT(state: any, payload: any) {
+    // this is for increment and decrement
+
+    state.localInvestments.changeInventoryValue(payload.investmentName, payload.investmentAmount);
+  },
+  SET_PLAYER_ROLE(state: any, payload: string) {
+    state.playerRole = payload;
+    
+    Object.keys(BaseInvestmentCosts[payload]).forEach((key) => {
+      state.localInvestments.updateCurrentCost(key, BaseInvestmentCosts[payload][key]);
+    });
+
+  },
+  SET_PLAYER_FINISHED(state: any, payload: boolean) {
+    state.playerFinishedWithPhase = payload;
+  },
+
+  // accomplishments
+  SET_ACTIVE_ACCOMPLISHMENTS(state: any, payload: any) {
+    //state.activeAccomplishmentCards = payload;
+    state.activeAccomplishmentCards = GetAccomplishmentsByPerson(state.playerRole,payload);
+  },
+  PURCHASE_ACCOMPLISHMENT(state:any, payload:any){
+    let bought = state.localInvestments.canPurchaseAccomplishment(payload.totalCostArray,false);
+    if(bought){
+      buyAccomplishment(payload.label);
+      state.boughtAccomplishmentCards.push(payload);
+
+      state.playerScore += payload.victoryPoints;
+      
+      let index = _.findIndex(state.activeAccomplishmentCards, (e) => e.label == payload.label);
+      state.activeAccomplishmentCards.splice(index,1,GetAccomplishmentsByPerson(state.playerRole,1)[0]);
+    }
+  },
+  /**
+   * SET_LAYOUT() mutation
+   * Changes the state of the layout state.
+   * @param state The state of the application.
+   * @param newLayout The string value of layout.
+   *
+   */
+  SET_LAYOUT(state: any, newLayout: string) {
+    state.layout = newLayout;
+  },
+
+  // trading
+  SET_TRADING_VIEW(state: any, newTradingView: string) {
+    state.tradingView = newTradingView;
+  },
+  SET_TRADING_MEMBER(state: any, newTradingMember: string) {
+    state.tradingMember = newTradingMember;
+  },
+
+  // notifications
+  SET_NOTIFICATION_MESSAGE(state: any, payload: string) {
+    state.notifMessage = payload;
+  },
+  SET_NOTIFICATION_STATUS(state: any, payload: string) {
+    state.notifIsActive = payload;
+  },
+  SET_TIME_REMAINING(state: any, payload: number) {
+    state.timeRemaining = payload;
+  },
+  ADD_TO_MARS_LOG(state: any, payload: string) {
+    // correct location for this?
+    // if (payload !== '') {
+    //   state.marsLog.push(payload);
+    // }
+    state.marsLog.addEntry(payload);
+  },
+  // CHANGE_LOCAL_ROUND_COSTS(state: any, payload: any) {
+  //   Object.keys(payload).forEach(key => {
+  //     state.localInvestments.updateCurrentCost(key, payload[key]);
+  //   });
+  // }
+};
