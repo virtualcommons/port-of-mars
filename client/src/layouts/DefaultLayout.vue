a84e6a13ddc09fd6f6c7510fb500a59b54a769b3
chrstngyn <Christine.N.Nguyen@asu.edu>
2019-11-22 16:10:06 -0700

    refactor: move animated space background to DefaultLayout
    remove animated space background from LoginPage
    add conditional statements to render background if route path != /game
    fix style of gamedashboard to fill viewport in DefaultLayout

--- client/src/layouts/DefaultLayout.vue
+++ client/src/layouts/DefaultLayout.vue
@@ -1,5 +1,10 @@
 <template>
+  <div class="default-layout">
+    <div class="stars" v-if="$route.path !== '/game'"></div>
+    <div class="twinkling" v-if="$route.path !== '/game'"></div>
+    <div class="clouds" v-if="$route.path !== '/game'"></div>
     <router-view />
+  </div>
 </template>
 
 <script lang="ts">
@@ -12,3 +17,60 @@ import { Vue, Component } from 'vue-property-decorator';
 export default class DefaultLayout extends Vue {
 }
 </script>
+
+<style>
+
+.default-layout {
+  background-color: var(--space-gray);
+}
+
+/* Background - Copyright (c) 2019 by WebSonick (https://codepen.io/WebSonick/pen/vjmgu) */
+@keyframes move-twink-back {
+  from {
+    background-position: 0 0;
+  }
+  to {
+    background-position: -10000px 5000px;
+  }
+}
+
+@keyframes move-clouds-back {
+  from {
+    background-position: 0 0;
+  }
+  to {
+    background-position: -10000px 5000px;
+  }
+}
+
+.stars,
+.twinkling,
+.clouds {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+}
+
+.stars {
+  z-index: 0;
+  background: #000 url('../assets/marsstars.png') repeat top center;
+}
+
+.twinkling {
+  z-index: 0;
+  background: transparent url('../assets/marstwinkling.png') repeat top center;
+  animation: move-twink-back 500s linear infinite;
+}
+
+.clouds {
+  z-index: 0;
+  background: transparent url('../assets/marsclouds.png') repeat top center;
+  animation: move-clouds-back 500s linear infinite;
+}
+
+/* ==============================================================================================
+    End Background - Copyright (c) 2019 by WebSonick (https://codepen.io/WebSonick/pen/vjmgu)
+================================================================================================= */
+</style>
--- client/src/views/LoginPage.vue
+++ client/src/views/LoginPage.vue
@@ -1,8 +1,5 @@
 <template>
   <div class="loginpage">
-    <div class="stars"></div>
-    <div class="twinkling"></div>
-    <div class="clouds"></div>
     <div class="login-screen">
       <h1>Port of Mars</h1>
       <LoginButton />
@@ -29,7 +26,6 @@ export default class LoginPage extends Vue {}
   width: 100vw;
   display: flex;
   align-items: center;
-  background-color: pink;
 }
 .login-screen {
   position: relative;
@@ -49,53 +45,4 @@ export default class LoginPage extends Vue {}
   font-weight: 700;
   color: var(--space-orange);
 }
-/* Background - Copyright (c) 2019 by WebSonick (https://codepen.io/WebSonick/pen/vjmgu) */
-@keyframes move-twink-back {
-  from {
-    background-position: 0 0;
-  }
-  to {
-    background-position: -10000px 5000px;
-  }
-}
-
-@keyframes move-clouds-back {
-  from {
-    background-position: 0 0;
-  }
-  to {
-    background-position: -10000px 5000px;
-  }
-}
-
-.stars,
-.twinkling,
-.clouds {
-  position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-}
-
-.stars {
-  z-index: 0;
-  background: #000 url('../assets/marsstars.png') repeat top center;
-}
-
-.twinkling {
-  z-index: 0;
-  background: transparent url('../assets/marstwinkling.png') repeat top center;
-  animation: move-twink-back 500s linear infinite;
-}
-
-.clouds {
-  z-index: 0;
-  background: transparent url('../assets/marsclouds.png') repeat top center;
-  animation: move-clouds-back 500s linear infinite;
-}
-
-/* ==============================================================================================
-    End Background - Copyright (c) 2019 by WebSonick (https://codepen.io/WebSonick/pen/vjmgu)
-================================================================================================= */
 </style>
