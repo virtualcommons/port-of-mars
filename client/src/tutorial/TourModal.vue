69ae14b1b5dc143e69a3b3ad2e1410b74ad43462
chrstngyn <Christine.N.Nguyen@asu.edu>
2019-12-03 17:06:08 -0700

    feat: add info about game to intro modal of tour

--- client/src/tutorial/TourModal.vue
+++ client/src/tutorial/TourModal.vue
@@ -13,10 +13,22 @@
         no-enforce-focus="false"
     >
         <template v-slot:modal-title>
-           <code>Port of Mars Tutorial</code>
+           <code><b>TUTORIAL</b></code>
         </template>
         <div class="d-block text-center">
-            <h3>Welcome to Port of Mars!</h3>
+            <h2><code><b>Welcome to Port of Mars!</b></code></h2>
+            <p>You are 1 of 5 residents at <b>Port of Mars</b>, the first long-term habitat on the planet. 
+            Your mission is to <b>stay alive</b> and <b>achieve the accomplishments</b> you came to Mars 
+            to do. Each round, you have 10 timeblocks that you can invest in the maintenance of the shared 
+            infrastructure <b>Upkeep</b> or buying influence currency in specific disciplines.</p>
+            <br />
+            <h4><b>OBJECTIVES</b></h4>
+            <ul>
+                <li>Keep <b>Upkeep</b> from reaching zero! If that happens, the habitat collapses, everyone dies, 
+                    and the game is over.</li>
+                <li>Earn points by achieving <b>Accomplishments</b>! Be the player with the most points at the end 
+                of the game to win.</li>
+            </ul>
         </div>
     </BModal>
 </template>
