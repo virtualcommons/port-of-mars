345de83c02a967bfe4e0a94526bbb8d8693f3954
Calvin Pritchard <pritchard.calvin@gmail.com>
2019-12-04 17:05:33 -0700

    [test](server): added accomplishment purchasing tests

--- server/src/__tests__/Game.ts
+++ server/src/__tests__/Game.ts
@@ -1,27 +1,21 @@
-import {Player} from "@/rooms/Game";
-import {AccomplishmentSet} from "@/state";
+import {GameState, AccomplishmentSet, Player} from "@/state";
+import {PIONEER, RESEARCHER} from "shared/types";
+import {getAccomplishmentByID, getAccomplishmentIDs} from "@/repositories/Accomplishment";
+import * as _ from 'lodash'
 
-describe('a Player', () => {
-  const repo = new AccomplishmentSet();
-  const p = new Player('pioneer');
-  it('can buy a Pioneer accomplishment', () => {
-    const accomplishment = getByID('pioneer', 1);
-    p.buy(accomplishment);
+describe('a Researcher Player Accomplishment', () => {
+  const repo = new AccomplishmentSet(RESEARCHER);
+  const p = new Player(RESEARCHER);
+  it('can be bought if their role matches', () => {
+    const accomplishment = getAccomplishmentByID(RESEARCHER, 1);
+    p.buyAccomplishment(accomplishment);
+    expect(p.inventory.science).toBe(-accomplishment.science);
+    expect(p.victoryPoints).toBe(accomplishment.victoryPoints);
   });
 
-  it('cannot buy a Researcher accomplishment', () => {
-
-  });
-});
-
-describe('an Event model', () => {
-  it('can draw events based on upkeep level', () => {
-
-  });
-});
-
-describe('an Effect', () => {
-  it('can impact the game', () => {
-
+  it('cannot be bought if their roles do not match', () => {
+    const accomplishment = getAccomplishmentByID(PIONEER, 21);
+    const possibleAccomplishments = getAccomplishmentIDs(RESEARCHER);
+    expect(possibleAccomplishments.includes(accomplishment.id)).toBe(false);
   });
 });
