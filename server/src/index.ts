85e62fc297a2caa9c501f11d725295809f027695
chrstngyn <Christine.N.Nguyen@asu.edu>
2019-12-16 15:51:06 -0700

    feat(server): set up postgres database

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,26 +1,37 @@
-import http from "http";
+import "reflect-metadata";
+import {createConnection} from "typeorm";
+import {User} from "./entity/User";
+import http, { createServer } from "http";
 import express from "express";
 import cors from "cors";
 import { Server } from "colyseus";
 import {GameRoom} from "@/game/room";
 import * as path from "path";
 
-const port = Number(process.env.PORT || 2567);
-const app = express()
+function createApp(connection: any) {
+    const port = Number(process.env.PORT || 2567);
+    const app = express()
 
-app.use(cors());
-app.use(express.json())
+    app.use(cors());
+    app.use(express.json())
 
-app.use('/', express.static(path.join(__dirname, 'static')))
+    app.use('/', express.static(path.join(__dirname, 'static')))
 
-const server = http.createServer(app);
-const gameServer = new Server({
-  server,
-  express: app
-});
+    const server = http.createServer(app);
+    const gameServer = new Server({
+    server,
+    express: app
+    });
 
-// register your room handlers
-gameServer.define('game', GameRoom);
+    // register your room handlers
+    gameServer.define('game', GameRoom);
 
-gameServer.listen(port);
-console.log(`Listening on ws://localhost:${ port }`)
+    gameServer.listen(port);
+    console.log(`Listening on ws://localhost:${ port }`)
+}
+
+createConnection().then(async connection => {
+
+    createApp(connection);
+
+}).catch(error => console.log(error));
