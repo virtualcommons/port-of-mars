8e01297fe4fab969757956d7603ebed80ace6268
Calvin Pritchard <pritchard.calvin@gmail.com>
2019-12-17 14:01:05 -0700

    feat(server/orm): added dateCreated field to game

--- server/src/entity/Game.ts
+++ server/src/entity/Game.ts
@@ -1,4 +1,4 @@
-import {Entity, OneToMany, PrimaryGeneratedColumn} from "typeorm";
+import {CreateDateColumn, Entity, OneToMany, PrimaryGeneratedColumn} from "typeorm";
 import {GameEvent} from "./GameEvent";
 
 @Entity()
@@ -8,4 +8,7 @@ export class Game {
 
   @OneToMany(type => GameEvent, event => event.game)
   events!: Array<GameEvent>;
+
+  @CreateDateColumn()
+  dateCreated!: Date;
 }
