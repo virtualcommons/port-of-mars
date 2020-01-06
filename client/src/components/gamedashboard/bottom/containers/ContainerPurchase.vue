2e959b635c2786e647711ee862ed4ba31f97b6c8
Nick Moran <nick42791@gmail.com>
2020-01-06 14:29:08 -0700

    [feat]Accomplishments are purchasable during purchase phase

---
+++ client/src/components/gamedashboard/bottom/containers/ContainerPurchase.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="">
+        <BarAccomplishment v-for="accomplishment in purchasableAccomplishments" :key="accomplishment.label+2" :accomplishment="accomplishment"/>
+    </div>
+</template>
+
+
+<script lang="ts">
+import {Vue, Component} from 'vue-property-decorator'
+import BarAccomplishment from '@/components/gamedashboard/global/cards/BarAccomplishment.vue';
+@Component({
+    components: {
+        BarAccomplishment
+    }
+})
+export default class ContainerPurchase extends Vue {
+    
+    
+    get purchasableAccomplishments() {
+        return this.$store.getters.player.accomplishment.purchasable;
+
+    }
+
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/stylesheets/gamedashboard/bottom/containers/ContainerPurchase.scss';
+</style>
