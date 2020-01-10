fdf174045c53b91ed3c5e217b81007aeaeae9ff7
Lorenzo Faivre <lfaivre@asu.edu>
2020-01-10 13:25:41 -0700

    feat: Created victory and defeat game screens
    [refactor] Abstracted GameBoard component into new containers
    
    Notes:
    
    This is a combination of 3 commits.
    Will need to fully flesh out what we want to do with each screen, but the basics are there.
    Closes #115, closes #116, and closes #117

---
+++ client/src/components/gamedashboard/global/containers/ContainerDefeat.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="container-defeat">
+    <h1 class="animated pulse slower infinite">Game Over</h1>
+    <div class="text">
+      <p>
+        Upkeep has reached zero. However, your efforts will inspire a new
+        generation to continue your legacy on planet Mars.
+      </p>
+      <p>Thank you for playing.</p>
+    </div>
+    <div class="buttons">
+      <button @click="handleRestart">Restart the Game</button>
+      <button @click="handleExit">Exit</button>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Inject } from 'vue-property-decorator';
+import { GameRequestAPI } from '@/api/game/request';
+
+@Component({})
+export default class ContainerDefeat extends Vue {
+  @Inject() readonly $api!: GameRequestAPI;
+
+  private handleRestart() {
+    this.$api.resetGame();
+  }
+
+  private handleExit() {
+    console.log('EXIT GAME');
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '~animate.css/source/attention_seekers/pulse.css';
+@import '@/stylesheets/gamedashboard/global/containers/ContainerDefeat.scss';
+</style>
