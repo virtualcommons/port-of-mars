82022cd98fdb71f53c50b2fa443bdd6fafc70684
Lorenzo Faivre <lfaivre@asu.edu>
2020-01-13 13:10:32 -0700

    feat: add Login view
    Notes:
    
    (Old) Login.vue -> (New) Home.vue
    Login.vue now has a form set up for authentication

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -1,15 +1,43 @@
 <template>
   <div class="login">
     <div class="wrapper">
-      <h1>Port of Mars</h1>
-      <div class="buttons">
-        <router-link to="/lobby">
-          <button @click="setLayout('default-layout')">Let's Go!</button>
-        </router-link>
-        <router-link to="/game">
-          <button @click="setLayout('tutorial-layout')">Go to Tutorial</button>
-        </router-link>
+      <div class="text">
+        <h1>Port of Mars</h1>
+        <h2>Sign In</h2>
       </div>
+      <form
+        @submit="submitForm"
+        action="https://vuejs.org/"
+        method="get"
+        autocomplete="off"
+        class="login-form"
+      >
+        <div class="input-username">
+          <label for="username">ASURITE ID</label>
+          <div class="input-wrapper">
+            <input
+              type="text"
+              id="username"
+              name="username"
+              v-model="username"
+            />
+          </div>
+        </div>
+        <div class="input-password">
+          <label for="password">Password</label>
+          <div class="input-wrapper">
+            <input
+              type="password"
+              id="password"
+              name="password"
+              v-model="password"
+            />
+          </div>
+        </div>
+        <div class="submit">
+          <input :disabled="submitDisabled" type="submit" value="Submit" />
+        </div>
+      </form>
     </div>
   </div>
 </template>
@@ -19,19 +47,26 @@ import { Vue, Component } from 'vue-property-decorator';
 
 @Component({})
 export default class Login extends Vue {
-  /**
-   * setLayout() method
-   * @param layout The string value of the layout.
-   * Commits to the store by changing the layout
-   * string value on the store.
-   *
-   */
-  setLayout(layout: string) {
-    this.$store.commit('SET_LAYOUT', layout);
+  private username: string = '';
+  private password: string = '';
+
+  get submitDisabled(): boolean {
+    if (this.username && this.password) {
+      return false;
+    }
+    return true;
+  }
+
+  private submitForm(e: any): any {
+    // TODO: Authenticate user and re-route to Home screen (?)
+    console.log('EVENT: ', e);
+    // console.log('USERNAME: ', this.username);
+    // console.log('PASSWORD: ', this.password);
+    e.preventDefault();
   }
 }
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 @import '@/stylesheets/views/Login.scss';
 </style>
