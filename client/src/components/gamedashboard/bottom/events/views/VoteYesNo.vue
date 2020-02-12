65d98a7a1bec4399db457fbaabd183bd00632b50
chrstngyn <Christine.N.Nguyen@asu.edu>
2020-02-12 13:41:53 -0700

    feat[client]: get player vote during Personal Gain event

--- client/src/components/gamedashboard/bottom/events/views/VoteYesNo.vue
+++ client/src/components/gamedashboard/bottom/events/views/VoteYesNo.vue
@@ -10,12 +10,27 @@
 
 <script lang="ts">
 import { Vue, Component } from 'vue-property-decorator';
+import { Role, ROLES, CURATOR, ENTREPRENEUR, PIONEER, POLITICIAN, RESEARCHER } from 'shared/types';
 
 @Component({})
 export default class VoteYesNo extends Vue {
-  private handleVote(selection: boolean): void {
-    console.log(selection);
+  private defaultVote: boolean = true;
+  private voteResults: { [role in Role]: boolean} = {
+    [CURATOR]: this.defaultVote,
+    [ENTREPRENEUR]: this.defaultVote,
+    [PIONEER]: this.defaultVote,
+    [POLITICIAN]: this.defaultVote,
+    [RESEARCHER]: this.defaultVote
+  };
+
+  get playerRole() {
+      return this.$tstore.state.role;
   }
+
+  private handleVote(selection: boolean): void {
+    this.voteResults[this.playerRole] = selection; 
+  }
+  
 }
 </script>
 
