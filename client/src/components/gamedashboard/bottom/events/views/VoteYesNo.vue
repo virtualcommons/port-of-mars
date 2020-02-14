19f45cf952047bbf3cf9710807b0fc230f2d3608
chrstngyn <Christine.N.Nguyen@asu.edu>
2020-02-14 11:51:29 -0700

    style(client): add text to indicate player's vote on Personal Gain event

--- client/src/components/gamedashboard/bottom/events/views/VoteYesNo.vue
+++ client/src/components/gamedashboard/bottom/events/views/VoteYesNo.vue
@@ -9,6 +9,12 @@
         No
       </button>
     </div>
+
+    <p :style="!playerVote ? 'color: rgba(0, 0, 0, 0); padding-top: 2rem' : ''" 
+        v-bind:class="{'selected-button-text': playerVote }"
+        >
+      You have selected {{ playerVote }}.
+    </p>
   </div>
 </template>
 
@@ -28,14 +34,21 @@ import { GameRequestAPI } from '@/api/game/request';
 @Component({})
 export default class VoteYesNo extends Vue {
   @Inject() api!: GameRequestAPI;
+  playerVote: string = '';
+  selectedButton = document.activeElement;
 
   get playerRole() {
     return this.$tstore.state.role;
   }
 
-  private handleVote(selection: boolean): void {
+  private handleVote(selection: boolean) {
     const voteResults = { role: this.playerRole, vote: selection };
     this.api.savePersonalGainVote(voteResults);
+    if (selection) {
+      this.playerVote = 'Yes';
+    } else {
+      this.playerVote = 'No';
+    }
   }
 }
 </script>
