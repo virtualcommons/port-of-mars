a956a8c596ccc0125acebed1301a5e72397ecdb1
Lorenzo Faivre <lfaivre@asu.edu>
2020-02-26 16:53:44 -0700

    feat: initial tournament entities

---
+++ server/src/entity/TournamentRoundInvite.ts
@@ -0,0 +1,39 @@
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  OneToMany,
+  CreateDateColumn,
+  PrimaryGeneratedColumn,
+  Unique
+} from 'typeorm';
+import { TournamentRound } from '@/entity/TournamentRound';
+import { User } from '@/entity/User';
+
+@Entity()
+export class TournamentRoundInvite {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column()
+  tournamentRoundId!: number;
+
+  @ManyToOne(
+    type => TournamentRound,
+    tournamentRound => tournamentRound.invitations
+  )
+  tournamentRound!: TournamentRound;
+
+  @Column()
+  userId!: number;
+
+  @ManyToOne(type => User)
+  user!: User;
+
+  @Column()
+  hasParticipated!: boolean;
+
+  @CreateDateColumn()
+  dateCreated!: Date;
+}
