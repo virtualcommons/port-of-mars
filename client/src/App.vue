25de9d3ffa5320540d5ec932be44b0bbe23aea4a
chrstngyn <Christine.N.Nguyen@asu.edu>
2020-02-28 14:02:31 -0700

    feat(client): create class that gets environment
    returns correct + current environment
    map keystrokes to commands linked to game function
    use lifecycle hooks to set keydown event
    add comments to improve code readability

--- client/src/App.vue
+++ client/src/App.vue
@@ -6,6 +6,7 @@
 
 <script lang="ts">
 import { Vue, Component } from 'vue-property-decorator';
+import { EnvironmentMode } from './settings';
 import Game from '@/views/Game.vue';
 import Tutorial from '@/views/Tutorial.vue';
 import BootstrapVue from 'bootstrap-vue';
@@ -18,8 +19,9 @@ Vue.use(BootstrapVue);
   }
 })
 export default class Home extends Vue {
+  env: EnvironmentMode = new EnvironmentMode;
+
   /**
-   * getLayout() computed property
    * Gets the value of the layout from the vuex store
    * @return the value of layout
    *
@@ -27,6 +29,15 @@ export default class Home extends Vue {
 
   get layout() {
     return this.$tstore.getters.layout;
+  }
+
+  /**
+   * Gets the string value of the current environment.
+   * @return The environment.
+   * 
+   */
+  get mode() {
+    return this.env.environment;
   }
 }
 </script>
