64dfaca280b36e300687c81ca352d52b464ed528
Allen Lee <allen.lee@asu.edu>
2020-03-02 16:16:12 -0700

    fix(content): minor refinements to tutorial intro

--- client/src/components/tutorial/TourModal.vue
+++ client/src/components/tutorial/TourModal.vue
@@ -24,10 +24,10 @@
       <p>
         You are 1 of 5 members of Generation Zero at <code><b>Port of Mars</b></code>, the first long-term human
         community on Mars. Your mission is to <code><b>keep the community alive</b></code> while earning individual
-        points by <code><b>achieving Accomplishments</b></code> that make up the future legacy of Mars. Each round, you
-        have <code><b>10 timeblocks</b></code> that you can invest in the maintenance of the shared infrastructure
-        called <code><b>System Health</b></code> or purchasing <code><b>influence</b></code> currency in specific
-        disciplines: <b>Culture, Legacy, Science, Governament, Finance</b>.
+        points by <code><b>achieving Accomplishments</b></code> for the future legacy of Mars. Each round, you
+        have <code><b>10 timeblocks</b></code> that you can either (a) invest in the maintenance of the shared infrastructure
+        called <code><b>System Health</b></code> or (b) purchase <code><b>influence</b></code> currency in specific
+        disciplines: <b>Culture, Finance, Government, Legacy, and Science</b>.
       </p>
 
       <p>
@@ -36,8 +36,7 @@
 
       <p>
         <code><b>Influence</b></code> resources are used to purchase <code><b>Accomplishments</b></code>, which is how you win
-        the game. Each <code><b>Accomplishment</b></code> you purchase gives you Victory Points. The player with the
-        most points at the end of the game wins.
+        the game. You <b>earn points</b> for each <code><b>Accomplishment</b></code> that you purchase.
       </p>
 
       <br />
@@ -49,8 +48,8 @@
           the habitat collapses, everyone dies, and the game is over.
         </li>
         <li>
-          Earn points by achieving <code><b>Accomplishments</b></code
-          >! Be the player with the most points at the end of the game to win.
+          Earn points by achieving <code><b>Accomplishments</b></code>!
+          Be the player with the most points at the end of the game to win.
         </li>
       </ul>
 
