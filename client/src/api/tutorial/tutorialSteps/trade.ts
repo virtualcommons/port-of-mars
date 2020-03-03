643a6d694ebd2ea226d59cefa85ea35709c3c0d1
Calvin Pritchard <pritchard.calvin@gmail.com>
2020-03-03 12:24:00 -0700

    style: consistently formatted trade tutorial steps
    [skip ci]

--- client/src/api/tutorial/tutorialSteps/trade.ts
+++ client/src/api/tutorial/tutorialSteps/trade.ts
@@ -1,19 +1,20 @@
 import {Step, LEFT, TOP} from "@/types/tutorial";
-import { Phase,RESEARCHER,CURATOR } from "shared/types";
+import {Phase, RESEARCHER, CURATOR} from "shared/types";
 
-const steps:Array<Step> = [
-    {
-      target: '.tour-trade',
-      content:
-        `The next phase is the Trading Phase! Here you will trade for resources that you need to purchase Accomplishment cards!`,
-      params: {
-        placement: LEFT,
-      },
-      
-      stateTransform:[
-        {SET_GAME_PHASE:Phase.trade,
-        SET_INVENTORY:{
-          data:{
+const steps: Array<Step> = [
+  {
+    target: '.tour-trade',
+    content:
+      `The next phase is the Trading Phase! Here you will trade for resources that you need to purchase Accomplishment cards!`,
+    params: {
+      placement: LEFT,
+    },
+
+    stateTransform: [
+      {
+        SET_GAME_PHASE: Phase.trade,
+        SET_INVENTORY: {
+          data: {
             culture: 0,
             finance: 5,
             government: 5,
@@ -21,14 +22,14 @@ const steps:Array<Step> = [
             science: 5,
             upkeep: 0,
           },
-          role:RESEARCHER,
+          role: RESEARCHER,
         },
-        ADD_TO_TRADES:{
-          id:'mock-trade',
-          trade:{
-            from:{
-              role:CURATOR,
-              resourceAmount:{
+        ADD_TO_TRADES: {
+          id: 'mock-trade',
+          trade: {
+            from: {
+              role: CURATOR,
+              resourceAmount: {
                 culture: 1,
                 finance: 1,
                 government: 1,
@@ -36,9 +37,9 @@ const steps:Array<Step> = [
                 science: 1,
               }
             },
-            to:{
-              role:RESEARCHER,
-              resourceAmount:{
+            to: {
+              role: RESEARCHER,
+              resourceAmount: {
                 culture: 1,
                 finance: 1,
                 government: 1,
@@ -47,105 +48,110 @@ const steps:Array<Step> = [
               }
             }
           }
-        }}
-      ],
-    },
-    {
-      target: '.tour-trade-item',
-      content: `All active trades are listed here. If you are on the receiving end of a trade, you will have the option to either 
+        }
+      }
+    ],
+  },
+  {
+    target: '.tour-trade-item',
+    content: `All active trades are listed here. If you are on the receiving end of a trade, you will have the option to either
       accept or decline the trade.
       If you are the sender of a trade, you only have the option to cancel the request.`,
-      params:{
-        placement: 'top',
-      }
-    },
-    {
-      target: '.tour-chat',
-      content:`Before making trade requests, you should talk about your plans in chat!`,
-      params: {
-        placement: 'left'
-      },
-      stateTransform:[
-        {ADD_TO_CHAT:{
-            message:'Hey Researcher, can I have 2 science in exchange for 1 legacy?',
-            role:`Pioneer`,
-            dateCreated:new Date().getTime(),
-            round:0,
-        }},
-        {ADD_TO_CHAT:{
-          message:'Sure, let me make that trade right now!',
-          role:`Researcher`,
-          dateCreated:new Date().getTime(),
-          round:0,
-      }},
-      ],
-    },
-    {
-      target: '.tour-trade-request',
-      content: `To request a trade, you must first select a person to trade with.`,
-      params:{
-        placement:'top',
-      },
-      stateTransform:[
-        {
-          required: true,
-        }
-      ]
-    },
-    {
-      target: '.tour-trade-request',
-      content: `Then, you select the amount of each resource you are willing to give up.
-      You cannot send more resources than you currently have.`,
-      params:{
-        placement:'top',
-      },
-      stateTransform:[
-        {
-          required: true,
-        }
-      ]
-    },
-    {
-      target: '.tour-trade-request',
-      content: `Finally, you choose how much of any resource that you want.`,
-      params:{
-        placement:'top',
-      },
-      stateTransform:[
-        {
-          required: true,
-        }
-      ]
-    },
-    {
-      target: '.tour-trade',
-      content: `Hit send to see your trade in action!
-      (you may have to scroll to see the button)`,
-      params:{
-        placement: LEFT 
-      },
-      stateTransform:[
-        {
-          required:true,
-        }
-
-      ]
-    },
-    {
-      target: '.tour-container-bottom',
-      content: 'Quiz Question',
-      params: {
-        placement: TOP,
-        tutorialElementId: 'trade'
-      },
-      stateTransform:[
-        {
-          required:false,
-        }
-
-      ]
+    params: {
+      placement: 'top',
     }
+  },
+  {
+    target: '.tour-chat',
+    content: `Before making trade requests, you should talk about your plans in chat!`,
+    params: {
+      placement: 'left'
+    },
+    stateTransform: [
+      {
+        ADD_TO_CHAT: {
+          message: 'Hey Researcher, can I have 2 science in exchange for 1 legacy?',
+          role: `Pioneer`,
+          dateCreated: new Date().getTime(),
+          round: 0,
+        }
+      },
+      {
+        ADD_TO_CHAT: {
+          message: 'Sure, let me make that trade right now!',
+          role: `Researcher`,
+          dateCreated: new Date().getTime(),
+          round: 0,
+        }
+      },
+    ],
+  },
+  {
+    target: '.tour-trade-request',
+    content: `To request a trade, you must first select a person to trade with.`,
+    params: {
+      placement: 'top',
+    },
+    stateTransform: [
+      {
+        required: true,
+      }
+    ]
+  },
+  {
+    target: '.tour-trade-request',
+    content: `Then, you select the amount of each resource you are willing to give up.
+      You cannot send more resources than you currently have.`,
+    params: {
+      placement: 'top',
+    },
+    stateTransform: [
+      {
+        required: true,
+      }
+    ]
+  },
+  {
+    target: '.tour-trade-request',
+    content: `Finally, you choose how much of any resource that you want.`,
+    params: {
+      placement: 'top',
+    },
+    stateTransform: [
+      {
+        required: true,
+      }
+    ]
+  },
+  {
+    target: '.tour-trade',
+    content: `Hit send to see your trade in action!
+      (you may have to scroll to see the button)`,
+    params: {
+      placement: LEFT
+    },
+    stateTransform: [
+      {
+        required: true,
+      }
+
+    ]
+  },
+  {
+    target: '.tour-container-bottom',
+    content: 'Quiz Question',
+    params: {
+      placement: TOP,
+      tutorialElementId: 'trade'
+    },
+    stateTransform: [
+      {
+        required: false,
+      }
+
+    ]
+  }
 
 ]
 
-export default steps; 
+export default steps;
