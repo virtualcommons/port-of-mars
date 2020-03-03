ccffd8e1d18f7bb473f49b16ec0ebfc01b53cfc6
Allen Lee <allen.lee@asu.edu>
2020-03-02 17:00:49 -0700

    fix(text): refine quiz questions text
    - improve readability and language
    - rename upkeep to System Health
    
    TODO: I think we still need to embed the image of the screwcard? Also,
    quiz question #5 seems unfinished or ... something?

--- server/src/data/QuizQuestions.ts
+++ server/src/data/QuizQuestions.ts
@@ -5,13 +5,12 @@ const quizQuestionsRaw: Array<QuizData> = [
     {
       id: 1,
       question: "When does the game end?",
-       
       correct: 2,
       options: [
-        "When upkeep reaches 0",
-       "After an unknown number of rounds",
-       "After an unknown number of rounds, or earlier if Upkeep reaches 0",
-       "When one of the players reaches 20 points",
+        "When System Health reaches 0.",
+       "After an unknown number of rounds.",
+       "After an unknown number of rounds, or earlier if System Health reaches 0.",
+       "When one of the players reaches 20 points.",
       ]
     },
     {
@@ -19,82 +18,75 @@ const quizQuestionsRaw: Array<QuizData> = [
       question: "Can you chat with other participants?",
       correct: 1,
       options:[
-        "No, communication is not possible in this game.",
-        "Yes, in the chat box during the whole duration of the game.",
-        "Yes, in the chat box during designated periods.",
-        "Yes, if Upkeep is bigger than 35.",
+        "No, communication is not allowed in this game.",
+        "Yes, in the chat box throughout the entire game.",
+        "Yes, in the chat box only during specially designated periods.",
+        "Yes, as long as System Health is greater than 35.",
       ]
     },
     {
       id: 3,
-      question: "How do you trade cards with others?",
-
+      question: "How do you trade Influence Resources with others?",
       correct: 2,
       options:[
-        "During the trade period you indicate your preferences of cards",
-        "Expressing your preferences in the chat box is sufficient.",
-        "Discuss via chat and then express your deals during trade period",
-        "When you allocate your time-blocks.",
+        "During the trading phase you say what Influence Resources you want and then you'll receive them.",
+        "By communicating your trade preferences in the chat box.",
+        "Discuss potential trades with the other members of your group in the chat and then issue Trade Requests during the trading phase to execute a Trade.",
+        "By investing your timeblocks.",
       ]
     },
     {
       id: 4,
-      question: "If Upkeep drops below 65 and is above 35 what happens?",
-       
+      question: "What happens if the System Health drops between 35 and 65?",
       correct: 1,
-      options:["Chat is not possible anymore.",
-      "Two events will happen that round.",
-      "You get an additional four time-blocks.",
+      options:["You can no longer chat with your group.",
+      "Two events will occur in this round.",
+      "You get an additional four timeblocks.",
       "You get an extra accomplishment card.",]
     },
     {
       id: 5,
       question: "The following card is a special accomplishment card [image screwcard]. What does this card do?",
-       
       correct: 3,
-      options:["It costs x influence cards, y influence cards , and ..",
+      options:["It costs x influence cards, y influence cards, and ..",
       "You earn x points",
       "You can only buy this card if you have the required influence cards",
       "All above three answers are correct",]
     },
     {
       id: 6,
-      question: "The following card is a special accomplishment card [image screwcard]. What does this card do?",
-       
+      question: "The following is a special Accomplishment card [image screwcard]. What does this Acomplishment do?",
       correct: 3,
-      options:["It cost the player nothing individually",
-      "The Upkeep level of the group will be reduced with x points",
-      "The individual player will earn x points",
+      options:["No Influence Resources are needed to purchase this Accomplishment.",
+      "The group's System Health will be reduced by 6",
+      "The player purchasing this Accomplishment will earn 3 points",
       "All above three answers are correct",]
     },
     {
       id: 7,
-      question: "You get each round 10 time blocks? Which of the following statements is incorrect:",
-       
+      question: "Each round you have 10 timeblocks to invest. Which of the following statements is incorrect?",
       correct: 1,
-      options:["You can spend as many time blocks as you want.",
-      "Every time block not spend can be saved for next round.",
-      "Each player has their special skills impacting the costs of influence cards.",
-      "By default each time-block in Upkeep increases the Upkeep of the group with one unit.",]
+      options:["You can spend as many timeblocks as you want.",
+      "Every timeblock you do not spend can be saved and used in the next round.",
+      "Each player has unique skills that change the costs of specific Influence Resources.",
+      "Each timeblock invested in System Health increases your group's System Health by 1.",]
     },
     {
       id: 8,
-      question: "If Upkeep drops below 35 which of the following statements is true?",
-       
+      question: "If your group's System Health drops below 35 which of the following statements is true?",
       correct: 1,
-      options:["The game ends",
-      "The group will experience three events this round.",
-      "The player with the least amount of points is eliminated.",
+      options:["The game ends.",
+      "The group will encounter three Events this round.",
+      "The player with the least number of points is eliminated.",
       "Nothing, life as usual on Mars.",]
     },
     {
       id: 9,
-      question: "At the start of each round, 25 units is subtracted from Upkeep. What does this represent?",
-       
+      question: "At the start of each round, 25 units is subtracted from System Health. What does this represent?",
       correct: 1,
-      options:["The lack of contributions of others.",
-      "A standard wear and tear.",
-      "Solar flares",
+      options:["A lack of contributions from the other members of your group.",
+      "Standard wear and tear.",
+      "A solar flare striking your habitat.",
       "The costs of trade.",]
     }
   ]
@@ -109,7 +101,7 @@ export const QuizQuestions:Array<QuizQuestionData> = quizQuestionsRaw.map((q) =>
 
 export const QuizQuestionAnswers = quizQuestionsRaw.map((q) => {
   return {
-    id:q.id,
-    correct:q.correct
-  }
-})
+    id: q.id,
+    correct: q.correct
+  };
+});
