047edbfc1c462ce60241f62b02b8ad0b3369b8cb
Calvin Pritchard <pritchard.calvin@gmail.com>
2020-03-03 16:55:10 -0700

    fix: add logging service

---
+++ server/src/services/logging.ts
@@ -0,0 +1,25 @@
+import pino, {LogFn, Logger} from 'pino'
+
+export interface LogService {
+  trace: LogFn
+  debug: LogFn
+  info: LogFn
+  warn: LogFn
+  fatal: LogFn
+}
+
+export interface Logging {
+  getLogger(filename: string): LogService;
+}
+
+export class DevLogging implements Logging {
+  logger: Logger;
+
+  constructor() {
+    this.logger = pino();
+  }
+
+  getLogger(filename: string): LogService {
+    return this.logger;
+  }
+}
