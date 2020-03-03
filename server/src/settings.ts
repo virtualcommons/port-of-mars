970b3192cc49289386ce931cf9acb89db6c4bcca
Calvin Pritchard <pritchard.calvin@gmail.com>
2020-03-03 11:39:59 -0700

    refactor: made settings setup lazy
    [skip ci]

--- server/src/settings.ts
+++ server/src/settings.ts
@@ -5,23 +5,23 @@ export interface AppSettings {
   host: string
 }
 
-const dev: AppSettings = {
+const dev: () => AppSettings = () => ({
   emailer: new MemoryEmailer(),
   host: 'http://localhost:8081'
-};
+});
 
-const staging = {
+const staging: () => AppSettings = () => ({
   emailer: new MemoryEmailer(),
   host: 'http://localhost:8081'
-};
+});
 
-const prod = {
+const prod: () => AppSettings = () => ({
   emailer: new MemoryEmailer(),
   host: 'https://portofmars.asu.edu'
-};
+});
 
 const env = process.env.NODE_ENV;
 
-export const settings = env === 'development' ?
-  dev : env === 'staging' ?
-    staging : prod;
+export const settings: AppSettings = env === 'development' ?
+  dev() : env === 'staging' ?
+    staging() : prod();
