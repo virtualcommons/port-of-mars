c23756a5419b24f5d79f245389b3b3729b48c541
Lorenzo Faivre <lfaivre@asu.edu>
2020-03-23 11:45:21 -0700

    feat: initial game board redesign work
    notes:
    
    - completed top section of game board
    - completed popups
    - completed profile menu
    
    - still need to implement phases in bottom section of game board
    - still need to add trading modal
    - still need to add player modals
    - still need to connect to tutorial
    - still need to add chat notifications
    
    - kept some 'new' component prefixes to prevent conflicts and confusion
    - can rename new and delete old components as we go

---
+++ client/src/repo/instructions.ts
@@ -0,0 +1,9 @@
+export const phaseInstructions = {
+  events: 'These are the instructions for the Events phase.',
+  invest: 'These are the instructions for the Investment phase.',
+  trade: 'These are the instructions for the Trade phase.',
+  purchase: 'These are the instructions for the Purchase phase.',
+  discard: 'These are the instructions for the Discard phase.',
+  victory: 'These are the instructions for the Victory phase.',
+  defeat: 'These are the instructions for the Defeat phase.'
+};
