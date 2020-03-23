86e8114f7f7d70ae0ca23a0bdbc5b1107b0a49da
Lorenzo Faivre <lfaivre@asu.edu>
2020-03-23 11:45:21 -0700

    feat: initial game board redesign work
    notes:
    
    - completed top section of game board
    - completed popups
    - completed profile menu
    
    - still need to implement phases in bottom section of game board
    - still need to add trading modal
    - still need to add player modals
    - still need to connect to tutorial
    - still need to add chat notifications
    
    - kept some 'new' component prefixes to prevent conflicts and confusion
    - can rename new and delete old components as we go

---
+++ client/src/components/gamedashboard/global/containers/NewGameBoardContainer.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="gameboardcontainer container">
+    <ProfileMenu />
+    <div class="gameboardtop row">
+      <NewGameBoardTop />
+    </div>
+    <div class="gameboardbottom row">
+      <NewGameBoardBottom />
+    </div>
+    <ActiveEventsPopup />
+    <InventoryPopup />
+    <ChatPopup />
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+import NewGameBoardTop from '@port-of-mars/client/components/gamedashboard/top/containers/NewGameBoardTop.vue';
+import NewGameBoardBottom from '@port-of-mars/client/components/gamedashboard/bottom/containers/NewGameBoardBottom.vue';
+import ProfileMenu from '@port-of-mars/client/components/gamedashboard/global/ProfileMenu.vue';
+import ActiveEventsPopup from '@port-of-mars/client/components/gamedashboard/global/ActiveEventsPopup.vue';
+import InventoryPopup from '@port-of-mars/client/components/gamedashboard/global/InventoryPopup.vue';
+import ChatPopup from '@port-of-mars/client/components/gamedashboard/global/ChatPopup.vue';
+
+@Component({
+  components: {
+    NewGameBoardTop,
+    NewGameBoardBottom,
+    ProfileMenu,
+    ActiveEventsPopup,
+    InventoryPopup,
+    ChatPopup
+  }
+})
+export default class NewGameBoardContainer extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+@import '@port-of-mars/client/stylesheets/gamedashboard/global/containers/NewGameBoardContainer.scss';
+</style>
