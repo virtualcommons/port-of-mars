d015127bfb9bf33dfe2a30acb1cba5f96e5875ad
Lorenzo Faivre <lfaivre@asu.edu>
2020-03-23 11:45:21 -0700

    feat: initial redesign setup

---
+++ client/src/components/gamedashboard/global/containers/NewGameBoardContainer.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="containerboard container">
+    <div class="gameboardtop row">top</div>
+    <div class="gameboardbottom row">
+      <NewGameBoardBottom />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+import NewGameBoardBottom from '@port-of-mars/client/components/gamedashboard/bottom/containers/NewGameBoardBottom.vue';
+
+@Component({
+  components: {
+    NewGameBoardBottom
+  }
+})
+export default class NewGameBoardContainer extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+@import '@port-of-mars/client/stylesheets/gamedashboard/global/containers/NewGameBoardContainer.scss';
+</style>
