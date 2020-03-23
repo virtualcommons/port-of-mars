326e4d9bdd76bf73c24e5fed0526205c7fcba760
Lorenzo Faivre <lfaivre@asu.edu>
2020-03-23 11:45:21 -0700

    feat: initial game board redesign work
    notes:
    
    - completed top section of game board
    - completed popups
    - completed profile menu
    
    - still need to implement phases in bottom section of game board
    - still need to add trading modal
    - still need to add player modals
    - still need to connect to tutorial
    - still need to add chat notifications
    
    - kept some 'new' component prefixes to prevent conflicts and confusion
    - can rename new and delete old components as we go

---
+++ client/src/components/gamedashboard/bottom/containers/NewPhaseContainer.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="c-phasecontainer row">
+    <div class="content">
+      <!-- THIS IS WHERE WE SWAP BOARD BASED ON PHASE -->
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+
+@Component({
+  components: {}
+})
+export default class NewPhaseContainer extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+@import '@port-of-mars/client/stylesheets/gamedashboard/bottom/containers/NewPhaseContainer.scss';
+</style>
