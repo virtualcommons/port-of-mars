f15f0c0ef8440b97de82bec380b630192d5482f6
chrstngyn <Christine.N.Nguyen@asu.edu>
2020-03-25 16:22:04 -0700

    feat(client): finish static player dashboard page
    add items to: action items, upcoming games, player stats
    add styling for spacing, items, buttons

--- client/src/views/PlayerDashboard.vue
+++ client/src/views/PlayerDashboard.vue
@@ -1,13 +1,10 @@
 <template>
-    <div class="player-dashboard">
+    <BRow class="player-dashboard">
         <UserHeader />
-        <h1>Port of Mars</h1>
-        <h2>Your Dashboard</h2>
-        <div class="left">
-
+        <BCol class="left">
             <!-- onboarding -->
             <div class="top">
-                <h3>Onboarding</h3>
+                <h2>Onboarding</h2>
                 <p>Pass Tutorial Quiz</p>
                 <p>Take Introduction Survey</p>
                 <p>Take Exit Survey</p>
@@ -15,29 +12,58 @@
 
             <!-- player stats -->
             <div class="bottom">
-                <h3>Your Stats</h3>
+                <h2>Your Stats</h2>
                 <p>Games Played: 1 (1 in progress)</p>
+                <div class="player-stat-item">
+                    <p class="player-stat-item-header">Game 1</p>
+                    <p class="player-stat-item-header">20 March 2020 @ 10:00 A.M.</p>
+                    <p>Status: Won</p>
+                    <p>Points: 10</p>
+                </div>
             </div>
-        </div>
+        </BCol>
         
         <!-- action items -->
-        <div class="middle">
-            <h3>Action Items</h3>
-        </div>
+        <BCol class="middle">
+            <div class="top">
+                <h1>Port of Mars</h1>
+                <h2>Player Dashboard</h2>
+            </div>
+            
+            <div class="bottom">
+                <h2>Action Items</h2>
+                <div class="action-item">
+                    <p>Connect to Current Game </p>
+                    <BButton squared class="button" variant="dark">Go</BButton>
+                </div>
+                <div class="action-item">
+                    <p>Take Exit Survey</p>
+                    <BButton squared class="button" variant="dark">Go</BButton>
+                </div>
+            </div>
+        </BCol>
 
         <!-- upcoming games -->
-        <div class="right">
-            <h3>Upcoming Games</h3>
-        </div>
-    </div>
+        <BCol class="right">
+            <h2>Upcoming Games</h2>
+            <div class="upcoming-game-item">
+                <p>April 1 @ 12:00 PM</p>
+                <BButton squared class="button" variant="dark" disabled>Join</BButton>
+            </div>
+        </BCol>
+    </BRow>
 </template>
 
 <script lang="ts">
 import { Vue, Component } from 'vue-property-decorator';
 import UserHeader from '@port-of-mars/client/components/global/UserHeader.vue';
+import { BRow, BCol, BButton } from 'bootstrap-vue';
 
 @Component({
     components: {
+        BButton,
+        BRow,
+        BCol,
         UserHeader
     }
 })
