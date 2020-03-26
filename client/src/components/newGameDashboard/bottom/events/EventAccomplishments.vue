8e824d9419db303e8981992e110b7dec4eecfd11
chrstngyn <Christine.N.Nguyen@asu.edu>
2020-03-26 16:37:54 -0700

    refactor(client): create new dir to house new components for new ui

---
+++ client/src/components/newGameDashboard/bottom/events/EventAccomplishments.vue
@@ -0,0 +1,31 @@
+<template>
+  <component :is="eventAccomplishmentsView"></component>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator';
+import AccomplishmentsSelectPurchased from '@port-of-mars/client/components/gamedashboard/bottom/events/views/AccomplishmentsSelectPurchased.vue';
+
+@Component({
+  components: {
+    AccomplishmentsSelectPurchased
+  }
+})
+export default class EventAccomplishments extends Vue {
+  @Prop({ default: '' }) private eventView!: string;
+
+  get eventAccomplishmentsView(): string {
+    switch (this.eventView) {
+      case 'ACCOMPLISHMENT_SELECT_PURCHASED':
+        return 'AccomplishmentsSelectPurchased';
+      default:
+        return '';
+    }
+    return '';
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@port-of-mars/client/stylesheets/gamedashboard/bottom/events/EventAccomplishments.scss';
+</style>
---
+++ client/src/components/newGameDashboard/global/containers/NewGameBoardContainer.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="gameboardcontainer container">
+    <ProfileMenu />
+    <div class="gameboardtop row">
+      <NewGameBoardTop />
+    </div>
+    <div class="gameboardbottom row">
+      <NewGameBoardBottom />
+    </div>
+    <ActiveEventsPopup />
+    <InventoryPopup />
+    <ChatPopup />
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+import NewGameBoardTop from '@port-of-mars/client/components/newGameDashboard/top/containers/NewGameBoardTop.vue';
+import NewGameBoardBottom from '@port-of-mars/client/components/newGameDashboard/bottom/containers/NewGameBoardBottom.vue';
+import ProfileMenu from '@port-of-mars/client/components/newGameDashboard/global/ProfileMenu.vue';
+import ActiveEventsPopup from '@port-of-mars/client/components/newGameDashboard/global/ActiveEventsPopup.vue';
+import InventoryPopup from '@port-of-mars/client/components/newGameDashboard/global/InventoryPopup.vue';
+import ChatPopup from '@port-of-mars/client/components/newGameDashboard/global/ChatPopup.vue';
+
+@Component({
+  components: {
+    NewGameBoardTop,
+    NewGameBoardBottom,
+    ProfileMenu,
+    ActiveEventsPopup,
+    InventoryPopup,
+    ChatPopup
+  }
+})
+export default class NewGameBoardContainer extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+@import '@port-of-mars/client/stylesheets/gamedashboard/global/containers/NewGameBoardContainer.scss';
+</style>
