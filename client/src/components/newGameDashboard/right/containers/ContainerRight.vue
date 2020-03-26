49c6a93d90d1b90a3c9c47e7c1d386f8f749e075
chrstngyn <Christine.N.Nguyen@asu.edu>
2020-03-26 16:37:54 -0700

    refactor(client): create new dir to house new components for new ui

---
+++ client/src/components/newGameDashboard/right/containers/ContainerRight.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container-right">
+    <ContainerPlayers />
+    <Chat />
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+import ContainerPlayers from '@port-of-mars/client/components/gamedashboard/right/containers/ContainerPlayers.vue';
+import Chat from '@port-of-mars/client/components/gamedashboard/right/Chat.vue';
+
+@Component({
+  components: {
+    ContainerPlayers,
+    Chat
+  }
+})
+export default class ContainerRight extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+@import '@port-of-mars/client/stylesheets/gamedashboard/right/containers/ContainerRight.scss';
+</style>
---
+++ client/src/views/NewGame.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="default-layout">
+    <NewGameDashboard />
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Inject, Provide, Prop } from 'vue-property-decorator';
+import { Client, Room } from 'colyseus.js';
+import { applyGameServerResponses } from '@port-of-mars/client/api/game/response';
+import { GameRequestAPI } from '@port-of-mars/client/api/game/request';
+import { EnvironmentMode } from '@port-of-mars/client/settings';
+import NewGameDashboard from '@port-of-mars/client/components/NewGameDashboard.vue';
+import _ from "lodash";
+import { LOBBY_PAGE } from "@port-of-mars/shared/routes";
+
+@Component({
+  name: 'game',
+  components: {
+    NewGameDashboard
+  }
+})
+export default class Game extends Vue {
+  @Inject() readonly $client!: Client;
+  @Provide() private api: GameRequestAPI = new GameRequestAPI();
+  private hasApi: boolean = false;
+  private env: EnvironmentMode = new EnvironmentMode();
+
+  async created() {
+    this.api.room?.leave();
+    let gameRoom: Room;
+    if (this.$ajax.reservation) {
+      console.log('consuming reservation');
+      gameRoom = await this.$client.consumeSeatReservation(this.$ajax.reservation);
+    } else if (this.$ajax.gameConnectionInfo) {
+      console.log('reconnecting reservation to ', this.$ajax.gameConnectionInfo);
+      gameRoom = await this.$client.reconnect(this.$ajax.gameConnectionInfo.roomId, this.$ajax.gameConnectionInfo.sessionId);
+    } else {
+      console.log('an error occured');
+      return;
+    }
+    applyGameServerResponses(gameRoom, this.$tstore);
+    this.api.connect(gameRoom);
+    this.hasApi = true;
+    this.$store.commit('SET_LAYOUT', 'game');
+    this.$store.commit('SET_ENVIRONMENT', this.env.environment);
+  }
+
+  destroyed() {
+    console.log('leaving game');
+    if (this.api.room) this.api.room.leave();
+    this.$tstore.commit('RESET_STATE', {});
+  }
+}
+</script>
+
+<style lang="scss">
+@import '@port-of-mars/client/stylesheets/layouts/DefaultLayout.scss';
+</style>
