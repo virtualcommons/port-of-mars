2cf1a8eaa6450f09af4cc0c3fec3f85a12c452f2
chrstngyn <Christine.N.Nguyen@asu.edu>
2020-03-27 01:54:08 -0700

    feat(chat): add badge to indicate unread chat msgs

--- client/src/components/newGameDashboard/global/ChatPopup.vue
+++ client/src/components/newGameDashboard/global/ChatPopup.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="c-chatpopup" :style="position">
-    <button @click="toggle" class="toggle">
+    <BButton @click="toggle" class="toggle">
       <span>Chat</span>
+      <BBadge variant="dark">10<span class="sr-only">unread messages</span></BBadge>
       <font-awesome-icon
         v-if="!visible"
         :icon="['fas', 'caret-up']"
@@ -12,7 +13,7 @@
         :icon="['fas', 'caret-down']"
         size="lg"
       />
-    </button>
+    </BButton>
     <div class="wrapper">
       <NewChat />
     </div>
@@ -21,28 +22,27 @@
 
 <script lang="ts">
 import { Vue, Component, Prop } from 'vue-property-decorator';
+import { BBadge, BButton } from 'bootstrap-vue';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faCaretUp } from '@fortawesome/free-solid-svg-icons/faCaretUp';
 import { faCaretDown } from '@fortawesome/free-solid-svg-icons/faCaretDown';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import NewChat from '@port-of-mars/client/components/newGameDashboard/right/NewChat.vue';
-
 library.add(faCaretUp);
 library.add(faCaretDown);
 Vue.component('font-awesome-icon', FontAwesomeIcon);
-
 @Component({
   components: {
+    BBadge,
+    BButton,
     NewChat
   }
 })
 export default class ChatPopup extends Vue {
   private visible: boolean = false;
-
   private toggle() {
     this.visible = !this.visible;
   }
-
   get position() {
     return this.visible ? { bottom: '0rem' } : { bottom: '-45rem' };
   }
