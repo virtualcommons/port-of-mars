547c662e6662142105261d9fcd21006f0766ab2c
chrstngyn <Christine.N.Nguyen@asu.edu>
2020-03-27 08:44:01 -0700

    feat: add phase instructions

--- client/src/repo/instructions.ts
+++ client/src/repo/instructions.ts
@@ -1,9 +1,16 @@
 export const phaseInstructions = {
-  events: 'These are the instructions for the Events phase.',
-  invest: 'These are the instructions for the Investment phase.',
-  trade: 'These are the instructions for the Trade phase.',
-  purchase: 'These are the instructions for the Purchase phase.',
-  discard: 'These are the instructions for the Discard phase.',
+  events: 'If System Health < 65, 2 Events will occur; ' + 
+          'if System Health is lower than 35, 3 Events will occur. ' + 
+          'Some events will require you to take special actions in the Active Events section.',
+  invest: 'You have 10 timeblocks to spend each round unless otherwise instructed. ' + 
+          'You can spend these timeblocks on System Health or on Influence. Remember that you cannot ' +
+          'recycle timeblocks between rounds.',
+  trade: 'You will request and trade for resources that you need to purchase Accomplishment cards.' +
+          'You can chat with other players to discuss and coordinate trades prior to making a trade request.',
+  purchase: 'Accomplishments that you can purchase have an orange header and are moved to the top. You can ' +
+            'view your purchased accomplishments by clicking on your own player to reveal your player information and inventory. ',
+  discard: 'When you discard an accomplishment, you can get new accomplishment cards to replace any card you may not like. ' +
+           'You can discard accomplishments in the Discardable Accomplishments section.',
   victory: 'These are the instructions for the Victory phase.',
   defeat: 'These are the instructions for the Defeat phase.'
 };
