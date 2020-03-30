9d97219e6d3d62787a93b62b7d9ee25b869cb238
Calvin Pritchard <pritchard.calvin@gmail.com>
2020-03-30 08:28:47 -0700

    refactor: removed standalone quiz service funtions

--- server/src/routes/quiz.ts
+++ server/src/routes/quiz.ts
@@ -1,13 +1,7 @@
 import { NextFunction, Request, Response, Router } from 'express';
 import {
-  createQuizSubmission,
-  createQuestionResponse,
-  getDefaultQuiz,
-  findQuizSubmission,
-  setUserQuizCompletion,
-  checkQuestionResponse,
-  checkQuizCompletion
-} from '@port-of-mars/server/services/quiz';
+  getServices
+} from '@port-of-mars/server/services';
 import { User } from '@port-of-mars/server/entity/User';
 import { QuizSubmission } from '@port-of-mars/server/entity/QuizSubmission';
 import { settings } from '@port-of-mars/server/settings';
@@ -34,17 +28,18 @@ quizRouter.get(
   '/submission/:id',
   async (req: Request, res: Response, next: NextFunction) => {
     try {
+      const quizService = getServices().quiz;
       const user = checkUser(req, res);
       if (user) {
         // FIXME: should have an optional quizId parameter for multiple quiz types
         const id = parseInt(req.params.id);
-        let submission = await findQuizSubmission(id, { where: { userId: user.id }, relations: ['quiz', 'quiz.questions'] });
+        let submission = await quizService.findQuizSubmission(id, { where: { userId: user.id }, relations: ['quiz', 'quiz.questions'] });
         let quiz = submission?.quiz;
         let statusCode = 200;
         if (!submission) {
-          quiz = await getDefaultQuiz({ relations: ['questions'] });
+          quiz = await quizService.getDefaultQuiz({ relations: ['questions'] });
           logger.debug('created new submission after looking up id ', id);
-          submission = await createQuizSubmission(user.id, quiz.id);
+          submission = await quizService.createQuizSubmission(user.id, quiz.id);
           statusCode = 201;
         }
         res.status(statusCode).json({ submissionId: submission.id, quizQuestions: quiz!.questions });
@@ -59,13 +54,14 @@ quizRouter.post(
   '/submission',
   async (req: Request, res: Response, next: NextFunction) => {
     try {
+      const quizService = getServices().quiz;
       const user = checkUser(req, res);
       if (user) {
         const userId = user!.id;
-        const quiz = await getDefaultQuiz({ relations: ['questions'] });
+        const quiz = await quizService.getDefaultQuiz({ relations: ['questions'] });
         const quizId = quiz!.id;
         const quizQuestions = quiz.questions;
-        const submission = await createQuizSubmission(userId, quizId);
+        const submission = await quizService.createQuizSubmission(userId, quizId);
         const submissionId = submission.id;
         res.status(201).json({ submissionId, quizQuestions });
       }
@@ -80,6 +76,7 @@ quizRouter.post(
   '/submission/:submissionId/:questionId',
   async (req: Request, res: Response, next: NextFunction) => {
     try {
+      const quizService = getServices().quiz;
       const submissionId = parseInt(req.params.submissionId);
       if (isNaN(submissionId))
         return res
@@ -99,13 +96,13 @@ quizRouter.post(
         return res.status(400).json({ error: `Invalid quiz response ${req.body.answer}: should have been an integer.` });
       }
       // NOTE: Create Question Response
-      const questionResponse = await createQuestionResponse(
+      const questionResponse = await quizService.createQuestionResponse(
         questionId,
         submissionId,
         answer
       );
       // NOTE: Check Question Response
-      const correct = await checkQuestionResponse(questionResponse);
+      const correct = await quizService.checkQuestionResponse(questionResponse);
       // notify the client of the quiz response validation 
       res.status(200).json(correct);
     }
@@ -121,6 +118,7 @@ quizRouter.get(
   '/complete',
   async (req: Request, res: Response, next: NextFunction) => {
     try {
+      const quizService = getServices().quiz;
       // NOTE: Get User
       const user = req.user as User | undefined;
       if (!user)
@@ -131,10 +129,10 @@ quizRouter.get(
 
       // NOTE: Check Quiz Completion
       const userId = user.id;
-      const complete = await checkQuizCompletion(userId);
+      const complete = await quizService.checkQuizCompletion(userId);
 
       // NOTE: Set User Completion in DB
-      await setUserQuizCompletion(userId, complete);
+      await quizService.setUserQuizCompletion(userId, complete);
 
       // NOTE: Send Quiz Completion Status
       res.status(200).send(complete);
--- server/src/services/quiz.ts
+++ server/src/services/quiz.ts
@@ -43,11 +43,17 @@ export class QuizService {
 
   async getDefaultQuiz(opts?: Partial<{ relations: Array<string>, where: object }>): Promise<Quiz> {
     const DEFAULT_QUIZ_NAME = 'TutorialQuiz';
-    return await getQuizByName(DEFAULT_QUIZ_NAME, opts);
+    return await this.getQuizByName(DEFAULT_QUIZ_NAME, opts);
   }
 
   async getQuizById(id: number, opts?: Partial<{ relations: Array<string>, where: object }>) {
     return await this.em.getRepository(Quiz).findOneOrFail({id, ...opts});
+  }
+
+  async getQuizByName(name: string, opts?: Partial<{ relations: Array<string>, where: object }>): Promise<Quiz> {
+    return await getConnection()
+      .getRepository(Quiz)
+      .findOneOrFail({name, ...opts});
   }
 
   async getQuizQuestionsByQuizId(quizId: number): Promise<Array<Question> | undefined> {
@@ -86,7 +92,7 @@ export class QuizService {
   async checkQuestionResponse(questionResponse: QuestionResponse): Promise<boolean> {
     const answer = questionResponse.answer;
     const questionId = questionResponse.questionId;
-    const question = await findQuestion(questionId);
+    const question = await this.findQuestion(questionId);
     if (question === undefined) return false;
     return answer === question.correctAnswer;
   }
@@ -96,8 +102,8 @@ export class QuizService {
     quizId?: number
   ): Promise<boolean> {
 
-    let quiz: Quiz = (quizId) ? await getQuizById(quizId, {relations: ['questions']}) : await getDefaultQuiz();
-    const quizSubmission = await getRecentQuizSubmission(userId, quiz.id, {relations: ['responses']});
+    let quiz: Quiz = (quizId) ? await this.getQuizById(quizId, {relations: ['questions']}) : await this.getDefaultQuiz();
+    const quizSubmission = await this.getRecentQuizSubmission(userId, quiz.id, {relations: ['responses']});
     if (!quizSubmission?.responses) return false;
 
     const questions = quiz.questions;
@@ -135,106 +141,4 @@ export async function createQuestionResponse(
   return await getConnection()
     .getRepository(QuestionResponse)
     .save(questionResponse);
-}
-
-export async function createQuizSubmission(
-  userId: number,
-  quizId: number
-): Promise<QuizSubmission> {
-  const quizSubmission = new QuizSubmission();
-  quizSubmission.userId = userId;
-  quizSubmission.quizId = quizId;
-  return await getConnection()
-    .getRepository(QuizSubmission)
-    .save(quizSubmission);
-}
-
-export async function findQuizSubmission(id: number, opts?: Partial<{ relations: Array<string>, where: object }>): Promise<QuizSubmission | undefined> {
-  return getConnection().getRepository(QuizSubmission).findOne({id, ...opts});
-}
-
-// FUNCTIONS TO GET FROM DATABASE
-export async function getDefaultQuiz(opts?: Partial<{ relations: Array<string>, where: object }>): Promise<Quiz> {
-  const DEFAULT_QUIZ_NAME = 'TutorialQuiz';
-  return await getQuizByName(DEFAULT_QUIZ_NAME, opts);
-}
-
-export async function getQuizById(id: number, opts?: Partial<{ relations: Array<string>, where: object }>) {
-  return await getConnection().getRepository(Quiz).findOneOrFail({id, ...opts});
-}
-
-export async function getQuizByName(name: string, opts?: Partial<{ relations: Array<string>, where: object }>): Promise<Quiz> {
-  return await getConnection()
-    .getRepository(Quiz)
-    .findOneOrFail({name, ...opts});
-}
-
-export async function getRecentQuizSubmission(
-  userId: number,
-  quizId: number,
-  opts: Partial<{ relations: Array<string>, where: object, order: object }> = {}
-): Promise<QuizSubmission | undefined> {
-  opts = {order: {dateCreated: 'DESC'}, ...opts};
-  return await getConnection()
-    .getRepository(QuizSubmission)
-    .findOne({quizId, userId, ...opts});
-}
-
-// FUNCTIONS TO UPDATE DATABASE
-
-export async function setUserQuizCompletion(
-  userId: number,
-  complete: boolean
-): Promise<any> {
-  return await getConnection()
-    .createQueryBuilder()
-    .update(User)
-    .set({passedQuiz: complete})
-    .where('id = :id', {id: userId})
-    .execute();
-}
-
-// NOTE: FUNCTIONS TO CHECK FROM DATABASE
-
-export async function findQuestion(id: number): Promise<Question | undefined> {
-  return await getConnection().getRepository(Question).findOne({id});
-}
-
-export async function checkQuestionResponse(questionResponse: QuestionResponse): Promise<boolean> {
-  const answer = questionResponse.answer;
-  const questionId = questionResponse.questionId;
-  const question = await findQuestion(questionId);
-  if (question === undefined) return false;
-  return answer === question.correctAnswer;
-}
-
-export async function checkQuizCompletion(
-  userId: number,
-  quizId?: number
-): Promise<boolean> {
-
-  let quiz: Quiz = (quizId) ? await getQuizById(quizId, {relations: ['questions']}) : await getDefaultQuiz();
-  const quizSubmission = await getRecentQuizSubmission(userId, quiz.id, {relations: ['responses']});
-  if (!quizSubmission?.responses) return false;
-
-  const questions = quiz.questions;
-  const responses = quizSubmission.responses;
-
-  // check if responses are same length as number of questions
-  if (responses.length < questions.length) return false;
-  for (const question of questions) {
-    const correctResponses = _.filter(
-      responses,
-      (response: QuestionResponse) => {
-        return (
-          response.questionId === question.id &&
-          response.answer === question.correctAnswer
-        );
-      }
-    );
-    if (correctResponses.length === 0) {
-      return false;
-    }
-  }
-  return true;
 }
