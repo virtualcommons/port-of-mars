a0b85624cf213b73c74872b110a66351334faa43
Lorenzo Faivre <lfaivre@asu.edu>
2020-04-03 22:03:06 -0700

    fix: trade phase styling

--- client/src/components/game/phases/Trades.vue
+++ client/src/components/game/phases/Trades.vue
@@ -1,16 +1,28 @@
 <template>
-  <div class="trade-container tour-trade">
-    <div class="trade-list tour-trade-item">
-      <div class=header>
-        <div class="section-text">
-          <p>Active Trade List</p>
+  <div class="trade-container tour-trade container">
+    <div class="wrapper row">
+      <div class="trade-list tour-trade-item col-12">
+        <div class="content container">
+          <div class="header row">
+            <div class="title col-10">
+              <p>Active Trade List</p>
+            </div>
+            <div class="requesttradebutton col-2">
+              <button @click="handleOpenTradeRequest()">
+                Request a Trade
+              </button>
+            </div>
+          </div>
+          <div class="trades row">
+            <div class="wrapper">
+              <ActiveTrade
+                v-for="trade in trades"
+                v-bind="trade"
+                :key="trade.id"
+              />
+            </div>
+          </div>
         </div>
-        <button class="request-trade-button" @click="handleOpenTradeRequest()">Request a trade </button>
-      </div>
-
-      <div class="trades-wrapper">
-
-        <ActiveTrade v-for="trade in trades" v-bind="trade" :key="trade.id" />
       </div>
     </div>
   </div>
@@ -22,21 +34,21 @@ import ActiveTrade from './trade/ActiveTrade.vue';
 @Component({
   components: {
     ActiveTrade,
-  }
+  },
 })
 export default class Trades extends Vue {
   get trades() {
     const tradeSet = this.$tstore.state.tradeSet;
-    const trades = Object.keys(tradeSet).map(id => ({
+    const trades = Object.keys(tradeSet).map((id) => ({
       id,
       from: tradeSet[id].from,
-      to: tradeSet[id].to
+      to: tradeSet[id].to,
     }));
     console.log(trades);
     return trades;
   }
 
-  handleOpenTradeRequest(){
+  private handleOpenTradeRequest() {
     this.$tstore.commit('SET_TRADE_REQUEST_MODAL_VISIBILITY', true);
   }
 }
