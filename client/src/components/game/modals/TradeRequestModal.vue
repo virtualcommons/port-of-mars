78131bca9f0f61e561bca7021bb70dc6a9f1808b
Lorenzo Faivre <lfaivre@asu.edu>
2020-04-03 22:43:21 -0700

    temp: start restyling trade modal

--- client/src/components/game/modals/TradeRequestModal.vue
+++ client/src/components/game/modals/TradeRequestModal.vue
@@ -1,63 +1,88 @@
 <template>
-    <div class="trade-request-modal">
-        <div class="trade-request-container">
-            <div class="header">
-                <h5>Request A Trade </h5>
-            </div>
-            <TradeRequest class="actions"/>
+  <div class="c-traderequestmodal container">
+    <button
+      @click="handleExit"
+      type="button"
+      name="Close Button"
+      class="modal-close"
+    >
+      <font-awesome-icon
+        :icon="['fas', 'times']"
+        size="lg"
+        class="close-icon"
+      />
+    </button>
+    <div class="wrapper row">
+      <div class="traderequest col-8">
+        <div class="topbar">
+          <p class="title">Request a Trade</p>
+        </div>
+        <TradeRequest class="actions" />
+      </div>
+      <div class="activeaccomplishments col-4">
+        <div class="topbar">
+          <p class="title">Purchasable Accomplishments</p>
         </div>
 
-        <div class="active-accomplishments-container">
-            <div class="exit">
-                <button
-                    @click="handleExit"
-                    type="button"
-                    name="Close Button"
-                    class="exit-button"
-                >
-                    <font-awesome-icon
-                    :icon="['fas', 'times']"
-                    size="lg"
-                    class="close-icon"
-                    />
-                </button>
-            </div>
-
-            <div class="active-accomplishments-section">
-                <div class="header">
-                    <h5>Active Accomplishments</h5>
-                </div>
-
-                <div class="active-accomplishments">
-                    <ContainerAccomplishmentsGeneral 
-                    :accomplishmentSet="activeAccomplishments"
-                    :isVisible="true"/>
-                </div>
-            </div>
+        <div class="outer-wrapper">
+          <div class="wrapper">
+            <!-- <BarAccomplishment
+              v-for="accomplishment in purchasableAccomplishments"
+              :accomplishment="accomplishment"
+              :purchase="false"
+              :discard="false"
+              :key="accomplishment.label + Math.random()"
+            /> -->
+          </div>
         </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import TradeRequest from '@port-of-mars/client/components/game/phases/trade/TradeRequest.vue'
-import ContainerAccomplishmentsGeneral from '@port-of-mars/client/components/game/accomplishments/ContainerAccomplishmentsGeneral.vue';
+import { AccomplishmentData } from '@port-of-mars/shared/types';
+import TradeRequest from '@port-of-mars/client/components/game/phases/trade/TradeRequest.vue';
+// import ContainerAccomplishmentsGeneral from '@port-of-mars/client/components/game/accomplishments/ContainerAccomplishmentsGeneral.vue';
+// import BarAccomplishment from '@port-of-mars/client/components/newGameDashboard/global/cards/BarAccomplishment.vue';
+import { canPurchaseAccomplishment } from '@port-of-mars/shared/validation';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faTimes } from '@fortawesome/free-solid-svg-icons/faTimes';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+library.add(faTimes);
+Vue.component('font-awesome-icon', FontAwesomeIcon);
 
 @Component({
-    components:{
-        TradeRequest,
-        ContainerAccomplishmentsGeneral
-    }
+  components: {
+    TradeRequest,
+    // ContainerAccomplishmentsGeneral,
+  },
 })
-export default class TradeRequestModal extends Vue{
+export default class TradeRequestModal extends Vue {
+  get activeAccomplishments() {
+    return this.$tstore.getters.player.accomplishments.purchasable;
+  }
 
-    get activeAccomplishments(){
-        return this.$tstore.getters.player.accomplishments.purchasable;
-    }
+  get purchasableAccomplishments() {
+    return this.$store.getters.player.accomplishments.purchasable
+      .slice()
+      .sort((a: AccomplishmentData, b: AccomplishmentData) => {
+        return (
+          Number(
+            canPurchaseAccomplishment(b, this.$store.getters.player.inventory)
+          ) -
+          Number(
+            canPurchaseAccomplishment(a, this.$store.getters.player.inventory)
+          )
+        );
+      });
+  }
 
-    handleExit(){
-        this.$tstore.commit('SET_TRADE_REQUEST_MODAL_VISIBILITY', false);
-    }
+  private handleExit() {
+    this.$tstore.commit('SET_TRADE_REQUEST_MODAL_VISIBILITY', false);
+  }
 }
 </script>
 
