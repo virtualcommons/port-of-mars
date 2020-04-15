4b6bc8d25e95971c64bcd582b7cbf63bec02f6e9
Lorenzo Faivre <lfaivre@asu.edu>
2020-04-15 13:14:06 -0700

    refactor: trade phase container styling

--- client/src/components/game/phases/Trades.vue
+++ client/src/components/game/phases/Trades.vue
@@ -1,22 +1,24 @@
 <template>
-  <div class="trade-container container">
+  <div class="c-trades container">
     <div class="wrapper row">
-      <div class="trade-list tour-trade-list col-12">
-        <div class="content container">
-          <div class="header row">
-            <div class="title col-10">
-              <p>Active Trade List</p>
-            </div>
-            <div class="requesttradebutton col-2 tour-request-trade">
-              <button @click="handleOpenTradeRequest()">
-                Request a Trade
-              </button>
+      <div class="trade-list col-12 tour-trade-list">
+        <div class="topbar">
+          <div class="title-wrapper">
+            <div class="wrapper">
+              <p class="title">Active Trades</p>
             </div>
           </div>
-          <div class="trades row">
-            <div class="wrapper">
-              <ActiveTrade v-for="trade in trades" v-bind="trade" :key="trade.id" />
-            </div>
+          <button @click="handleOpenTradeRequest" class="tour-request-trade">
+            Request a Trade
+          </button>
+        </div>
+        <div class="outer-wrapper">
+          <div class="wrapper">
+            <ActiveTrade
+              v-for="trade in trades"
+              v-bind="trade"
+              :key="trade.id"
+            />
           </div>
         </div>
       </div>
