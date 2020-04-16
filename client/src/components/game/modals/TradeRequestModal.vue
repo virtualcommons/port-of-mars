eb9726b2acc5b9a98b49fc4e7fa031789db75d8e
Lorenzo Faivre <lfaivre@asu.edu>
2020-04-15 22:14:14 -0700

    temp: add view switcher to trade modal
    notes:
    
    - closes #410

--- client/src/components/game/modals/TradeRequestModal.vue
+++ client/src/components/game/modals/TradeRequestModal.vue
@@ -11,12 +11,34 @@
           </div>
         </div>
       </div>
-      <div class="cards col-4">
-        <div class="topbar">
-          <p class="title">Active Accomplishments</p>
+      <div class="side-view col-4">
+        <div class="buttons">
+          <button
+            @click="switchView('Inventory')"
+            :class="selectedView === 'Inventory' ? 'selected' : ''"
+          >
+            Inventory
+          </button>
+          <button
+            @click="switchView('Active Accomplishments')"
+            :class="selectedView === 'Active Accomplishments' ? 'selected' : ''"
+          >
+            Accomplishments
+          </button>
         </div>
-        <div class="outer-wrapper">
-          <div class="wrapper">
+        <div class="topbar">
+          <p class="title">{{ selectedView }}</p>
+        </div>
+        <div v-if="selectedView === 'Inventory'" class="inventory-wrapper">
+          <div class="inventory">
+            <Inventory :isSelf="true" :displaySystemHealth="false" />
+          </div>
+        </div>
+        <div
+          v-if="selectedView === 'Active Accomplishments'"
+          class="accomplishment-cards-wrapper"
+        >
+          <div class="accomplishment-cards">
             <AccomplishmentCard
               v-for="accomplishment in activeAccomplishments"
               :key="accomplishment.id"
@@ -34,18 +56,25 @@
 import { Component, Inject, Vue, Prop } from 'vue-property-decorator';
 import { TradeRequestModalData } from '@port-of-mars/client/types/modals';
 import TradeRequest from '@port-of-mars/client/components/game/phases/trade/TradeRequest.vue';
+import Inventory from '@port-of-mars/client/components/game/Inventory.vue';
 import AccomplishmentCard from '@port-of-mars/client/components/game/accomplishments/AccomplishmentCard.vue';
 import { TutorialAPI } from '../../../api/tutorial/request';
 
 @Component({
   components: {
     TradeRequest,
+    Inventory,
     AccomplishmentCard,
   },
 })
 export default class TradeRequestModal extends Vue {
   @Prop({}) private modalData!: TradeRequestModalData;
   @Inject() readonly api!: TutorialAPI;
+  private selectedView: string = 'Inventory';
+
+  private switchView(view: string) {
+    this.selectedView = view;
+  }
 
   get activeAccomplishments() {
     return this.$tstore.getters.player.accomplishments.purchasable;
