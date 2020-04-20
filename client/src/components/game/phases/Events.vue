c6dc37dd28e2008bbee14c4f3e0cb80469f69c96
Lorenzo Faivre <lfaivre@asu.edu>
2020-04-20 15:26:28 -0700

    fix: events container

--- client/src/components/game/phases/Events.vue
+++ client/src/components/game/phases/Events.vue
@@ -1,11 +1,25 @@
 <template>
   <div class="c-events container tour-event">
     <div class="wrapper row">
-      <div class="event-deck col-3 tour-event-deck">
-        <div class="topbar">
-          <p class="title">Event Deck</p>
+      <div
+        v-if="shouldShowAccomplishments"
+        class="event-deck col-4 tour-event-deck"
+      >
+        <div class="buttons">
+          <button
+            @click="switchView('Event Deck')"
+            :class="selectedView === 'Event Deck' ? 'selected' : ''"
+          >
+            Event Deck
+          </button>
+          <button
+            @click="switchView('Active Accomplishments')"
+            :class="selectedView === 'Active Accomplishments' ? 'selected' : ''"
+          >
+            Accomplishments
+          </button>
         </div>
-        <div class="outer-wrapper">
+        <div v-if="selectedView === 'Event Deck'" class="event-deck-wrapper">
           <div class="wrapper event-scroll">
             <EventCard
               class="event-container"
@@ -17,10 +31,38 @@
             />
           </div>
         </div>
+        <div
+          v-if="selectedView === 'Active Accomplishments'"
+          class="accomplishments-wrapper"
+        >
+          <div class="wrapper">
+            <AccomplishmentCard
+              class="cards"
+              v-for="accomplishment in accomplishmentCards"
+              :key="accomplishment.id"
+              :accomplishment="accomplishment"
+              :showDescription="false"
+            />
+          </div>
+        </div>
       </div>
-      <div class="active-events tour-active-events"
-        v-bind="{class: shouldShowAccomplishments ?
-                'col-6 active-events-mid' : 'col-9 active-events-right'}">
+      <div v-else class="event-deck col-4 tour-event-deck">
+        <div class="topbar">
+          <p class="title">Active Accomplishments</p>
+        </div>
+        <div class="accomplishments-wrapper">
+          <div class="wrapper">
+            <AccomplishmentCard
+              class="cards"
+              v-for="accomplishment in accomplishmentCards"
+              :key="accomplishment.id"
+              :accomplishment="accomplishment"
+              :showDescription="false"
+            />
+          </div>
+        </div>
+      </div>
+      <div class="active-events tour-active-events col-8">
         <div class="topbar">
           <p class="title">
             {{ eventTitle }}
@@ -29,23 +71,6 @@
         <div class="outer-wrapper">
           <div class="wrapper">
             <EventContainer :event="currentEvent" :key="eventNumber" />
-          </div>
-        </div>
-      </div>
-      <div class="active-accomplishments col-3"
-        v-if="shouldShowAccomplishments">
-        <div class="topbar">
-          <p class="title">Accomplishments</p>
-        </div>
-        <div class="outer-wrapper">
-          <div class="wrapper event-scroll">
-            <AccomplishmentCard
-              class="cards"
-              v-for="accomplishment in accomplishmentCards"
-              :key="accomplishment.id"
-              :accomplishment="accomplishment"
-              :showDescription="false"
-            />
           </div>
         </div>
       </div>
@@ -68,8 +93,31 @@ import { MarsEventData, Phase } from '@port-of-mars/shared/types';
   },
 })
 export default class Events extends Vue {
-  get eventsForTheRound() {
-    return this.$tstore.state.marsEvents;
+  private selectedView: string = 'Event Deck';
+
+  // NOTE :: LIFECYCLE HOOKS
+
+  updated(): any {
+    const elem = this.$el.querySelector('.event-scroll');
+    if (elem) {
+      elem!.scrollTop = elem!.scrollHeight;
+    }
+  }
+
+  // NOTE :: EVENT SPECIFIC
+
+  get currentEvent(): MarsEventData {
+    const current: MarsEventData = this.$store.getters.currentEvent;
+    return current;
+  }
+
+  get eventTitle() {
+    if (this.eventsProcessed && this.currentEvent) {
+      return `Active Event #${this.eventsProcessed + 1}: ${
+        this.currentEvent.name
+      }`;
+    }
+    return `Active Event: None`;
   }
 
   get eventsProcessed(): number {
@@ -81,11 +129,23 @@ export default class Events extends Vue {
     return eventNumber;
   }
 
-  get currentEvent(): MarsEventData {
-    const current: MarsEventData = this.$store.getters.currentEvent;
-    // const data = { name: current.name, visibility: true };
-    //this.$store.commit('SET_EVENT_VISIBILITY', data);
-    return current;
+  // NOTE :: VIEW HANDLING
+
+  private switchView(view: string) {
+    this.selectedView = view;
+  }
+
+  get shouldShowAccomplishments(): boolean {
+    return (
+      this.currentEvent.clientViewHandler == 'INFLUENCES_SELECT' ||
+      this.currentEvent.clientViewHandler == 'INFLUENCES_DRAW'
+    );
+  }
+
+  // NOTE :: EVENT CARDS
+
+  get eventsForTheRound() {
+    return this.$tstore.state.marsEvents;
   }
 
   private eventVisible(index: number) {
@@ -100,27 +160,10 @@ export default class Events extends Vue {
     );
   }
 
-  get eventTitle() {
-    if (this.eventsProcessed && this.currentEvent) {
-      return `Active Event #${this.eventsProcessed + 1}: ${
-        this.currentEvent.name
-      }`;
-    }
-    return `Active Event: None`;
-  }
-
-  private updated(): any {
-    const elem = this.$el.querySelector('.event-scroll');
-    elem!.scrollTop = elem!.scrollHeight;
-  }
+  // NOTE :: ACCOMPLISHMENT CARDS
 
   get accomplishmentCards(): any {
-      return this.$tstore.getters.player.accomplishments.purchasable;
-  }
-
-  get shouldShowAccomplishments(): boolean {
-    return this.currentEvent.clientViewHandler == 'INFLUENCES_SELECT' ||
-           this.currentEvent.clientViewHandler == 'INFLUENCES_DRAW';
+    return this.$tstore.getters.player.accomplishments.purchasable;
   }
 }
 </script>
