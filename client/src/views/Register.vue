17afc74dbf1cc312591b4eb2e5288c209a8dff41
Allen Lee <allen.lee@asu.edu>
2020-04-21 16:50:54 -0700

    fix: redirect to dashboard after registration

--- client/src/views/Register.vue
+++ client/src/views/Register.vue
@@ -9,60 +9,43 @@
         <div class="input-username">
           <label for="name">Name</label>
           <div class="input-wrapper">
-            <input
-              type="text"
-              id="name"
-              name="name"
-              v-model="name"
-            />
+            <input type="text" id="name" name="name" v-model="name" />
           </div>
         </div>
         <div class="input-username">
           <label for="email">Email</label>
           <div class="input-wrapper">
-            <input
-              type="email"
-              id="email"
-              name="email"
-              v-model="email"
-            />
+            <input type="email" id="email" name="email" v-model="email" />
           </div>
         </div>
         <div class="submit">
-          <input
-            :disabled="submitDisabled"
-            type="submit"
-            @click="register"
-            value="Register"
-          />
+          <input :disabled="submitDisabled" type="submit" @click="register" value="Register" />
         </div>
-        <p class="error" v-if="error">
-          {{ error }}
-        </p>
+        <p class="error" v-if="error">{{ error }}</p>
       </form>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { Vue, Component } from 'vue-property-decorator';
-import {GAME_PAGE} from "@port-of-mars/shared/routes";
+import { Vue, Component } from "vue-property-decorator";
+import { url } from "@port-of-mars/client/util";
+import { DASHBOARD_PAGE } from "@port-of-mars/shared/routes";
 
 @Component({})
 export default class Register extends Vue {
-  username: string = '';
-  name: string = '';
-  email: string = '';
-
-  error: string = '';
+  username = "";
+  name = "";
+  email = "";
+  error = "";
 
   async register(e: Event) {
     e.preventDefault();
     const fd = new FormData((e as any).target.form);
-    const data: any = { name: fd.get('name'), email: fd.get('email') };
+    const data: any = { name: fd.get("name"), email: fd.get("email") };
     const response = await this.$ajax.post(this.registerUrl, data);
     if (response.status === 200) {
-      this.$router.push(GAME_PAGE);
+      this.$router.push(DASHBOARD_PAGE);
     } else {
       this.error = await response.json();
     }
@@ -73,11 +56,11 @@ export default class Register extends Vue {
   }
 
   get registerUrl() {
-    return `${process.env.SERVER_URL_HTTP}/registration/register`;
+    return url('/registration/register');
   }
 }
 </script>
 
 <style lang="scss" scoped>
-@import '@port-of-mars/client/stylesheets/views/Login.scss';
+@import "@port-of-mars/client/stylesheets/views/Login.scss";
 </style>
