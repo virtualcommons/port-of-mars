f379941c161f0dbda8d01fb4d271f7cda00f1102
Lorenzo Faivre <lfaivre@asu.edu>
2020-04-22 13:01:26 -0700

    refactor: player dashboard built from components in preparation for api service connections
    notes:
    
    - fix: scroll overflow issue

--- client/src/views/Dashboard.vue
+++ client/src/views/Dashboard.vue
@@ -1,105 +1,102 @@
 <template>
-    <BRow class="player-dashboard">
-        <BCol class="left">
-            <!-- player stats -->
-            <div class="bottom">
-                <h2>Your Stats</h2>
-                <p>Games Played: {{ gamesPlayedCount }}</p>
-                <div class="player-stat-item" v-for="gs in stats.games">
-                    <p class="player-stat-item-header">Tournament Name: {{ gs.tournamentName }}</p>
-                    <p class="player-stat-item-header">{{ new Date(gs.time).toDateString() }}</p>
-                    <p>Round: {{ gs.round }}</p>
-                    <p>Winner: {{ gs.winner }}</p>
-                    <p>Points: {{ gs.points }}</p>
-                </div>
+  <div class="c-player-dashboard outer-container">
+    <div class="player-dashboard container">
+      <div class="title-wrapper row">
+        <div class="title col-12">
+          <h1>Port of Mars</h1>
+          <h2>Player Dashboard</h2>
+        </div>
+      </div>
+      <div class="content-wrapper row">
+        <div class="stats col-3">
+          <h2>Your Stats</h2>
+          <div class="information">
+            <p>Games Played: {{ gamesPlayedCount }}</p>
+          </div>
+          <div class="outer-wrapper">
+            <div class="wrapper">
+              <PlayerStatItem
+                v-for="playerStatItem in stats.games"
+                :playerStatItem="playerStatItem"
+                :key="playerStatItem.tournamentName + Math.random()"
+              />
             </div>
-        </BCol>
-
-        <!-- action items -->
-        <BCol class="middle">
-            <div class="top">
-                <h1>Port of Mars</h1>
-                <h2>Player Dashboard</h2>
+          </div>
+        </div>
+        <div class="action-items col-4">
+          <h2>Action Items</h2>
+          <div class="outer-wrapper">
+            <div class="wrapper">
+              <p v-if="loading">Action Items are loading...</p>
+              <ActionItemComponent
+                v-for="(actionItem, index) in actionItems"
+                :actionItem="actionItem"
+                :key="index + Math.random()"
+              />
             </div>
-
-            <div class="bottom">
-                <h2>Action Items</h2>
-                <div v-if="loading">
-                    <p>Action Items are loading...</p>
-                </div>
-
-                <div v-else>
-                    <div v-for="(action,index) in actionItems" :key="index" class="action-item">
-                        <p>{{action.description}}</p>
-                        <BButton squared class="button" variant="dark" :to="action.link.data" v-if="isInternal(action.link)">Go</BButton>
-                        <BButton squared class="button" variant="dark" :href="action.link.data" v-else>Go</BButton>
-                    </div>
-                </div>
+          </div>
+        </div>
+        <div class="upcoming-games col-3">
+          <h2>Upcoming Games</h2>
+          <div class="outer-wrapper">
+            <div class="wrapper">
+              <UpcomingGameItem
+                v-for="upcomingGame in upcomingGames"
+                :upcomingGame="upcomingGame"
+                :key="upcomingGame.tournamentName + Math.random()"
+              />
             </div>
-        </BCol>
-
-        <!-- upcoming games -->
-        <BCol class="right">
-            <h2>Upcoming Games</h2>
-            <div class="upcoming-game-item" v-for="game in upcomingGames">
-                <p>{{ toTimeString(game.time) }}</p>
-                <BButton squared class="button" variant="dark" disabled><router-link :to="joinLink">Join</router-link></BButton>
-            </div>
-        </BCol>
-    </BRow>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
 import { Vue, Component, Mixins } from 'vue-property-decorator';
-import { BRow, BCol, BButton, BLink } from 'bootstrap-vue';
-import {DashboardAPI} from '@port-of-mars/client/api/dashboard/request';
-import {ActionItem, DashboardData, GameMeta} from '@port-of-mars/shared/types';
-import {LOBBY_NAME} from "@port-of-mars/shared/lobby";
-import {LOBBY_PAGE} from "@port-of-mars/shared/routes";
+import ActionItemComponent from '@port-of-mars/client/components/dashboard/ActionItem.vue';
+import UpcomingGameItem from '@port-of-mars/client/components/dashboard/UpcomingGameItem.vue';
+import PlayerStatItem from '@port-of-mars/client/components/dashboard/PlayerStatItem.vue';
+import { DashboardAPI } from '@port-of-mars/client/api/dashboard/request';
+import {
+  ActionItem,
+  DashboardData,
+  GameMeta,
+} from '@port-of-mars/shared/types';
 
 @Component({
-    components: {
-        BButton,
-        BLink,
-        BRow,
-        BCol
-    },
+  components: {
+    ActionItemComponent,
+    UpcomingGameItem,
+    PlayerStatItem,
+  },
 })
 export default class PlayerDashboard extends Mixins(Vue, DashboardAPI) {
-    private visible = false;
-    private loading = true;
-    private actionItems:Array<ActionItem> = [];
-    private stats: DashboardData['stats'] = {games: []};
-    private upcomingGames: Array<GameMeta> = [];
+  private loading: boolean = true;
+  private actionItems: Array<ActionItem> = [];
+  private stats: DashboardData['stats'] = { games: [] };
+  private upcomingGames: Array<GameMeta> = [];
 
-    async mounted(){
-        await this.initialize();
-    }
+  async mounted() {
+    await this.initialize();
+  }
 
-    async initialize() {
-      const data = await this.getData();
-      console.log(data);
-      this.actionItems = data.actionItems;
-      this.stats.games.splice(0, this.stats.games.length, ...data.stats.games);
-      this.upcomingGames.splice(0, this.upcomingGames.length, ...data.upcomingGames);
-      this.loading = false;
-    }
+  async initialize() {
+    const data = await this.getData();
+    this.actionItems = data.actionItems;
+    this.stats.games.splice(0, this.stats.games.length, ...data.stats.games);
+    this.upcomingGames.splice(
+      0,
+      this.upcomingGames.length,
+      ...data.upcomingGames
+    );
+    this.loading = false;
+  }
 
-    get joinLink() {
-      return { name: LOBBY_PAGE };
-    }
-
-    get gamesPlayedCount() {
-      return this.stats.games.length;
-    }
-
-    isInternal(link: ActionItem['link']) {
-      return link.kind === 'internal';
-    }
-
-    toTimeString(unixtimestamp: number): string {
-      return new Date(unixtimestamp).toLocaleTimeString()
-    }
+  get gamesPlayedCount() {
+    return this.stats.games.length;
+  }
 }
 </script>
 
