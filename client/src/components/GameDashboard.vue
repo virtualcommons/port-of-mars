621252e7c5b3c4af9d72004e0eb43e043a3af414
Lorenzo Faivre <lfaivre@asu.edu>
2020-04-29 14:20:04 -0700

    refactor: move ModalController to GameboardContainer component

--- client/src/components/GameDashboard.vue
+++ client/src/components/GameDashboard.vue
@@ -1,8 +1,5 @@
 <template>
   <div class="c-game-dashboard">
-    <ModalController
-      v-if="gamePhase !== phase.defeat && gamePhase !== phase.victory"
-    />
     <GameboardContainer
       v-if="gamePhase !== phase.defeat && gamePhase !== phase.victory"
     />
@@ -13,7 +10,6 @@
 
 <script lang="ts">
 import { Vue, Component } from 'vue-property-decorator';
-import ModalController from '@port-of-mars/client/components/game/modals/ModalController.vue';
 import GameboardContainer from '@port-of-mars/client/components/root/GameboardContainer.vue';
 import ContainerVictory from '@port-of-mars/client/components/root/ContainerVictory.vue';
 import ContainerDefeat from '@port-of-mars/client/components/root/ContainerDefeat.vue';
@@ -21,7 +17,6 @@ import { Phase } from '@port-of-mars/shared/types';
 
 @Component({
   components: {
-    ModalController,
     GameboardContainer,
     ContainerVictory,
     ContainerDefeat,
