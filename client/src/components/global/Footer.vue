c81a9d9d6c8b275e799e9a0d836aaeac240ac070
chrstngyn <Christine.N.Nguyen@asu.edu>
2020-05-13 14:50:45 -0700

    style(footer): fix positioning of items in footer
    resolve problem of content breaking to next line
    use bootstrap flexbox utilities: flex and automargins for aligning flex items

--- client/src/components/global/Footer.vue
+++ client/src/components/global/Footer.vue
@@ -1,66 +1,65 @@
 <template>
   <div class="c-footer d-flex flex-row">
-    <b-button pill class="footer-button" @click="toggle" :style="button">
+    <b-button :style="button" @click="toggle" class="footer-button" pill>
       <font-awesome-icon
-        v-if="!footer"
         :icon="['fas', 'chevron-circle-up']"
         size="lg"
+        v-if="!footer"
 
       />
       <font-awesome-icon
-        v-if="footer"
         :icon="['fas', 'chevron-circle-down']"
         size="lg"
+        v-if="footer"
       />
     </b-button>
 
-    <footer class="footer d-flex flex-row p-2" :style="show">
+    <footer :style="show" class="footer d-flex p-2">
       <div class="p-2 footer-copyright">
-        &copy; 2020 <a target='_blank' href='https://www.azregents.edu/'>Arizona Board of Regents</a>
+        &copy; 2020 <a href='https://www.azregents.edu/' target='_blank'>Arizona Board of
+        Regents</a>
       </div>
-      <div class="p-2 footer-build">
+      <div class="p-2 mr-auto footer-build">
         Build: {{ buildId }}
       </div>
-      <div class="links d-flex">
-        <div class="p-2 footer-github">
-          <a href="https://github.com/virtualcommons/port-of-mars/" target="_blank">
-            <font-awesome-icon
-              :icon="['fab', 'github']"
-              size="lg"
-            />
-          </a>
-        </div>
-        <div class="p-2 footer-mail">
-          <a href="mailto:portmars@asu.edu">
-            <font-awesome-icon
-              :icon="['fas', 'envelope']"
-              size="lg"
-            />
-          </a>
-        </div>
-        <div class="p-2 footer-license">
-          <a href="https://github.com/virtualcommons/port-of-mars/blob/master/LICENSE" target="_blank">
-            GNU AGPL v3.0
-          </a>
-        </div>
+      <div class="p-2 footer-github">
+        <a href="https://github.com/virtualcommons/port-of-mars/" target="_blank">
+          <font-awesome-icon
+            :icon="['fab', 'github']"
+            size="lg"
+          />
+        </a>
+      </div>
+      <div class="p-2 footer-mail">
+        <a href="mailto:portmars@asu.edu">
+          <font-awesome-icon
+            :icon="['fas', 'envelope']"
+            size="lg"
+          />
+        </a>
+      </div>
+      <div class="p-2 footer-license">
+        <a href="https://github.com/virtualcommons/port-of-mars/blob/master/LICENSE"
+           target="_blank">
+          GNU AGPL v3.0
+        </a>
       </div>
     </footer>
   </div>
 </template>
 
 <script lang="ts">
-  import { Vue, Component } from 'vue-property-decorator';
+  import {Component, Vue} from 'vue-property-decorator';
   import BootstrapVue from 'bootstrap-vue';
 
-  import { getBuildId } from '@port-of-mars/client/settings';
-
+  import {getBuildId} from '@port-of-mars/client/settings';
   // FontAwesome icons
-  import { library } from '@fortawesome/fontawesome-svg-core';
-  import { faGithub } from '@fortawesome/free-brands-svg-icons/faGithub';
-  import { faEnvelope} from "@fortawesome/free-solid-svg-icons";
-  import { faChevronCircleUp } from '@fortawesome/free-solid-svg-icons/faChevronCircleUp';
-  import { faChevronCircleDown } from '@fortawesome/free-solid-svg-icons/faChevronCircleDown';
-  import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+  import {library} from '@fortawesome/fontawesome-svg-core';
+  import {faGithub} from '@fortawesome/free-brands-svg-icons/faGithub';
+  import {faEnvelope} from "@fortawesome/free-solid-svg-icons";
+  import {faChevronCircleUp} from '@fortawesome/free-solid-svg-icons/faChevronCircleUp';
+  import {faChevronCircleDown} from '@fortawesome/free-solid-svg-icons/faChevronCircleDown';
+  import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome';
 
   library.add(faChevronCircleUp, faChevronCircleDown, faGithub, faEnvelope);
 
@@ -71,14 +70,6 @@
   export default class Footer extends Vue {
     footer = false;
     buildId = '';
-
-    async mounted() {
-      this.buildId = await getBuildId();
-    }
-
-    private toggle() {
-      this.footer = !this.footer;
-    }
 
     get show() {
       return {
@@ -91,9 +82,17 @@
         bottom: this.footer ? '10rem' : '1rem'
       };
     }
+
+    async mounted() {
+      this.buildId = await getBuildId();
+    }
+
+    private toggle() {
+      this.footer = !this.footer;
+    }
   }
 </script>
 
 <style lang="scss" scoped>
-@import '@port-of-mars/client/stylesheets/global/Footer.scss';
+  @import '@port-of-mars/client/stylesheets/global/Footer.scss';
 </style>
