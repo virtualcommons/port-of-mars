1d4dac1f529caa8aa0f5d581c899bef22ed0a56d
chrstngyn <Christine.N.Nguyen@asu.edu>
2020-05-21 11:16:11 -0700

    style(victory): fix css issue
    winners now display

--- client/src/components/root/Victory.vue
+++ client/src/components/root/Victory.vue
@@ -1,56 +1,63 @@
 <template>
-  <div class="c-gameview-victory container">
-    <div class="title-wrapper row">
-      <div class="title col-12">
-        <h1>Port of Mars</h1>
-        <h2 class="animated pulse slower infinite">Winners</h2>
-        <div v-for="winner in winners" class="winner">
-          <p>{{ winner }}</p>
+  <div class="victory">
+      <!-- TITLE -->
+      <div class="title-wrapper">
+        <div class="title col-12">
+          <h1>Port of Mars</h1>
+          <h2>Winners</h2>
+          <div v-for="winner in winners" class="d-flex flex-row">
+            <p>{{ winner }}</p>
+          </div>
         </div>
       </div>
-    </div>
-    <div class="content-wrapper row">
-      <div class="content col-12">
-        <h2>Despite all doubts, you have done it. Thanks to you future generations
-          can flourish on planet Mars.</h2>
+
+      <!-- SUBTITLE -->
+      <div class="content-wrapper">
+        <div class="content">
+          <h2>Despite all doubts, you have successfully established an extraterrestrial society. Thanks to you, future generations
+            can flourish on planet Mars.</h2>
+        </div>
       </div>
-    </div>
-    <div class="marslog-wrapper row">
-      <div class="marslog col-6">
-        <div class="wrapper">
-          <div
-            :key="log.timestamp + Math.random()"
-            :style="{ backgroundColor: categoryColorMap.get(log.category) }"
-            class="message"
-            v-for="(log, index) in logs"
-          >
-            <p class="category">{{ log.category }}</p>
-            <p class="final-log" v-if="index === 0">Final Log</p>
-            <p class="list" v-if="index !== 0">
+
+      <!-- MARS LOG -->
+      <div class="marslog-wrapper">
+        <div class="marslog col-6">
+          <div class="wrapper">
+            <div
+              :key="log.timestamp + Math.random()"
+              :style="{ backgroundColor: categoryColorMap.get(log.category) }"
+              class="message"
+              v-for="(log, index) in logs"
+            >
+              <p class="category">{{ log.category }}</p>
+              <p class="final-log" v-if="index === 0">Final Log</p>
+              <p class="list" v-if="index !== 0">
               <span>
                 Log: {{ logs.length - index }} / {{ logs.length }}
               </span>
-            </p>
-            <p class="content">{{ log.content }}</p>
-            <p class="time">
-              <span>[ </span>{{ logTime(log.timestamp) }}<span> ]</span>
-            </p>
-            <div class="round row" v-if="delineateRound(index, logs)"><b>Round {{ log.round - 1
-              }}</b></div>
-          </div>
-        </div> <!--end wrapper-->
-      </div>
-    </div>
-    <div class="footnote-wrapper row">
-      <div class="footnote col-12">
-        <h2>Thank you for playing.</h2>
-        <div class="buttons">
-          <router-link :to="'dashboard'">
-            <button class="exit">Exit</button>
-          </router-link>
+              </p>
+              <p class="content">{{ log.content }}</p>
+              <p class="time">
+                <span>[ </span>{{ logTime(log.timestamp) }}<span> ]</span>
+              </p>
+              <div class="round" v-if="delineateRound(index, logs)"><b>Round {{ log.round - 1
+                }}</b></div>
+            </div>
+          </div> <!--end wrapper-->
         </div>
       </div>
-    </div>
+
+      <!-- FOOTER -->
+      <div class="footnote-wrapper">
+        <div class="footnote col-12">
+          <h2>Thank you for playing.</h2>
+          <div class="buttons">
+            <router-link :to="'dashboard'">
+              <button class="exit">Exit</button>
+            </router-link>
+          </div>
+        </div>
+      </div>
   </div>
 </template>
 
