d3ca0aed40f6fd9453844a6646c7b5bb8b306ffd
chrstngyn <Christine.N.Nguyen@asu.edu>
2020-07-16 00:32:27 -0700

    refactor(trade): change trade filtering checkboxes --> single switch
    toggle local player's trades vs all trades with switch
    simplify filtering logic by using boolean value (checked) to determine how trades should be filtered

--- client/src/components/game/phases/Trades.vue
+++ client/src/components/game/phases/Trades.vue
@@ -6,20 +6,16 @@
           <div class="title-wrapper">
             <div class="wrapper">
               <p class="title">Active Trades</p>
-              <div class="filter-options">
-                <input :value="0" @change="checked(0)" id="yourTrades" type="checkbox"
-                       v-model="activeFilters">
-                <label class="filter-option" for="yourTrades">Your Trades</label>
-                <input :value="1" @change="checked(1)" id="requestedTrades" type="checkbox"
-                       v-model="activeFilters">
-                <label class="filter-option" for="requestedTrades">Trades Sent to You</label>
-                <input :value="-1" @change="checked(-1)" id="allTrades" type="checkbox"
-                       v-model="activeFilters">
-                <label class="filter-option" for="allTrades">All Active Trades</label>
-              </div>
+              <b-form-checkbox
+                v-model="checked"
+                switch
+              >
+                <span v-if="checked">Your Trades</span>
+                <span v-else>All Trades</span>
+              </b-form-checkbox>
             </div>
           </div>
-          <button @click="handleOpenTradeRequest" class="tour-request-trade"
+          <button @click="requestTrade" class="tour-request-trade"
                   :disabled="playerReady"
           >
             Request a Trade
@@ -54,25 +50,29 @@
     @Inject()
     readonly api!: TutorialAPI;
 
-    //the array of active filters
-    //1-> show trades I've received
-    //0-> show trades I've sent
-    //-1-> show all trades
+    private checked: boolean = false;
 
-    //NOTE: 1 and 0 can be active at the same time, but -1 cannot.
-    private activeFilters: Array<number> = [-1];
-
-    get disabled(): boolean {
+    /**
+     * If player is ready, disabled is true.
+     */
+    get disabled() {
       return this.playerReady;
     }
 
+    /**
+     * Check if player is ready.
+     */
     get playerReady() {
       return this.$tstore.getters.player.ready;
     }
 
+    /**
+     * Trades
+     */
     get trades() {
       const tradeSet = this.$tstore.state.tradeSet;
-      //map the trades to something we can use
+
+      // map the trades to something we can use
       const trades = Object.keys(tradeSet).map(id => ({
         id,
         sender: tradeSet[id].sender,
@@ -81,54 +81,41 @@
       }));
 
       return trades
-        .filter((trade) => {/* Filter the trades by active filters. if -1 is in the list, we want to show all trades. */
-          return this.activeFilters.indexOf(-1) != -1 || this.activeFilters.indexOf(this.associatedWithTrade(trade)) != -1;
+        // filter the trades by active filter (show all trades vs local player's trades)
+        .filter((trade) => {
+          return !this.checked || this.associatedWithTrade(trade) != -1;
         })
-        .sort((a, b) => {/* Sort the trades by association trades sent to you will be at the top, followed by trades you've sent. */
+
+        // sort the trades by association trades sent to local player will be at the top,
+        // followed by trades that local player has sent
+        .sort((a, b) => {
           return this.associatedWithTrade(b) - this.associatedWithTrade(a);
         });
     }
 
+    /**
+     * The role of the local player.
+     */
     get myRole() {
       return this.$tstore.getters.player.role;
     }
 
-    //this checks each trade to determine a player's relation to it.
-    //1 if the player is reciving the trade,
-    //0 if the player sent the trade
-    //-1 if the player has no interaction with trade
-    associatedWithTrade(trade: any) {
+    /**
+     * Determine local player's relation to each trade (sender, recipient, N/A).
+     * @param trade A trade.
+     *
+     * 1 : local player is recipient of trade
+     * 0 : local player is sender of trade
+     * -1: local player is not involved in trade
+     */
+    private associatedWithTrade(trade: any): number {
       return trade.recipient.role == this.myRole ? 1 : trade.sender.role == this.myRole ? 0 : -1;
     }
 
-    //this is some extra validation on the checkboxes.
-    /*we don't want to be able to select 'show all trades'
-    and either 'your trades' or 'trades sent by you' at the same time.
-    */
-
-    //if there are no options selected, we default to all trades.
-    async checked(value: number) {
-      //finding if no filter is active
-      const noFilter = this.activeFilters.indexOf(-1);
-
-      //if 'show all trades' is active and the input value is not -1, we want to remove
-      //'show all trades' from the list of active filters.
-      if (noFilter != -1 && value != -1) {
-        this.activeFilters.splice(noFilter, 1);
-        return;
-      }
-
-      //if 'show all trades' was selected OR no options are selected,
-      //turn on 'show all trades'
-      if (value == -1 || this.activeFilters.length < 1) {
-        await this.$nextTick();
-        this.activeFilters = [-1];
-        return;
-      }
-    }
-
-
-    private handleOpenTradeRequest() {
+    /**
+     * Open trade request modal when player wants to initiate a trade.
+     */
+    private requestTrade() {
       this.api.setModalVisible({type: 'TradeRequestModal', data: {}});
     }
   }
