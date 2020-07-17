bd6cc2dd16ca66cab004a4e072e8e6036cc5951f
Calvin Pritchard <pritchard.calvin@gmail.com>
2020-07-17 14:09:32 -0700

    feat: added trade and accomplishment purchase tables

--- client/src/components/game/phases/NewRound.vue
+++ client/src/components/game/phases/NewRound.vue
@@ -3,23 +3,37 @@
     <b-row class="report">
       <b-col fluid cols="8" class="system-health tour-contribute">
         <h1 class="m-4">System Health Report</h1>
-        <h2>Upcoming System Health: <b-badge :variant="systemHealthBadgeVariant">{{ nextRoundSystemHealth }}</b-badge></h2>
+        <h2>Upcoming System Health:
+          <b-badge :variant="systemHealthBadgeVariant">{{ nextRoundSystemHealth }}</b-badge>
+        </h2>
         <div v-if="isFirstRound">
-          Welcome to Mars! Each round, your System Health degrades by <b>{{ systemHealthMaintenanceCost }}</b> due to standard wear and tear.
-          Your System Health at the start of this round is <code>{{ nextRoundSystemHealth }}</code>.
+          Welcome to Mars! Each round, your System Health degrades by <b>{{
+          systemHealthMaintenanceCost }}</b> due to standard wear and tear.
+          Your System Health at the start of this round is <code>{{ nextRoundSystemHealth
+          }}</code>.
         </div>
         <div v-else>
           <p>
-          In the previous round you invested <b>{{ systemHealthContribution }}</b> and the rest of your group invested
-          <b>{{ groupContributions }}</b> in System Health for a total of {{ totalSystemHealthContributions }}.
-           Your group's average investment was {{ averageContribution }}.
+            In the previous round you invested <b>{{ systemHealthContribution }}</b> and the rest
+            of your group invested
+            <b>{{ groupContributions }}</b> in System Health for a total of {{
+            totalSystemHealthContributions }}.
+            Your group's average investment was {{ averageContribution }}.
           </p>
           <p>
-          {{ previousRoundSummary }}
+            {{ previousRoundSummary }}
           </p>
         </div>
-        <b-table responsive sticky-header small dark bordered striped :items="tabularContributions" :fields="tabularContributionFields">
-        </b-table>
+        <div class="summary-tables overflow-auto">
+          <b-table responsive sticky-header small dark bordered striped
+                   :items="tabularContributions" :fields="tabularContributionFields">
+          </b-table>
+          <b-table responsive sticky-header small dark bordered striped :items="purchases" v-if="purchases.length > 0"
+                   :fields="purchaseFields">
+          </b-table>
+          <b-table responsive sticky-header small dark bordered striped :items="trades" v-if="trades.length > 0">
+          </b-table>
+        </div>
       </b-col>
       <b-col fluid cols="4" class="events m-auto tour-report-hint">
         <h4>If System Health drops below 65, your group will encounter <b>two</b> Events.</h4>
@@ -30,21 +44,19 @@
 </template>
 
 <script lang="ts">
-  import { Vue } from 'vue-property-decorator';
-  import { PlayerClientData } from '@port-of-mars/shared/game/client/state';
-  import { SYSTEM_HEALTH_MAINTENANCE_COST } from '@port-of-mars/shared/settings';
+  import {Vue} from 'vue-property-decorator';
+  import {PlayerClientData} from '@port-of-mars/shared/game/client/state';
   import _ from 'lodash';
+  import {RESOURCES, TradeAmountData} from "@port-of-mars/shared/types";
 
   export default class NewRound extends Vue {
 
     get systemHealthBadgeVariant() {
       if (this.systemHealth >= 65) {
         return "success";
-      }
-      else if (this.systemHealth >= 35) {
+      } else if (this.systemHealth >= 35) {
         return "warning";
-      }
-      else {
+      } else {
         return "danger";
       }
     }
@@ -66,7 +78,7 @@
     }
 
     get nextRoundSystemHealth() {
-       return _.clamp(this.systemHealth + this.totalSystemHealthContributions + this.systemHealthMaintenanceCost, 0, 100);
+      return _.clamp(this.systemHealth + this.totalSystemHealthContributions + this.systemHealthMaintenanceCost, 0, 100);
     }
 
     get isFirstRound() {
@@ -81,8 +93,35 @@
       return this.$tstore.getters.systemHealthContribution;
     }
 
+    get purchaseFields() {
+      return [{key: 'name', label: 'Accomplishment'}, {key: 'victoryPoints'}]
+    }
+
+    get purchases() {
+      return this.$tstore.state.roundIntroduction.accomplishmentPurchases;
+    }
+
+    getTradeAmount(tradeAmount: TradeAmountData) {
+      const resources = [];
+      for (const r of RESOURCES) {
+        if (tradeAmount.resourceAmount[r] !== 0) {
+          resources.push(`${r} ${tradeAmount.resourceAmount[r]}`);
+        }
+      }
+      return resources
+
+    }
+
+    get trades() {
+      return this.$tstore.state.roundIntroduction.completedTrades.map(t => {
+        const sender = this.getTradeAmount(t.sender)
+        const recipient =  this.getTradeAmount(t.recipient);
+        return {description: `${t.sender.role} traded ${sender.join(', ')} for ${recipient.join(', ')} with ${t.recipient.role}`}
+      });
+    }
+
     get tabularContributionFields() {
-      return [{key: 'label'}, {key: 'role'}, {key: 'value', class: 'text-md-right'}];
+      return [{key: 'label', label: 'Description'}, {key: 'role'}, {key: 'value', label: 'System Health', class: 'text-md-right'}];
     }
 
     get tabularContributions() {
@@ -91,7 +130,12 @@
         {label: 'Group Contributions', role: 'System', value: this.totalSystemHealthContributions},
         ...this.$tstore.getters.purchaseSystemHealth,
         {label: 'Wear and Tear', role: 'System', value: this.systemHealthMaintenanceCost},
-        {label: 'Upcoming System Health', role: 'System', value: this.nextRoundSystemHealth, _rowVariant: this.systemHealthBadgeVariant}
+        {
+          label: 'Upcoming System Health',
+          role: 'System',
+          value: this.nextRoundSystemHealth,
+          _rowVariant: this.systemHealthBadgeVariant
+        }
       ];
       const isUnderAudit = this.$tstore.getters.isUnderAudit;
       if (isUnderAudit) {
@@ -105,10 +149,14 @@
       return items;
     }
 
-    otherPlayerContributions(): Array<{label:string, role: string, value:number}> {
+    otherPlayerContributions(): Array<{ label: string, role: string, value: number }> {
       const contributions = [];
       for (const [role, player] of Object.entries(this.$tstore.getters.otherPlayers)) {
-        contributions.push({label: 'System Health', role, value: (player as PlayerClientData).systemHealthChanges.investment});
+        contributions.push({
+          label: 'System Health',
+          role,
+          value: (player as PlayerClientData).systemHealthChanges.investment
+        });
       }
       return contributions;
     }
@@ -124,5 +172,5 @@
 </script>
 
 <style lang="scss" scoped>
-@import '@port-of-mars/client/stylesheets/game/phases/NewRound.scss';
+  @import '@port-of-mars/client/stylesheets/game/phases/NewRound.scss';
 </style>
