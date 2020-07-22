182f1ae81a956d5d26ee075aecc498d0538a02e8
chrstngyn <Christine.N.Nguyen@asu.edu>
2020-07-22 12:29:21 -0700

    style: bot takeover warning
    add color and styling to warning modal
    prevent close on clicking backdrop or keypress esc
    vertically center warning
    
    close #564

--- client/src/components/root/GameboardContainer.vue
+++ client/src/components/root/GameboardContainer.vue
@@ -1,64 +1,69 @@
 <template>
   <div class="c-gameboard-container container">
-    <ModalController />
-    <ProfileMenu />
+    <ModalController/>
+    <ProfileMenu/>
     <div class="system-health row">
-      <SystemHealth />
+      <SystemHealth/>
     </div>
     <div class="gameboard row">
       <div class="left col-9">
         <div class="hud row">
-          <HUD />
+          <HUD/>
         </div>
         <div class="phase-switcher row">
-          <PhaseSwitcher />
+          <PhaseSwitcher/>
         </div>
       </div>
       <div class="right col-3">
-        <ChatMarsLog />
+        <ChatMarsLog/>
       </div>
     </div>
-    <b-modal title="Timeout Warning: Impending Bot Takeover" hide-header-close v-model="botWarning">
-      <p>You have been inactive for at least four minutes. After five minutes a bot will takeover your player.</p>
-      <b-button block @click="resetBotWarning" class="btn-primary">OK</b-button>
+    <b-modal body-bg-variant="dark" body-text-variant="warning" centered no-close-on-backdrop no-close-on-esc no-enforce-focus
+             header-bg-variant="dark" header-border-variant="dark" header-text-variant="warning" hide-footer
+             hide-header-close title="Timeout Warning: Impending Bot Takeover" v-model="botWarning"
+             variant="outline-warning">
+      <p>You have been inactive for at least four minutes. After five minutes a bot will takeover
+        your player.</p>
+      <b-button @click="resetBotWarning" block variant="warning">OK</b-button>
     </b-modal>
   </div>
 </template>
 
 <script lang="ts">
-  import {Vue, Component, Inject} from 'vue-property-decorator';
-import ModalController from '@port-of-mars/client/components/game/modals/ModalController.vue';
-import HUD from '@port-of-mars/client/components/game/HUD.vue';
-import PhaseSwitcher from '@port-of-mars/client/components/game/PhaseSwitcher.vue';
-import ProfileMenu from '@port-of-mars/client/components/game/static/popups/ProfileMenu.vue';
-import SystemHealth from '@port-of-mars/client/components/game/static/systemhealth/SystemHealth.vue';
-import ChatMarsLog from '@port-of-mars/client/components/game/ChatMarsLog.vue';
+  import {Component, Inject, Vue} from 'vue-property-decorator';
+  import ModalController from '@port-of-mars/client/components/game/modals/ModalController.vue';
+  import HUD from '@port-of-mars/client/components/game/HUD.vue';
+  import PhaseSwitcher from '@port-of-mars/client/components/game/PhaseSwitcher.vue';
+  import ProfileMenu from '@port-of-mars/client/components/game/static/popups/ProfileMenu.vue';
+  import SystemHealth
+    from '@port-of-mars/client/components/game/static/systemhealth/SystemHealth.vue';
+  import ChatMarsLog from '@port-of-mars/client/components/game/ChatMarsLog.vue';
   import {GameRequestAPI} from "@port-of-mars/client/api/game/request";
 
-@Component({
-  components: {
-    ModalController,
-    HUD,
-    PhaseSwitcher,
-    ProfileMenu,
-    SystemHealth,
-    ChatMarsLog,
-  },
-})
-export default class GameboardContainer extends Vue {
-  @Inject()
-  api!: GameRequestAPI;
+  @Component({
+    components: {
+      ModalController,
+      HUD,
+      PhaseSwitcher,
+      ProfileMenu,
+      SystemHealth,
+      ChatMarsLog,
+    },
+  })
+  export default class GameboardContainer extends Vue {
+    @Inject()
+    api!: GameRequestAPI;
 
-  get botWarning() {
-    return this.$tstore.getters.player.botWarning;
-  }
+    get botWarning() {
+      return this.$tstore.getters.player.botWarning;
+    }
 
-  resetBotWarning() {
-    this.api.resetBotWarning();
+    resetBotWarning() {
+      this.api.resetBotWarning();
+    }
   }
-}
 </script>
 
 <style lang="scss" scoped>
-@import '@port-of-mars/client/stylesheets/root/GameboardContainer.scss';
+  @import '@port-of-mars/client/stylesheets/root/GameboardContainer.scss';
 </style>
