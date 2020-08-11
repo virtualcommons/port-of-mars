5b0e6dcd25b8012d211f877d3be7efdab6347244
chrstngyn <Christine.N.Nguyen@asu.edu>
2020-08-11 16:04:02 -0700

    refactor: modify verify page
    restyle verify view -> modal

--- client/src/views/Verify.vue
+++ client/src/views/Verify.vue
@@ -1,51 +1,42 @@
 <template>
-  <div class="login">
-    <div class="wrapper">
-      <div class="text">
-        <h1>Verify your email address</h1>
-      </div>
-      <div>
-        <div>
-          <b-button squared size="lg" variant="dark" class="button" @click="submit">Verify Email</b-button>
-        </div>
-      </div>
-    </div>
-  </div>
+  <b-modal body-bg-variant="dark" centered header-bg-variant="dark" header-border-variant="dark"
+           hide-footer hide-header
+           hide-header-close no-close-on-backdrop no-close-on-esc
+           v-model="verifyEmail">
+    <h3 class="text-center p-2"><strong>VERIFY YOUR EMAIL</strong></h3>
+    <b-button @click="submit" block class="mt-4" size="lg" squared variant="warning">Verify
+    </b-button>
+  </b-modal>
 </template>
 
 <script lang="ts">
-import { Vue, Component, Prop } from "vue-property-decorator";
-import {
-  DASHBOARD_PAGE,
-  GAME_PAGE,
-  LOBBY_PAGE,
-  LOGIN_PAGE
-} from "@port-of-mars/shared/routes";
-import { url } from "@port-of-mars/client/util";
+  import {Component, Prop, Vue} from "vue-property-decorator";
+  import {DASHBOARD_PAGE} from "@port-of-mars/shared/routes";
+  import {url} from "@port-of-mars/client/util";
 
-@Component({})
-export default class Verify extends Vue {
-  @Prop()
-  token!: string;
+  @Component({})
+  export default class Verify extends Vue {
+    @Prop() token!: string;
+    private verifyEmail: boolean = true;
 
-  async submit() {
-    console.log(`POSTING TO ${this.verifyUrl}`)
-    await this.$ajax.post(this.verifyUrl, ({ data, status }) => {
-      // FIXME: these types of store commits should be abstracted away by a coherent store API that all Vue components talk to
-      this.$tstore.commit("SET_DASHBOARD_MESSAGE", {
-        kind: "success",
-        message: "Email successfully verified."
+    get verifyUrl() {
+      return url(`/registration/verify/${this.token}`);
+    }
+
+    async submit() {
+      console.log(`POSTING TO ${this.verifyUrl}`)
+      await this.$ajax.post(this.verifyUrl, ({data, status}) => {
+        // FIXME: these types of store commits should be abstracted away by a coherent store API that all Vue components talk to
+        this.$tstore.commit("SET_DASHBOARD_MESSAGE", {
+          kind: "success",
+          message: "Email successfully verified."
+        });
+        this.$router.push({name: DASHBOARD_PAGE});
       });
-      this.$router.push({ name: DASHBOARD_PAGE });
-    });
+    }
   }
-
-  get verifyUrl() {
-    return url(`/registration/verify/${this.token}`);
-  }
-}
 </script>
 
 <style lang="scss" scoped>
-@import "@port-of-mars/client/stylesheets/views/Login.scss";
+  @import "@port-of-mars/client/stylesheets/views/Login.scss";
 </style>
