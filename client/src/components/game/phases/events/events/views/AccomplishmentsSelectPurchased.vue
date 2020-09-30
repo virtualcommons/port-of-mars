e29cc99e69f5fbd96411957bf4333524af2d93cd
chrstngyn <Christine.N.Nguyen@asu.edu>
2020-09-30 12:18:43 -0700

    feat: unhighlight available resources during efforts wasted
    related to #612

--- client/src/components/game/phases/events/events/views/AccomplishmentsSelectPurchased.vue
+++ client/src/components/game/phases/events/events/views/AccomplishmentsSelectPurchased.vue
@@ -1,38 +1,34 @@
 <template>
-  <div class="event-select-purchased-accomplishment-container">
-    <div class="actions">
-      <div>
-        <template v-if="purchasedAccomplishmentsLength > 0">
-          <p>
-            {{ marsEvent.effect }}
-          </p>
-          <p>
-            {{ marsEvent.flavorText }}
-          </p>
-        </template>
-        <p v-if="purchasedAccomplishmentsLength === 0">
-          No Purchased Accomplishments. Please click 'Continue'.
-        </p>
-        <!--:style="handleAccomplishmentStyle(accomplishment)"-->
-        <AccomplishmentCard
-          :accomplishment="accomplishment"
-          :key="accomplishment.id"
-          @discarded="handleDiscardAccomplishment(accomplishment)"
-          :type="discardType"
-          v-for="accomplishment in purchasedAccomplishments"
-          />
-      </div>
-    </div>
-  </div>
+  <b-container fluid>
+    <b-row class="justify-content-center m-3">
+      <template v-if="purchasedAccomplishmentsLength > 0">
+        <p class="pb-1"><i>{{ marsEvent.flavorText }}</i></p>
+        <p><strong>{{ marsEvent.effect }}</strong></p>
+      </template>
+      <p v-if="purchasedAccomplishmentsLength < 1">
+        No Purchased Accomplishments. Please click 'Continue'.
+      </p>
+      <AccomplishmentCard
+        v-else
+        :accomplishment="accomplishment"
+        :key="accomplishment.id"
+        @discarded="handleDiscardAccomplishment"
+        :type="discardType"
+        v-for="accomplishment in purchasedAccomplishments"
+        :showAvailableResources="showResources"
+      />
+    </b-row>
+  </b-container>
 </template>
 
 <script lang="ts">
-  import {Vue, Component, Prop, Inject} from 'vue-property-decorator';
-
-  import {AbstractGameAPI} from "@port-of-mars/client/api/game/types";
-  import AccomplishmentCard from '@port-of-mars/client/components/game/accomplishments/AccomplishmentCard.vue';
-  import {AccomplishmentCardType} from '@port-of-mars/client/types/cards.ts'
-  import {AccomplishmentData, MarsEventData, RESEARCHER} from '@port-of-mars/shared/types';
+import {Component, Inject, Prop, Vue} from 'vue-property-decorator';
+import _ from "lodash";
+import AccomplishmentCard from '@port-of-mars/client/components/game/accomplishments/AccomplishmentCard.vue';
+import {AccomplishmentCardType} from '@port-of-mars/client/types/cards.ts'
+import {AccomplishmentData, MarsEventData, RESEARCHER} from '@port-of-mars/shared/types';
+import {GameRequestAPI} from "@port-of-mars/client/api/game/request";
+import accomplishments from "@port-of-mars/client/store/mutations/accomplishments";
 
 @Component({
   components: {
@@ -40,10 +36,10 @@
   },
 })
 export default class AccomplishmentsSelectPurchased extends Vue {
-  @Inject()
-  api!: AbstractGameAPI;
+  @Inject() readonly api!: GameRequestAPI;
 
-  private purchasedAccomplishmentsLength: number = -1;
+  @Prop() complete: boolean = false;
+  @Prop() showResources: boolean = false;
   private selectedPurchasedAccomplishment: AccomplishmentData = {
     id: -1,
     role: RESEARCHER,
@@ -59,6 +55,7 @@ export default class AccomplishmentsSelectPurchased extends Vue {
     effect: '',
   };
 
+
   get marsEvent(): MarsEventData {
     return this.$tstore.getters.currentEvent!;
   }
@@ -68,43 +65,36 @@ export default class AccomplishmentsSelectPurchased extends Vue {
   }
 
   get purchasedAccomplishments() {
-    const purchased = this.$tstore.getters.player.accomplishments.purchased;
-
     // TODO: There's definitely a better place to do this...
-    this.purchasedAccomplishmentsLength = Object.keys(purchased).length;
-    return purchased;
+    // this.purchasedAccomplishmentsLength = Object.keys(purchased).length;
+    return this.$tstore.getters.player.accomplishments.purchased;
   }
 
-  get completed() {
-    if (
-      this.purchasedAccomplishmentsLength === 0 ||
-      this.selectedPurchasedAccomplishment.id !== -1
-    ) {
-      return true;
-    }
-    return false;
+  get purchasedAccomplishmentsLength() {
+    return Object.keys(this.purchasedAccomplishments).length;
   }
 
-  private handleDiscardAccomplishment(accomplishment: AccomplishmentData) {
-    this.selectedPurchasedAccomplishment = accomplishment;
-    this.api.stageDiscardOfPurchasedAccomplishment(accomplishment.id);
+
+  private handleDiscardAccomplishment(id: number) {
+    const test = _.slice(this.purchasedAccomplishments, 0, this.purchasedAccomplishments.length)
+    const test2 = _.filter(test, accomplishment => accomplishment.id = id)
+
+    this.selectedPurchasedAccomplishment = test2[0] as AccomplishmentData;
+    this.api.stageDiscardOfPurchasedAccomplishment(id);
+    // this.api.discardAccomplishment(id)
   }
 
-  private handleContinue() {
-    console.log(
-      'DISCARD PURCHASED ACCOMPLISHMENT:',
-      this.selectedPurchasedAccomplishment
-    );
-  }
+  /**
+   * Asynchronously change accomplishment status to discarded.
+   * @param id The ID number of an accomplishment.
+   * > Hide accomplishments on client side when status changes
+   * > Filter accomplishments by ID - if ID is not in the array, card has been discarded
+   */
+  wasDiscarded(id: number): boolean {
+    // possible that animation isn't working because of the 'staged' discard
+    // card is technically still available in purchased, changes have't been applied yet
+    return this.selectedPurchasedAccomplishment.id === id;
 
-  private handleAccomplishmentStyle(a: any) {
-    if (a.id === this.selectedPurchasedAccomplishment.id) {
-      return {
-        backgroundColor: 'var(--light-accent)',
-        color: 'var(--dark-shade)',
-      };
-    }
-    return {};
   }
 }
 </script>
