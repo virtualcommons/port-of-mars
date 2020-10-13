400dc9d8e3b41b0422f4684f22566c033dd6a03d
chrstngyn <Christine.N.Nguyen@asu.edu>
2020-10-13 11:14:11 -0700

    refactor: improve ui to indicate discard on efforts wasted
    if staged discard is completed, only display the pending discarded accomplishment
    
    close #612

--- client/src/components/game/phases/events/events/views/AccomplishmentsSelectPurchased.vue
+++ client/src/components/game/phases/events/events/views/AccomplishmentsSelectPurchased.vue
@@ -1,28 +1,34 @@
 <template>
   <b-container fluid>
-    <b-row class="justify-content-center m-2 pt-3 text-center">
-      <template class="py-2" v-if="completed">
-        <p><strong>You have discarded the Accomplishment below.</strong></p>
+    <b-row class="justify-content-center m-2 py-3 text-center">
+      <template v-if="completed">
+        <p class="pb-3"><strong>You have discarded the Accomplishment below:</strong></p>
       </template>
-      <template class="py-2" v-else>
+      <template v-else class="py-2">
         <p class="pb-1"><i>{{ marsEvent.flavorText }}</i></p>
-        <p><strong>{{ marsEvent.effect }}</strong></p>
+        <p class="pb-3"><strong>{{ marsEvent.effect }}</strong></p>
       </template>
-      <div class="mt-3" v-if="purchasedAccomplishmentsLength < 1">
+      <div v-if="purchasedAccomplishmentsLength < 1" class="mt-3">
         <b-row class="pt-5">
           <p>No Purchased Accomplishments. Please click 'Continue'.</p>
         </b-row>
         <b-row class="justify-content-center align-items-center pt-3">
-          <button @click="ready" class="button">Continue</button>
+          <button class="button" @click="ready">Continue</button>
         </b-row>
       </div>
       <AccomplishmentCard
-        v-if="!completed"
-        :accomplishment="accomplishment"
-        :key="accomplishment.id"
-        @discardPurchased="stageDiscard"
-        :type="cardType"
         v-for="accomplishment in purchasedAccomplishments"
+        v-if="!completed"
+        :key="accomplishment.id"
+        :accomplishment="accomplishment"
+        :type="cardType"
+        @discardPurchased="stageDiscard"
+      />
+      <AccomplishmentCard
+        v-for="accomplishment in selectedPurchasedAccomplishment"
+        v-else
+        :key="accomplishment.id"
+        :accomplishment="accomplishment"
       />
     </b-row>
   </b-container>
@@ -61,10 +67,6 @@ export default class AccomplishmentsSelectPurchased extends Vue {
     effect: '',
   };
 
-  ready() {
-    this.api.setPlayerReadiness(true);
-  }
-
   get marsEvent(): MarsEventData {
     return this.$tstore.getters.currentEvent!;
   }
@@ -83,6 +85,10 @@ export default class AccomplishmentsSelectPurchased extends Vue {
 
   get completed() {
     return this.selectedPurchasedAccomplishment.id !== -1;
+  }
+
+  ready() {
+    this.api.setPlayerReadiness(true);
   }
 
   private stageDiscard(id: number) {
