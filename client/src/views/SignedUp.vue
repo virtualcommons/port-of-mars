4edaae8cbb403153d17488c51421961817655cf1
chrstngyn <Christine.N.Nguyen@asu.edu>
2020-12-03 10:49:26 -0700

    style: signed up page
    add return to login button
    add player role images
    add textured background to match styling on other views
    
    refs #648

--- client/src/views/SignedUp.vue
+++ client/src/views/SignedUp.vue
@@ -1,20 +1,67 @@
 <template>
-<b-container>
-  <b-row class="p-3">
-    <b-col>
-      <h1>Thank you for signing up!</h1>
-      <p class="text-center">We will send you an email when the tournament is ready to begin</p>
-    </b-col>
-  </b-row>
-</b-container>
+  <b-container class="m-0 p-0 h-100" fluid>
+    <!-- BACKGROUND -->
+    <img
+      :src="require(`@port-of-mars/client/assets/marsbg.jpg`)"
+      alt="Background Image"
+      class="background-image h-100 w-100"
+    />
+
+    <b-row align-v="center" class="h-75 w-100">
+      <b-col cols="auto" class="mr-auto">
+        <img
+          :src="
+          require(`@port-of-mars/client/assets/characters-large/Researcher.png`)
+        "
+          alt="Researcher Role"
+          class="researcher"
+        />
+      </b-col>
+
+      <b-col class="text-center">
+        <h1>Thank you for signing up!</h1>
+        <p>We will send you an email when the tournament is ready to begin.</p>
+        <b-button size="lg" class="my-3" variant="outline-warning" :to="loginPage">Return to Login</b-button>
+      </b-col>
+
+      <b-col cols="auto"  >
+        <img
+          :src="
+          require(`@port-of-mars/client/assets/characters-large/Entrepreneur.png`)
+        "
+          alt="Entrepreneur Role"
+          class="entrepreneur"
+        />
+      </b-col>
+    </b-row>
+  </b-container>
 </template>
 
-<script>
-export default {
-name: "SignedUp"
+<script lang="ts">
+import {Component, Vue} from 'vue-property-decorator';
+import {LOGIN_PAGE} from "@port-of-mars/shared/routes";
+
+@Component({})
+export default class SignedUp extends Vue {
+  get loginPage() {
+    return {name: LOGIN_PAGE};
+  }
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+.background-image {
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: -1;
+  opacity: 0.0625;
+}
+
+.researcher, .entrepreneur {
+  //position: absolute;
+  height: 40vh;
+  top: 10vh;
+}
 
 </style>
