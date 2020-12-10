44a243e8bdbf965611e9933e9848b1022f47f582
chrstngyn <Christine.N.Nguyen@asu.edu>
2020-12-10 01:45:38 -0700

    refactor(WIP): login page
    use new art assets
    add smooth/infinite scroll effect - e.g. when scrolling, app auto-scrolls and centers next content
    deprecate Login.scss
    need to make login work again, add consent form, and sponsors/footer
    need to optimize for responsiveness

--- client/src/views/VerifyEmail.vue
+++ client/src/views/VerifyEmail.vue
@@ -1,47 +1,54 @@
 <template>
-  <b-modal body-bg-variant="dark" centered header-bg-variant="dark" header-border-variant="dark"
-           hide-footer hide-header
-           hide-header-close no-close-on-backdrop no-close-on-esc
-           v-model="verifyEmail">
+  <b-modal
+    body-bg-variant="dark"
+    centered
+    header-bg-variant="dark"
+    header-border-variant="dark"
+    hide-footer
+    hide-header
+    hide-header-close
+    no-close-on-backdrop
+    no-close-on-esc
+    v-model="verifyEmail"
+  >
     <h3 class="text-center p-2"><strong>VERIFY YOUR EMAIL</strong></h3>
-    <b-button @click="submit" block class="mt-4" size="lg" squared variant="warning">Verify
+    <b-button @click="submit" block class="mt-4" size="lg" squared variant="warning"
+      >Verify
     </b-button>
   </b-modal>
 </template>
 
 <script lang="ts">
-  import {Component, Prop, Vue} from "vue-property-decorator";
-  import {DASHBOARD_PAGE, SIGNEDUP_PAGE} from "@port-of-mars/shared/routes";
-  import {url} from "@port-of-mars/client/util";
+import { Component, Prop, Vue } from "vue-property-decorator";
+import { DASHBOARD_PAGE, SIGNEDUP_PAGE } from "@port-of-mars/shared/routes";
+import { url } from "@port-of-mars/client/util";
 
-  @Component({})
-  export default class Verify extends Vue {
-    @Prop() token!: string;
-    private verifyEmail: boolean = true;
+@Component({})
+export default class Verify extends Vue {
+  @Prop() token!: string;
+  private verifyEmail: boolean = true;
 
-    get verifyUrl() {
-      return url(`/registration/verify/${this.token}`);
-    }
-
-    async submit() {
-      console.log(`POSTING TO ${this.verifyUrl}`)
-      await this.$ajax.post(this.verifyUrl, ({data, status}) => {
-        // FIXME: these types of store commits should be abstracted away by a coherent store API that all Vue components talk to
-        this.$tstore.commit("SET_DASHBOARD_MESSAGE", {
-          kind: "success",
-          message: "Email successfully verified."
-        });
-        const signUpEnabled = data;
-        if (signUpEnabled) {
-          this.$router.push({name: SIGNEDUP_PAGE});
-        } else {
-          this.$router.push({name: DASHBOARD_PAGE});
-        }
-      });
-    }
+  get verifyUrl() {
+    return url(`/registration/verify/${this.token}`);
   }
+
+  async submit() {
+    console.log(`POSTING TO ${this.verifyUrl}`);
+    await this.$ajax.post(this.verifyUrl, ({ data, status }) => {
+      // FIXME: these types of store commits should be abstracted away by a coherent store API that all Vue components talk to
+      this.$tstore.commit("SET_DASHBOARD_MESSAGE", {
+        kind: "success",
+        message: "Email successfully verified."
+      });
+      const signUpEnabled = data;
+      if (signUpEnabled) {
+        this.$router.push({ name: SIGNEDUP_PAGE });
+      } else {
+        this.$router.push({ name: DASHBOARD_PAGE });
+      }
+    });
+  }
+}
 </script>
 
-<style lang="scss" scoped>
-  @import "@port-of-mars/client/stylesheets/views/Login.scss";
-</style>
+<style lang="scss" scoped></style>
