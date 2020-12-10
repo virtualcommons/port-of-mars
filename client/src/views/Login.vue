a16ff91ef7b6413cc4d646821abe69b60111135d
chrstngyn <Christine.N.Nguyen@asu.edu>
2020-12-10 01:45:38 -0700

    refactor(WIP): login page
    use new art assets
    add smooth/infinite scroll effect - e.g. when scrolling, app auto-scrolls and centers next content
    deprecate Login.scss
    need to make login work again, add consent form, and sponsors/footer
    need to optimize for responsiveness

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -1,51 +1,79 @@
 <template>
-  <b-container class="h-100 w-100 p-0 m-0" fluid>
-    <!-- BACKGROUND -->
-    <!-- :style="'background: yellow'" -->
+  <b-container class="h-100 w-100 p-0 m-0 parent" fluid>
+    <!-- BACKGROUND IMAGE -->
     <img
-      :src="require(`@port-of-mars/client/assets/marsbg.jpg`)"
+      :src="require(`@port-of-mars/client/assets/background/space_open.png`)"
       alt="Background Image"
-      class="background-image h-100 w-100"
-    />
-    <!-- PLAYER IMAGES: POLITICIAN, PIONEER -->
-    <b-row align-v="center" class="h-100">
-      <b-col cols="auto" class="h-50 mr-auto">
-        <img
-          :src="require(`@port-of-mars/client/assets/characters-large/Politician.png`)"
-          alt="Player Image"
-          class="politician"
-        />
-      </b-col>
-      <b-col class="h-50 text-center">
-        <h2>Welcome to</h2>
-        <router-link :to="'/'"><h1>Port of Mars</h1></router-link>
-        <b-button v-if="isLoggedIn" @click="logout" variant="light" class="m-4" size="lg">
-          {{ logoutText }}
-        </b-button>
-        <b-button v-else :href="asuLoginUrl" class="m-4" size="lg" variant="outline-light">
-          Sign In via ASU CAS
-        </b-button>
+      class="background-image w-100"
+    >
+    </img>
 
+    <section>
+      <b-row class="h-100 p-5 text-center" align-v="center">
+        <b-col></b-col>
+        <b-col align-self="center" cols="8" class="m-0 p-5">
+          <h2 class="py-4">
+          Port of Mars is a fun, game-based social science experiment set on the first human
+          community on the Red Planet.
+          </h2>
+          <h4 class="py-4">
+          Sign up to be notified about the next opportunity to play Port of Mars in the Mars Madness
+          tournament.
+          </h4>
+          <b-icon-arrow-down-circle-fill scale=2></b-icon-arrow-down-circle-fill>
+        </b-col>
+        <b-col></b-col>
+      </b-row>
+    </section>
+
+    <!-- LOGIN -->
+    <section>
+      <b-row class="h-100 p-5 text-center" align-v="center">
+        <b-col></b-col>
+        <b-col align-self="center" cols="8" class="blurred-box m-0 p-5 text-center">
+          <b-button v-if="isLoggedIn" @click="logout" variant="danger" class="m-4" size="lg">
+            {{ logoutText }}
+          </b-button>
+          <b-button v-else :href="asuLoginUrl" class="m-5" size="lg" variant="secondary">
+            Sign In via ASU CAS
+          </b-button>
+        
         <b-form class="justify-content-center" inline>
           <label class="sr-only" for="inline-form-input-name">Username</label>
           <b-form-input class="w-25 mx-3" id="inline-form-input-name" placeholder="DEV_LOGIN">
           </b-form-input>
           <b-button @click="toggleDevLogin" class="dev-login-toggler" pill v-if="isDevLoginEnabled">
-            <b-icon-gear></b-icon-gear>
+          <b-icon-gear></b-icon-gear>
           </b-button>
         </b-form>
-      </b-col>
+        </b-col>
+        <b-col></b-col>
+      </b-row>
+    </section>
 
-      <b-col cols="auto" class="h-50">
-        <img
-          :src="require(`@port-of-mars/client/assets/characters-large/Pioneer.png`)"
-          alt="Player Image"
-          class="pioneer"
-        />
-      </b-col>
-    </b-row>
+    <section>
+      <b-row class="h-100 p-5 text-center" align-v="center">
+        <b-col></b-col>
+        <b-col align-self="center" cols="8" class="m-0 p-5 text-center" :style="'background-color: grey'">
+          <h2>Consent Form</h2>
 
-    <!-- LOGIN -->
+        </b-col>
+        <b-col></b-col>
+      </b-row>
+    </section>
+
+    <section>
+      <b-row class="h-100 p-5 text-center" align-v="center">
+        <b-col></b-col>
+        <b-col align-self="center" cols="8" class="m-0 p-5 text-center" :style="'background-color: grey'">
+          <h2>This project was made possible by funding and support from our sponsors.</h2>
+          
+        </b-col>
+        <b-col></b-col>
+      </b-row>
+    </section>
+
+    <!-- OLD LOGIN -->
     <!-- <div class="content-wrapper row">
       <div class="logged-in col-12" >
         <b-button @click="logout" variant="outline-light" class="m-4" size="lg">
@@ -167,6 +195,7 @@ export default class Login extends Vue {
 </script>
 
 <style lang="scss" scoped>
+// HTML TEXT ELEMENTS
 a {
   //color: transparent;
   text-decoration: none;
@@ -189,18 +218,45 @@ h2 {
   color: $light-shade;
 }
 
+// IMAGES
+
 .background-image {
   position: absolute;
-  top: 0;
   z-index: -1;
-  opacity: 0.0625;
 }
 
-.politician,
-.pioneer {
-  height: 45vh;
-  top: 15vh;
+section {
+  scroll-snap-align: start;
+  height: 100vh;
+  width: 100vw;
 }
+
+.blurred-box{
+  position: relative;
+  width: 250px;
+  height: 350px;
+  top: calc(50% - 175px);
+  left: calc(50% - 125px);
+  background: inherit;
+  border-radius: 2px;
+  overflow: hidden;
+}
+
+.blurred-box:after{
+ content: '';
+ width: 300px;
+ height: 300px;
+ background: inherit; 
+ position: absolute;
+ left: -25px;
+ right: 0;
+ top: -25px;  
+ bottom: 0;
+ box-shadow: inset 0 0 0 200px rgba(255,255,255,0.05);
+ filter: blur(10px);
+}
+
+// LOGIN :: FORM
 
 input[type="text"] {
   border: solid 0.1rem $light-shade;
