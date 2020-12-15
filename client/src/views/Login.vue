534b2dc196c2666e4cf39d661f64a2e66c6c4035
chrstngyn <Christine.N.Nguyen@asu.edu>
2020-12-15 15:14:17 -0700

    refactor: make login page un-scrollable
    move login buttons and forms to landing page
    remove consent form and sponsors from landing page

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -1,76 +1,34 @@
 <template>
-  <b-container class="p-0 m-0" fluid>
+  <b-container class="vh-100 p-0 m-0" fluid>
     <!-- splash page text -->
-    <section>
-      <b-row align-v="center" class="h-100 p-5 text-center">
+      <b-row class="h-100 p-5 text-center">
         <b-col></b-col>
-        <b-col align-self="end" class="m-5 " cols="8">
-          <h2 class="p-5">
+        <b-col align-self="end" class="m-5 p-5" cols="8">
+          <h2 class="my-5 p-5">
             Port of Mars is a fun, game-based social science experiment set on the first human
             community on the Red Planet.
           </h2>
-          <b-icon-arrow-down-circle-fill scale=2></b-icon-arrow-down-circle-fill>
+          <h2 class="my-5">Sign up to be notified about the next opportunity to play in the Mars Madness
+            tournament.</h2>
+          <b-button v-if="isLoggedIn" class="justify-content-center mt-3" size="lg" variant="warning" @click="logout">
+            {{ logoutText }}
+          </b-button>
+          <b-button v-else-if="!toggleDevLogin" :href="asuLoginUrl" class="justify-content-center mt-3" size="lg" variant="success">
+            Sign Up via ASU CAS
+          </b-button>
+          <b-form v-if="isDevMode && toggleDevLogin" class="justify-content-center mt-3" inline @submit="devLogin">
+            <label class="sr-only" for="input-username">Username</label>
+            <b-form-input id="input-username" v-model="username" class="mx-3" placeholder="DEV_LOGIN" required>
+            </b-form-input>
+            <b-button type="submit">
+              <b-icon-gear></b-icon-gear>
+            </b-button>
+          </b-form>
+          <b-form-checkbox v-if="isDevMode" switch size="sm" v-model="toggleDevLogin" class="pt-3">Developer Login</b-form-checkbox>
+          <b-alert v-if="error" variant="warning">{{ error }}</b-alert>
         </b-col>
         <b-col></b-col>
       </b-row>
-    </section>
-
-    <!-- sign up + dev login buttons -->
-    <section>
-      <b-row align-v="center" class="h-100 p-5 text-center">
-        <b-col></b-col>
-        <b-col align-self="center" class="m-0 p-5 text-center" cols="8">
-          <b-card
-            header="Register"
-            header-bg-variant="primary"
-            style="font-family: '$exo';"
-          >
-            <b-card-text>
-              <p class="py-2"><strong>Sign up to be notified about the next opportunity to play in the Mars Madness
-                tournament.</strong></p>
-
-              <b-button v-if="isLoggedIn" class="m-4" size="lg" variant="danger" @click="logout">
-                {{ logoutText }}
-              </b-button>
-              <b-button v-else :href="asuLoginUrl" class="mt-2 mb-5" size="lg" variant="success">
-                Sign Up via ASU CAS
-              </b-button>
-              <b-form v-if="isDevMode && toggleDevLogin" class="justify-content-center" inline @submit="devLogin">
-                <label class="sr-only" for="input-username">Username</label>
-                <b-form-input id="input-username" v-model="username" class="w-25 mx-3" placeholder="DEV_LOGIN" required>
-                </b-form-input>
-                <b-button type="submit">
-                  <b-icon-gear></b-icon-gear>
-                </b-button>
-              </b-form>
-              <b-form-checkbox :state="toggleDevLogin" v-if="isDevMode" switch size="sm" v-model="toggleDevLogin" class="pt-3">Developer Login</b-form-checkbox>
-              <b-alert v-if="error" variant="warning">{{ error }}</b-alert>
-            </b-card-text>
-          </b-card>
-        </b-col>
-        <b-col></b-col>
-      </b-row>
-    </section>
-
-    <!-- consent form -->
-    <section>
-      <b-row align-v="center" class="h-100 p-5">
-        <b-col></b-col>
-        <b-col align-self="center" cols="8">
-          <Consent/>
-        </b-col>
-        <b-col></b-col>
-      </b-row>
-    </section>
-    <section>
-      <b-row align-v="center" class="h-100 p-5 text-center">
-        <b-col></b-col>
-        <b-col align-self="center" class="h-100 m-0 p-5 text-center" cols="8">
-          <Sponsors/>
-        </b-col>
-        <b-col></b-col>
-      </b-row>
-    </section>
   </b-container>
 </template>
 
