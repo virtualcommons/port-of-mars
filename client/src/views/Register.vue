48747ec8bb06ed02df07e147fcba48670eead370
chrstngyn <Christine.N.Nguyen@asu.edu>
2021-01-11 16:26:48 -0700

    refactor: boostrap grid box refactoring
    clean up b-row/b-col layout on register page
    fix styling of error messages

--- client/src/views/Register.vue
+++ client/src/views/Register.vue
@@ -1,17 +1,15 @@
 <template>
-    <b-container class="h-100 p-0 m-0" fluid>
-      <b-row align-v="center" class="h-100 p-5">
-        <b-col></b-col>
-        <b-col align-self="center" cols="8">
-          <b-row>
-            <Messages />
-          </b-row>
-          <b-row>
-            <ConsentForm />
-          </b-row>
-        </b-col>
-        <b-col></b-col>
-      </b-row>
+  <b-container class="h-100 p-0 m-0" fluid>
+    <b-row class="p-5 justify-content-center">
+      <b-col cols="4">
+        <Messages/>
+      </b-col>
+    </b-row>
+    <b-row class="justify-content-center">
+      <b-col>
+        <ConsentForm/>
+      </b-col>
+    </b-row>
   </b-container>
 </template>
 
@@ -21,7 +19,8 @@ import Messages from '@port-of-mars/client/components/dashboard/Messages.vue';
 import ConsentForm from '@port-of-mars/client/components/dashboard/Consent.vue';
 
 @Component({
-  components: { Messages, ConsentForm }
+  components: {Messages, ConsentForm}
 })
-export default class Register extends Vue {}
+export default class Register extends Vue {
+}
 </script>
