769bd7dd0a30aaa050ac2417b7f5db32ec971742
chrstngyn <Christine.N.Nguyen@asu.edu>
2021-02-26 10:11:06 -0700

    fix: test that app renders + mounts

--- client/tests/unit/app.spec.ts
+++ client/tests/unit/app.spec.ts
@@ -4,6 +4,6 @@ import App from "@port-of-mars/client/App.vue";
 describe("App.vue", () => {
   it("renders", () => {
     const wrapper = shallowMountPOM(App);
-    expect(wrapper.attributes("class")).toBe("game");
+    expect(wrapper.attributes("class")).toBe("h-100 w-auto p-0 m-0");
   });
 });
