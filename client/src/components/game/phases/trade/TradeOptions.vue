2585972ee3cc0733a36e1ac22157993a052d78c8
chrstngyn <Christine.N.Nguyen@asu.edu>
2021-08-31 11:28:55 -0700

    refactor: bootstrapify TradeOptions
    use b-form-spinbutton to replace increment/decrement
    replace divs with b-containers and  b-rows

--- client/src/components/game/phases/trade/TradeOptions.vue
+++ client/src/components/game/phases/trade/TradeOptions.vue
@@ -1,131 +1,82 @@
 <template>
-  <div class="c-trade-options container">
-    <div class="title-wrapper row">
-      <div class="title col-12">
-        <p>{{ text }}</p>
-      </div>
-    </div>
-    <!-- FIXME: add better visual cues (e.g. tooltips) to guide players better in making trades -->
-    <div class="investment-wrapper row">
+  <b-container fluid class="h-100">
+    <b-row class="w-100 h-auto">
+      <p>{{ text }}</p>
+    </b-row>
+    <b-row class="w-100">
       <div
         v-for="(value, resource) in resources"
-        :key="resource + Math.random()"
-        class="investment col"
-        :class="{ 'unavailable-investment': grayOutResources(resource) }"
+        :key="resource"
+        class="m-2"
       >
-        <div class="investment-input-wrapper">
-          <!-- TODO: add tooltip -->
-          <img
-            :src="require(`@port-of-mars/client/assets/icons/${resource}.svg`)"
-            alt="Investment"
-          />
-          <div class="input">
-            <input
-              v-if="mode === 'outgoing'"
-              type="number"
-              v-model.number="resources[resource]"
-              :disabled="playerInventory[resource] === 0"
-            />
-            <input v-else type="number" v-model.number="resources[resource]" />
-          </div>
-        </div>
-        <div class="buttons-wrapper">
-          <button
-            @click="decreaseNum(resource)"
-            :disabled="mode === 'outgoing' && playerInventory[resource] === 0"
-          >
-            <font-awesome-icon
-              :icon="['fas', 'minus']"
-              size="sm"
-              class="icon"
-            />
-          </button>
-          <button
-            @click="increaseNum(resource)"
-            :disabled="mode === 'outgoing' && playerInventory[resource] === 0"
-          >
-            <font-awesome-icon :icon="['fas', 'plus']" size="sm" class="icon" />
-          </button>
-        </div>
+        <b-img
+          v-bind="mainProps"
+          rounded="circle"
+          :src="require(`@port-of-mars/client/assets/icons/${resource}.svg`)"
+          :alt="resource"
+          class="m-3"
+        >
+        </b-img>
+        <b-form-spinbutton
+          v-model="resources[resource]"
+          v-if="mode === 'outgoing'"
+          min="0"
+          max="999"
+          :disabled="playerInventory[resource] === 0 || !hasSelectedPlayer"
+          inline
+          @change="resourceReader(resources)"
+        ></b-form-spinbutton>
+        <b-form-spinbutton
+          v-else
+          v-model="resources[resource]"
+          :disabled="!hasSelectedPlayer"
+          min="0"
+          max="999"
+          inline
+          @change="resourceReader(resources)"
+        ></b-form-spinbutton>
       </div>
-    </div>
-  </div>
+    </b-row>
+  </b-container>
 </template>
 
 <script lang="ts">
 import { Vue, Component, Prop } from 'vue-property-decorator';
-import { Resource, ResourceAmountData } from '@port-of-mars/shared/types';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import { faPlus } from '@fortawesome/free-solid-svg-icons/faPlus';
-import { faMinus } from '@fortawesome/free-solid-svg-icons/faMinus';
-
-library.add(faPlus);
-library.add(faMinus);
-Vue.component('font-awesome-icon', FontAwesomeIcon);
+import { ResourceAmountData } from '@port-of-mars/shared/types';
 
 @Component({})
 export default class TradeOptions extends Vue {
-  //the text that displays what type of option set it is
+  // the text that displays what type of option set it is
   @Prop({ default: '' }) text!: string;
 
-  //mode -> 'incoming' | 'outgoing'
+  // mode -> 'incoming' | 'outgoing'
   @Prop({ default: '' }) mode!: string;
 
-  //the way the parent gets the updates from the child
+  // the way the parent gets the updates from the child
   @Prop() resourceReader!: any;
 
   //the starting values inherited from the parent
   @Prop() resources!: ResourceAmountData;
 
-  get tradePartnerName() {
-    return this.$tstore.state.ui.tradeData.recipient.role;
+  mainProps = {
+    center: true,
+    fluid: true,
+    blankColor: '#bbb',
+    width: 64,
+    height: 64,
   }
 
-  private tradePartner() {
-    return this.tradePartnerName != '';
+  get selectedPlayer() {
+    return this.$tstore.state.ui.tradeData.recipient.role;
   }
 
   get playerInventory() {
     return this.$store.getters.player.inventory;
   }
 
-  private grayOutResources(resource: Resource) {
-    return (this.playerInventory[resource] == 0 && this.mode == 'outgoing') || !this.tradePartner();
-
-  }
-
-  private increaseNum(resource: Resource): void {
-    let max: number;
-
-    if (this.mode === 'outgoing') {
-      max = this.playerInventory[resource];
-    } else {
-      max = 999;
-    }
-
-    if (this.resources[resource] < max) {
-      this.resources[resource]++;
-    } else {
-      this.resources[resource] = max;
-    }
-
-    this.resourceReader(this.resources);
-  }
-
-  private decreaseNum(resource: Resource): void {
-    const min: number = 0;
-
-    if (this.resources[resource] > min) {
-      this.resources[resource]--;
-    } else {
-      this.resources[resource] = min;
-    }
-    this.resourceReader(this.resources);
+  hasSelectedPlayer() {
+    console.log("selectedPLayer: ", this.selectedPlayer);
+    return this.selectedPlayer != '';
   }
 }
 </script>
-
-<style lang="scss" scoped>
-@import '@port-of-mars/client/stylesheets/game/phases/trade/TradeOptions.scss';
-</style>
