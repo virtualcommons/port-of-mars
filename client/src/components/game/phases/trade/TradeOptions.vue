ad1c74540eab8d20129e1c54fabea84fbac8927f
chrstngyn <Christine.N.Nguyen@asu.edu>
2021-09-16 13:02:51 -0700

    style: trade request increment/decrement
    use v-slots on b-form-spinbuttons to customize increment/decrement buttons

--- client/src/components/game/phases/trade/TradeOptions.vue
+++ client/src/components/game/phases/trade/TradeOptions.vue
@@ -4,11 +4,7 @@
       <p>{{ text }}</p>
     </b-row>
     <b-row class="w-100">
-      <div
-        v-for="(value, resource) in resources"
-        :key="resource"
-        class="m-2"
-      >
+      <div v-for="(value, resource) in resources" :key="resource" class="m-2">
         <b-img
           v-bind="mainProps"
           rounded="circle"
@@ -25,7 +21,14 @@
           :disabled="playerInventory[resource] === 0 || !hasSelectedPlayer"
           inline
           @change="resourceReader(resources)"
-        ></b-form-spinbutton>
+        >
+          <template #decrement>
+            <b-icon-dash scale="1.25" color="white"></b-icon-dash>
+          </template>
+          <template #increment>
+            <b-icon-plus scale="1.25" color="white"></b-icon-plus>
+          </template>
+        </b-form-spinbutton>
         <b-form-spinbutton
           v-else
           v-model="resources[resource]"
@@ -34,23 +37,30 @@
           max="999"
           inline
           @change="resourceReader(resources)"
-        ></b-form-spinbutton>
+        >
+          <template #decrement>
+            <b-icon-dash scale="1.25" color="white"></b-icon-dash>
+          </template>
+          <template #increment>
+            <b-icon-plus scale="1.25" color="white"></b-icon-plus>
+          </template>
+        </b-form-spinbutton>
       </div>
     </b-row>
   </b-container>
 </template>
 
 <script lang="ts">
-import { Vue, Component, Prop } from 'vue-property-decorator';
-import { ResourceAmountData } from '@port-of-mars/shared/types';
+import { Vue, Component, Prop } from "vue-property-decorator";
+import { ResourceAmountData } from "@port-of-mars/shared/types";
 
 @Component({})
 export default class TradeOptions extends Vue {
   // the text that displays what type of option set it is
-  @Prop({ default: '' }) text!: string;
+  @Prop({ default: "" }) text!: string;
 
   // mode -> 'incoming' | 'outgoing'
-  @Prop({ default: '' }) mode!: string;
+  @Prop({ default: "" }) mode!: string;
 
   // the way the parent gets the updates from the child
   @Prop() resourceReader!: any;
@@ -61,10 +71,10 @@ export default class TradeOptions extends Vue {
   mainProps = {
     center: true,
     fluid: true,
-    blankColor: '#bbb',
+    blankColor: "#bbb",
     width: 64,
-    height: 64,
-  }
+    height: 64
+  };
 
   get selectedPlayer() {
     return this.$tstore.state.ui.tradeData.recipient.role;
@@ -76,7 +86,7 @@ export default class TradeOptions extends Vue {
 
   hasSelectedPlayer() {
     console.log("selectedPLayer: ", this.selectedPlayer);
-    return this.selectedPlayer != '';
+    return this.selectedPlayer != "";
   }
 }
 </script>
