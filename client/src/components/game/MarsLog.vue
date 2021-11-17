8d05f0d00bab571eb6884448f3d67ea1c9913025
chrstngyn <Christine.N.Nguyen@asu.edu>
2021-11-17 13:48:19 -0700

    style: improve mars log msg spacing +  padding

--- client/src/components/game/MarsLog.vue
+++ client/src/components/game/MarsLog.vue
@@ -4,7 +4,7 @@
       class="w-100 h-100 m-auto scroll-to-recent"
       style="overflow-y: auto; overflow-x: hidden;"
     >
-      <b-row class="w-100 my-2" align-content="end">
+      <b-row class="w-100 justify-content-center" align-content="end">
         <!-- if there is no history to display -->
         <p v-if="logs.length === 0" class="m-5" style="color: rgba(241, 224, 197, 0.25)">
           Nothing to report.
@@ -14,20 +14,20 @@
           v-for="log in logs"
           :style="{ backgroundColor: categoryColorMap.get(log.category) }"
           :key="log.id"
-          class="w-100 my-1 d-flex flex-column overflow-hidden"
+          class="h-auto w-100 my-1 flex-column justify-content-start"
           style="background-color: var(--marslog-orange)"
         >
-          <b-col>
+          <b-col align-self="end">
             <!-- category (e.g. System Health, Event, etc.) -->
             <p
-              class="mb-0 ml-4 mt-2 text-uppercase"
+              class="mb-0 mx-2 mt-2 text-uppercase"
               style="color: var(--light-accent); font-weight: bold"
             >
               {{ log.category }}
             </p>
             <!-- message -->
-            <p class="ml-4" style="word-wrap: break-word">{{ log.content }}</p>
-            <b-row class="w-100 ml-2">
+            <p class="mx-2" style="word-wrap: break-word">{{ log.content }}</p>
+            <b-row class="px-2">
               <!-- timestamp and round -->
               <b-col cols="auto" class="mr-auto">
                 <p>[ {{ logTime(log.timestamp) }} ]</p>
