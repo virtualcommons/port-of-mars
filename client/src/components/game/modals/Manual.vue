ef0d1ea41bc7b3cda3fcb4f3e134bc6e17cb1cb0
chrstngyn <Christine.N.Nguyen@asu.edu>
2022-01-11 16:32:45 -0700

    feat (WIP): add in-game manual
    create an skeleton for modal component that displays a cheat sheet in-game
    TODO: add final draft of manual

---
+++ client/src/components/game/modals/Manual.vue
@@ -0,0 +1,13 @@
+<template>
+  <b-modal hide-footer title="Game Manual">
+    
+  </b-modal>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+export default class Manual extends Vue {}
+</script>
+
+<style scoped lang="scss"></style>
