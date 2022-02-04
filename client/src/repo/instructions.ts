087a07b97b06c8bc632a28d4308feed4ebcc120d
Allen Lee <allen.lee@asu.edu>
2022-02-03 23:27:58 -0700

    refactor: refine phase instructions text

--- client/src/repo/instructions.ts
+++ client/src/repo/instructions.ts
@@ -1,17 +1,17 @@
 export const PHASE_INSTRUCTIONS = {
   newRound:
-    "System Health Report for the previous round. See report below for more details on your net contributions.",
+    "ALERT: Please review the System Health Report below.",
   events:
     "If System Health is less than 65, 2 Events will occur; " +
     "if System Health is less than 35, 3 Events will occur. ",
   invest:
-    "You can invest these timeblocks into System Health or use them to purchase Influences. " +
-    "You cannot save uninvested timeblocks this round to use in a later round.",
+    "Invest timeblocks to purchase Influence or System Health. " +
+    "Uninvested timeblocks do not persist and cannot be used in later rounds.",
   trade:
     "Request and trade for Influence resources you need to purchase your Accomplishments." +
-    "Chat with the other players to discuss and coordinate trades.",
+    "Chat with other players to discuss and coordinate trades.",
   purchase:
-    "Accomplishments that you can purchase have an orange header and have been sorted to the top.",
+    "Accomplishments that you can purchase have an orange header and are sorted to the top.",
   discard:
     "At the beginning of the next round, you will receive up to 3 new Accomplishments to replace Accomplishments you have purchased or discarded."
 };
