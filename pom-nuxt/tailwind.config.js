7540ca9f8667fd74c7e267f5046968dfb7470f95
chrstngyn <Christine.N.Nguyen@asu.edu>
2022-07-18 15:01:16 -0700

    fix: omit nuxt config in content module export
    https://github.com/nuxt-community/tailwindcss-module/issues/429#issuecomment-1041486660

--- pom-nuxt/tailwind.config.js
+++ pom-nuxt/tailwind.config.js
@@ -5,7 +5,9 @@ module.exports = {
     "./layouts/**/*.vue",
     "./pages/**/*.vue",
     "./plugins/**/*.{js,ts}",
-    "./nuxt.config.{js,ts}",
+    // * https://github.com/nuxt-community/tailwindcss-module/issues/429#issuecomment-1041486660
+    // *  the main reason we need to include nuxt.config.{js,ts} in the list of files that Tailwind CSS scans is because you might be applying Tailwind classes to the <html> or <body> elements in this file using the htmlAttrs or bodyAttrs properties available in Vue Meta. By excluding this file, those styles won't be picked up, and your site will look incorrect.
+    // "./nuxt.config.{js,ts}",
   ],
   theme: {
     extend: {},
