21bb8ca2f6026638392e708f277b884f73d27150
chrstngyn <Christine.N.Nguyen@asu.edu>
2022-07-18 14:30:34 -0700

    feat: set up static pages for nuxt app
    create header and footer components
    display index page by using the <NuxtPage> component

---
+++ pom-nuxt/components/Footer.vue
@@ -0,0 +1,274 @@
+<!-- 
+Footer designed by Christina Carrasquilla for Port of Mars
+ccarra1@asu.edu
+-->
+<template>
+  <footer>
+    <section id="footer-nav">
+      <h2>Navigation</h2>
+      <ul>
+        <li>
+          <a href="#about" title="about"
+            >About
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="icon icon-tabler icon-tabler-link"
+              width="16"
+              height="16"
+              viewBox="0 0 24 24"
+              stroke-width="2"
+              stroke="#ffffff"
+              fill="none"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path
+                d="M10 14a3.5 3.5 0 0 0 5 0l4 -4a3.5 3.5 0 0 0 -5 -5l-.5 .5"
+              />
+              <path
+                d="M14 10a3.5 3.5 0 0 0 -5 0l-4 4a3.5 3.5 0 0 0 5 5l.5 -.5"
+              /></svg
+          ></a>
+        </li>
+        <li>
+          <a href="#leaderboard" title="leaderboard"
+            >Leaderboard
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="icon icon-tabler icon-tabler-link"
+              width="16"
+              height="16"
+              viewBox="0 0 24 24"
+              stroke-width="2"
+              stroke="#ffffff"
+              fill="none"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path
+                d="M10 14a3.5 3.5 0 0 0 5 0l4 -4a3.5 3.5 0 0 0 -5 -5l-.5 .5"
+              />
+              <path
+                d="M14 10a3.5 3.5 0 0 0 -5 0l-4 4a3.5 3.5 0 0 0 5 5l.5 -.5"
+              /></svg
+          ></a>
+        </li>
+        <li>
+          <a href="#news" title="news"
+            >News
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="icon icon-tabler icon-tabler-link"
+              width="16"
+              height="16"
+              viewBox="0 0 24 24"
+              stroke-width="2"
+              stroke="#ffffff"
+              fill="none"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path
+                d="M10 14a3.5 3.5 0 0 0 5 0l4 -4a3.5 3.5 0 0 0 -5 -5l-.5 .5"
+              />
+              <path
+                d="M14 10a3.5 3.5 0 0 0 -5 0l-4 4a3.5 3.5 0 0 0 5 5l.5 -.5"
+              /></svg
+          ></a>
+        </li>
+        <li>
+          <a href="#gameplay" title="gameplay"
+            >Gameplay
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="icon icon-tabler icon-tabler-link"
+              width="16"
+              height="16"
+              viewBox="0 0 24 24"
+              stroke-width="2"
+              stroke="#ffffff"
+              fill="none"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path
+                d="M10 14a3.5 3.5 0 0 0 5 0l4 -4a3.5 3.5 0 0 0 -5 -5l-.5 .5"
+              />
+              <path
+                d="M14 10a3.5 3.5 0 0 0 -5 0l-4 4a3.5 3.5 0 0 0 5 5l.5 -.5"
+              /></svg
+          ></a>
+        </li>
+      </ul>
+    </section>
+    <section id="sponsors">
+      <h2>Sponsors</h2>
+      <ul>
+        <li>
+          <a
+            href="https://interplanetary.asu.edu/"
+            title="ASU Interplanetary Initiative"
+            >ASU Interplanetary Initiative
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="icon icon-tabler icon-tabler-external-link"
+              width="16"
+              height="16"
+              viewBox="0 0 24 24"
+              stroke-width="2"
+              stroke="#ffffff"
+              fill="none"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path
+                d="M11 7h-5a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-5"
+              />
+              <line x1="10" y1="14" x2="20" y2="4" />
+              <polyline points="15 4 20 4 20 9" /></svg
+          ></a>
+        </li>
+        <li>
+          <a
+            href="https://complexity.asu.edu/"
+            title="ASU School of Complex Adaptive Systems"
+            >ASU School of Complex Adaptive Systems
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="icon icon-tabler icon-tabler-external-link"
+              width="16"
+              height="16"
+              viewBox="0 0 24 24"
+              stroke-width="2"
+              stroke="#ffffff"
+              fill="none"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path
+                d="M11 7h-5a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-5"
+              />
+              <line x1="10" y1="14" x2="20" y2="4" />
+              <polyline points="15 4 20 4 20 9" />
+            </svg>
+          </a>
+        </li>
+        <li>
+          <a href="https://www.nsf.gov/" title="US National Science Foundation"
+            >US National Science Foundation (SES-2049553)
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="icon icon-tabler icon-tabler-external-link"
+              width="16"
+              height="16"
+              viewBox="0 0 24 24"
+              stroke-width="2"
+              stroke="#ffffff"
+              fill="none"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path
+                d="M11 7h-5a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-5"
+              />
+              <line x1="10" y1="14" x2="20" y2="4" />
+              <polyline points="15 4 20 4 20 9" /></svg
+          ></a>
+        </li>
+      </ul>
+    </section>
+    <section id="social-media">
+      <h2>Social Media Links</h2>
+      <ul>
+        <li>
+          <a href="mailto:portofmars@asu.edu" title="email us"
+            >Email
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="icon icon-tabler icon-tabler-at"
+              width="15"
+              height="15"
+              viewBox="0 0 24 24"
+              stroke-width="2"
+              stroke="#ffffff"
+              fill="none"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <circle cx="12" cy="12" r="4" />
+              <path
+                d="M16 12v1.5a2.5 2.5 0 0 0 5 0v-1.5a9 9 0 1 0 -5.5 8.28"
+              /></svg
+          ></a>
+        </li>
+        <li>
+          <a href="https://www.instagram.com/portofmars/" title="Instagram"
+            >Instagram
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="icon icon-tabler icon-tabler-brand-instagram"
+              width="16"
+              height="16"
+              viewBox="0 0 24 24"
+              stroke-width="2"
+              stroke="#ffffff"
+              fill="none"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <rect x="4" y="4" width="16" height="16" rx="4" />
+              <circle cx="12" cy="12" r="3" />
+              <line x1="16.5" y1="7.5" x2="16.5" y2="7.501" /></svg
+          ></a>
+        </li>
+        <li>
+          <a href="https://twitter.com/PortOfMars" title="Twitter"
+            >Twitter
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="icon icon-tabler icon-tabler-brand-twitter"
+              width="16"
+              height="16"
+              viewBox="0 0 24 24"
+              stroke-width="2"
+              stroke="#ffffff"
+              fill="none"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path
+                d="M22 4.01c-1 .49 -1.98 .689 -3 .99c-1.121 -1.265 -2.783 -1.335 -4.38 -.737s-2.643 2.06 -2.62 3.737v1c-3.245 .083 -6.135 -1.395 -8 -4c0 0 -4.182 7.433 4 11c-1.872 1.247 -3.739 2.088 -6 2c3.308 1.803 6.913 2.423 10.034 1.517c3.58 -1.04 6.522 -3.723 7.651 -7.742a13.84 13.84 0 0 0 .497 -3.753c-.002 -.249 1.51 -2.772 1.818 -4.013z"
+              /></svg
+          ></a>
+        </li>
+      </ul>
+    </section>
+    <div id="copyright">
+      <small>&copy; 2020-2022 Arizona Board of Regents</small>
+    </div>
+  </footer>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import { MANUAL_PAGE } from "@port-of-mars/shared/routes";
+import { BUILD_ID } from "@port-of-mars/shared/settings";
+
+const buildId = ref(BUILD_ID);
+const currentYear = new Date().getFullYear();
+const manual = { name: MANUAL_PAGE };
+</script>
+
+<style lang="css">
+@import url("../assets/css/main.css");
+</style>
---
+++ pom-nuxt/components/Header.vue
@@ -0,0 +1,61 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import {
+  DASHBOARD_PAGE,
+  HOME_PAGE,
+  LOGIN_PAGE,
+  REGISTER_PAGE,
+  MANUAL_PAGE,
+  GAME_PAGE,
+  LOBBY_PAGE,
+} from "@port-of-mars/shared/routes";
+import { isDevOrStaging } from "@port-of-mars/shared/settings";
+import BootstrapVue from "bootstrap-vue";
+
+const title = ref("Mission Control Dashboard");
+
+const isDevMode: boolean = isDevOrStaging;
+const contactUrl = "mailto:portmars@asu.edu";
+const SITE_URL = "https://portofmars.asu.edu";
+const INSTAGRAM_URL = "https://www.instagram.com/portofmars/";
+
+const dashboard = { name: DASHBOARD_PAGE };
+const consent = { name: REGISTER_PAGE };
+const home = { name: HOME_PAGE };
+const login = { name: LOGIN_PAGE };
+const manual = { name: MANUAL_PAGE };
+const game = { name: GAME_PAGE };
+const lobby = { name: LOBBY_PAGE };
+</script>
+
+<template>
+  <header>
+    <img
+      id="logo"
+      src="images/logo-Port-of-Mars-White.svg"
+      alt="the planet mars illustrated in white above Port of Mars"
+    />
+    <h1 class="hidden">Port of Mars</h1>
+    <nav>
+      <h2 class="hidden">Main Navigation</h2>
+      <div id="nav-mobile">
+        <input type="checkbox" />
+        <span></span>
+        <span></span>
+        <span></span>
+        <ul id="nav">
+          <li><a href="#about" title="about">About</a></li>
+          <li><a href="#winners" title="winners">Winners</a></li>
+          <li><a href="#news" title="news">News</a></li>
+          <li><a href="#gameplay" title="gameplay">Gameplay</a></li>
+          <li><a href="sign-up" title="sign-up">Sign Up</a></li>
+          <li><a :to="login" title="log-in">Log In</a></li>
+        </ul>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<style lang="css">
+@import url("../assets/css/main.css");
+</style>
