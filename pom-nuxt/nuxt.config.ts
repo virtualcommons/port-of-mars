1fff00d9ec42106a37b7680b0cd742458312e6dc
chrstngyn <Christine.N.Nguyen@asu.edu>
2022-07-18 14:32:20 -0700

    build(deps): install and configure tailwind css

--- pom-nuxt/nuxt.config.ts
+++ pom-nuxt/nuxt.config.ts
@@ -1,6 +1,26 @@
-import { defineNuxtConfig } from 'nuxt'
+import { defineNuxtConfig } from "nuxt";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
-})
+  alias: {
+    "@": "./*",
+    assets: "./assets",
+  },
+  build: {
+    postcss: {
+      plugins: {
+        tailwindcss: {
+          cssPath: "assets/css/main.css",
+          configPath: "tailwind.config.js",
+          exposeConfig: false,
+          config: {},
+          injectPosition: 0,
+          viewer: true,
+        },
+        autoprefixer: {},
+      },
+    },
+  },
+  css: ["@/assets/css/main.css"],
+  modules: ["@nuxtjs/tailwindcss"],
+});
