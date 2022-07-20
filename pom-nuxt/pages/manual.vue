569a8c217d7a8ccf52ee06ffa17822198c6ef87f
chrstngyn <Christine.N.Nguyen@asu.edu>
2022-07-20 13:21:58 -0700

    feat: add manual page

---
+++ pom-nuxt/pages/manual.vue
@@ -0,0 +1,242 @@
+<template>
+  <main>
+    <section id="play">
+      <h2>Gameplay</h2>
+      <div id="game-objectives">
+        <section id="the-game">
+          <h3>The Game</h3>
+          <p>
+            You are 1 of 5 residents at the Port of Mars, the first long-term
+            habitat on the planet. Each game consists of 8 —12 rounds, each with
+            phases of investing, trading, purchasing, discarding, and reacting
+            to Mars events. Chance and player actions impact system health. If
+            system health falls to zero all players die.
+          </p>
+        </section>
+        <section id="the-objectives">
+          <h3>The Objective</h3>
+          <p>
+            Your mission is to stay alive and achieve the most victory points.
+            Each round you receive 10 time blocks that you can invest in system
+            health or accomplishments. System health will maintain the shared
+            infrastructure (upkeep) and keep the team alive. Accomplishments
+            will earn you victory points.
+          </p>
+        </section>
+      </div>
+      <section id="the-roles">
+        <h3>The Roles</h3>
+        <p>
+          Roles (Port of Mars residents, AKA characters) will be assigned
+          randomly as the game begins.
+        </p>
+        <section>
+          <h4>Researcher</h4>
+          <img
+            src="assets/images/character-Researcher.png"
+            alt="Port of Mars character, the researcher, wears a purple suit and carries a transparent tablet"
+          />
+        </section>
+        <section>
+          <h4>Politician</h4>
+          <img
+            src="assets/images/character-Politician.png"
+            alt="Port of Mars character, the Politician, wears a green suit with honorary sashes"
+          />
+        </section>
+        <section>
+          <h4>Pioneer</h4>
+          <img
+            src="assets/images/character-Pioneer.png"
+            alt="Port of Mars character, the pioneer, wears a black suit with climbing equipment"
+          />
+        </section>
+        <section>
+          <h4>Entrepreneur</h4>
+          <img
+            src="assets/images/character-Entreprenuer.png"
+            alt="Port of Mars character, the entrepreneur, wears a brown suit and looks at a transparent computer interface"
+          />
+        </section>
+        <section>
+          <h4>Curator</h4>
+          <img
+            src="assets/images/character-Curator.png"
+            alt="Port of Mars character, the curator, wears a gray suit with a walking stick"
+          />
+        </section>
+      </section>
+      <section id="the-phases">
+        <h3>The Phases</h3>
+        <p>
+          A game consists of between 8 — 12 rounds and each round consists of
+          the following phases.
+        </p>
+        <section>
+          <h4>New Round</h4>
+          <p>
+            Each new round is a summary of what occurred in the previous round.
+            It will show what accomplishment cards were purchased, player
+            investments made, and Mars events that contributed to system health.
+          </p>
+        </section>
+        <section>
+          <h4>Mars Event</h4>
+          <p>
+            The “chance” card of the game. Players encounter a number of Mars
+            events depending on system health level. They can positively or
+            negatively impact players and the overall environment.
+          </p>
+        </section>
+        <section>
+          <h4>Investment</h4>
+          <p>Invest time blocks into resources and/or system health.</p>
+        </section>
+        <section>
+          <h4>Trade</h4>
+          <p>Trade resources to purchase accomplishment cards.</p>
+        </section>
+        <section>
+          <h4>Purchase</h4>
+          <p>Purchase accomplishment cards to earn points.</p>
+        </section>
+        <section>
+          <h4>Discard</h4>
+          <p>
+            Discard unwanted accomplishment cards for new ones in the next
+            round.
+          </p>
+        </section>
+      </section>
+      <section id="the-resources">
+        <h3>The Resources</h3>
+        <p>
+          There are five different resource types in Port of Mars. These units
+          can be acquired from Mars events, time blocks, are trades. Use them to
+          purchase accomplishment cards which earns victory points.
+        </p>
+        <section>
+          <h4>Science</h4>
+          <img
+            src="assets/images/icon-science.png"
+            alt="circular blue background with a white lab beaker"
+          />
+        </section>
+        <section>
+          <h4>Finace</h4>
+          <img
+            src="assets/images/icon-finance.png"
+            alt="circular yellow background with three white coins"
+          />
+        </section>
+        <section>
+          <h4>Legacy</h4>
+          <img
+            src="assets/images/icon-legacy.png"
+            alt="circular purple background with a white moon with a flag"
+          />
+        </section>
+        <section>
+          <h4>Government</h4>
+          <img
+            src="assets/images/icon-government.png"
+            alt="circular red background with a white government building"
+          />
+        </section>
+        <section>
+          <h4>Culture</h4>
+          <img
+            src="assets/images/icon-culture.png"
+            alt="circular brown background with a white paintbrush"
+          />
+        </section>
+      </section>
+      <section id="the-glossary">
+        <h3>The Glossary</h3>
+        <section>
+          <h4>Accomplishment Card</h4>
+          <p>Purchased with resources and earns victory points</p>
+        </section>
+        <section>
+          <h4>Chat</h4>
+          <p>Players can communicate to strategize</p>
+        </section>
+        <section>
+          <h4>Inventory</h4>
+          <p>Storage of resources owned by a player</p>
+        </section>
+        <section>
+          <h4>Investment</h4>
+          <p>
+            Player’s allocation of time blocks into system health or resources
+          </p>
+        </section>
+        <section>
+          <h4>Invite</h4>
+          <p>
+            Received by top players of surviving teams to play in the next round
+            of the tournament
+          </p>
+        </section>
+        <section>
+          <h4>Mars Event</h4>
+          <p>
+            The “chance” card of the game, can positively or negatively impact
+            the game.
+          </p>
+        </section>
+        <section>
+          <h4>Mars Log</h4>
+          <p>
+            Summarizes past events such as system health and accomplishment
+            cards
+          </p>
+        </section>
+        <section>
+          <h4>Phase</h4>
+          <p>
+            Stages of a round- new round, Mars event, invest, trade, purchase,
+            and discard
+          </p>
+        </section>
+        <section>
+          <h4>Resource</h4>
+          <p>Assets- culture, finance, government, legacy, science</p>
+        </section>
+        <section>
+          <h4>Role</h4>
+          <p>
+            Characters- Curator, Entrepreneur, Pioneer, Politician, Researcher
+          </p>
+        </section>
+        <section>
+          <h4>Round</h4>
+          <p>
+            Each game consists of 8-12 rounds, each round consists of phases
+          </p>
+        </section>
+        <section>
+          <h4>Resource</h4>
+          <p>Assets- culture, finance, government, legacy, science</p>
+        </section>
+        <section>
+          <h4>System Health</h4>
+          <p>
+            The condition of your environment in Port of Mars. If it falls to 0
+            all players die.
+          </p>
+        </section>
+        <section>
+          <h4>Tournament</h4>
+          <p>
+            A contest. A series of elimination games to determine the winner.
+          </p>
+        </section>
+        <section>
+          <h4>Victory Points</h4>
+          <p>Earned by a player from accomplishments</p>
+        </section>
+      </section>
+    </section>
+  </main>
+</template>
