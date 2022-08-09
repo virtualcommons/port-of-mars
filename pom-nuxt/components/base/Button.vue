6a5185dd0ad50e576d77e2f7b69b523c708ccffb
chrstngyn <Christine.N.Nguyen@asu.edu>
2022-08-09 15:50:35 -0700

    feat: add dynamic button component

---
+++ pom-nuxt/components/base/Button.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+defineProps({
+  to: {
+    type: String,
+    default: null,
+  },
+  href: {
+    type: String,
+    default: null,
+  },
+});
+const nuxtLinkComponent = resolveComponent("NuxtLink");
+const buttonComponent = resolveComponent("button");
+</script>
+
+<template>
+  <component
+    :is="to ? nuxtLinkComponent : href ? 'a' : buttonComponent"
+    :to="to"
+    :href="href"
+    @click="$emit('click')"
+    class="rounded-md bg-sky-400 hover:bg-sky-400/50 p-2"
+  >
+    <slot></slot>
+  </component>
+</template>
