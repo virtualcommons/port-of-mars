347907b6b137f9d904b1d68ed334808104d57b2d
chrstngyn <Christine.N.Nguyen@asu.edu>
2022-08-15 15:15:33 -0700

    feat: remove tailwind configurations from nuxt.config
    remove tailwind and css configurations in favor of default tailwind
    configurations that come with tailwindcss for nuxtjs
    https://tailwindcss.nuxtjs.org/getting-started/setup#tailwind-files

--- pom-nuxt/nuxt.config.ts
+++ pom-nuxt/nuxt.config.ts
@@ -2,13 +2,11 @@ import { defineNuxtConfig } from "nuxt";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  // https://tailwindcss.nuxtjs.org/getting-started/setup/
+  alias: {
+    "@port-of-mars/shared/": "../shared/src/",
+  },
   modules: ["@nuxtjs/tailwindcss"],
-  // ssr: false,
-
-  // FIXME: re-enable when we're ready to have full typescript type checking
-  // typescript: {
-  //   shim: false,
-  //   strict: true,
-  // },
+  typescript: {
+    strict: true,
+  },
 });
