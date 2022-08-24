1dbe72b00021a2444957b04d0c7a0e5a378372af
chrstngyn <Christine.N.Nguyen@asu.edu>
2022-08-24 14:27:15 -0700

    refactor(style): use base styles for headers
    remove redundant utilities on header tags to avoid repetition

--- pom-nuxt/pages/dashboard.vue
+++ pom-nuxt/pages/dashboard.vue
@@ -13,17 +13,13 @@ const tutorialVideoUrl = ref("https://player.vimeo.com/video/642036661");
         <div
           class="flex flex-col items-center row-span-1 md:col-span-1 p-5 bg-amber-900/20"
         >
-          <h1 class="font-medium text-black leading-tight text-xl my-2">
-            Messages
-          </h1>
+          <h3>Messages</h3>
         </div>
 
         <div
           class="flex flex-col items-center row-span-1 md:col-span-1 gap-2 bg-amber-900/20"
         >
-          <h1 class="font-medium text-black leading-tight text-xl my-2">
-            Onboarding Tasks
-          </h1>
+          <h3>Onboarding Tasks</h3>
           <base-card
             :title="'Verify Email'"
             :status="'complete'"
@@ -43,9 +39,7 @@ const tutorialVideoUrl = ref("https://player.vimeo.com/video/642036661");
         <div
           class="flex flex-col items-center row-span-1 col-span-1 gap-2 bg-amber-900/30"
         >
-          <h1 class="font-medium text-black leading-tight text-xl my-2">
-            Schedule
-          </h1>
+          <h3>Schedule</h3>
           <base-card
             :title="'Game 1'"
             :subtitle="'September 01, 2022 11AM'"
@@ -64,17 +58,13 @@ const tutorialVideoUrl = ref("https://player.vimeo.com/video/642036661");
         <div
           class="flex flex-col justify-center items-center row-span-1 md:col-span-2 p-5 bg-amber-900/20"
         >
-          <h1 class="font-medium text-black leading-tight text-3xl">
-            Next Game
-          </h1>
+          <h1>Next Game</h1>
           <p class="font-semibold text-black text-5xl">23: 01 : 33 : 89</p>
         </div>
         <div
           class="flex flex-col items-center row-span-2 md:col-span-2 bg-amber-900/40"
         >
-          <h1 class="font-medium text-black leading-tight text-2xl my-2">
-            Tutorial
-          </h1>
+          <h3>Tutorial</h3>
           <iframe
             :src="tutorialVideoUrl"
             width="600"
