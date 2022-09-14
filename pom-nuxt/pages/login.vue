16291bac1eaf1236f49d7be9e108a80982906334
chrstngyn <Christine.N.Nguyen@asu.edu>
2022-09-14 00:03:42 -0700

    feat: make api call when user logs in

--- pom-nuxt/pages/login.vue
+++ pom-nuxt/pages/login.vue
@@ -1,5 +1,28 @@
 <script setup lang="ts">
-import { url } from "@port-of-mars/client/util";
+import { computed, reactive, ref, watch } from "vue";
+import { isEmailValid, isSubmitDisabled } from "~/composables/useValidation";
+import { loginWithEmail } from "~/composables/useAuth";
+const email = ref("");
+const form = reactive({ feedback: "", isValid: false });
+
+watch(
+  () => email.value,
+  (email) => {
+    if (isEmailValid(email)) {
+      form.feedback = "";
+      form.isValid = true;
+    } else {
+      form.feedback = "Invalid email";
+      form.isValid = false;
+    }
+  }
+);
+
+const postLoginForm = async (e: Event) => {
+  console.log(email);
+  e.preventDefault();
+  await loginWithEmail(email);
+};
 </script>
 
 <template>
@@ -8,12 +31,35 @@ import { url } from "@port-of-mars/client/util";
       <div
         class="flex flex-col rounded-lg justify-center items-center h-4/6 w-5/6 bg-slate-900/50"
       >
-        <h1
-          class="font-semibold font-sans text-white leading-tight text-2xl my-2"
-        >
-          Sign In to Port of Mars
-        </h1>
-        <base-button to="/login" :initial-color="'main'">Register</base-button>
+        <form @submit="postLoginForm">
+          <h1
+            class="font-semibold font-sans text-white leading-tight text-2xl my-2"
+          >
+            Sign In to Port of Mars
+          </h1>
+          <label class="label">
+            <span class="label-text">Your Email</span>
+          </label>
+          <label class="input-group">
+            <span>Email</span>
+            <input
+              required
+              v-model="email"
+              type="text"
+              placeholder="info@site.com"
+              class="input input-bordered w-full"
+            />
+          </label>
+          <span class="text-red-800" v-if="form.feedback">{{
+            form.feedback
+          }}</span>
+          <input
+            type="submit"
+            value="Register"
+            class="btn btn-wide bg-emerald-800/70 hover:bg-emerald-500/50 my-2"
+            :disabled="isSubmitDisabled(form.isValid)"
+          />
+        </form>
       </div>
     </div>
   </main>
