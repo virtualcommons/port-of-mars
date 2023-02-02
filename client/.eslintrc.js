fbd158c2722f4cc25aff693445a6ba5eb065313d
sgfost <sgfost@asu.edu>
2023-02-02 15:22:39 -0700

    chore: setup linting/formatting workflow
    - install/configure eslint and prettier
    - add github workflow to check for warnings/errors
    
    * shared is combined with server check to avoid extra
      config/dependencies
    
    [no ci]

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -3,33 +3,34 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/airbnb',
-    '@vue/typescript',
-  ],
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-useless-constructor': 'off',
-    'no-empty-function': 'off',
-    'import/prefer-default-export': 'off',
-  },
+  parser: "vue-eslint-parser",
   parserOptions: {
-    parser: '@typescript-eslint/parser',
+    parser: "@typescript-eslint/parser",
   },
+  extends: ["eslint:recommended", "plugin:vue/essential", "@vue/eslint-config-typescript"],
+  rules: {
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-useless-constructor": "off",
+    "no-empty-function": "off",
+    "import/prefer-default-export": "off",
+  },
+  ignorePatterns: [
+    "node_modules/*",
+    "public/*",
+    "dist/*",
+    "tests/*", // ignoring tests for now, should lint them if they ever exist
+  ],
   overrides: [
     {
-      files: [
-        '**/__tests__/*.{j,t}s?(x)',
-      ],
+      files: ["**/__tests__/*.{j,t}s?(x)"],
       env: {
         jest: true,
       },
     },
   ],
   settings: {
-    'import/resolver': {
+    "import/resolver": {
       typescript: {
         alwaysTryTypes: true,
       },
--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -1,20 +1,22 @@
 module.exports = {
   root: true,
-  parser: '@typescript-eslint/parser',
-  plugins: [
-    '@typescript-eslint',
-  ],
+  env: {
+    node: true,
+  },
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended'
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
   ],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    '@typescript-eslint/no-empty-function': 'off',
-    'no-useless-constructor': 'off',
-    'import/prefer-default-export': 'off',
-    '@typescript-eslint/camelcase': 'off',
-  }
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "@typescript-eslint/no-empty-function": "off",
+    "no-useless-constructor": "off",
+    "import/prefer-default-export": "off",
+    "@typescript-eslint/camelcase": "off",
+  },
+  ignorePatterns: ["node_modules/*", "lib/*", "dist/*", "fixtures/*", "src/migrations/*"],
 };
