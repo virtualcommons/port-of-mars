636b999dbcd070e425abba030eedbd2bf09c51a0
Allen Lee <alee@users.noreply.github.com>
2023-04-20 13:20:51 -0700

    refactor: rename copypasta variables
    TODO: should consider renaming leaderboard route to stats with
    /leaderboard /player endpoints instead

--- server/src/routes/leaderboard.ts
+++ server/src/routes/leaderboard.ts
@@ -8,8 +8,8 @@ leaderboardRouter.get("/", async (req, res, next) => {
   const limitStr = req.query.limit as string;
   const limit = parseInt(limitStr) || 50;
   try {
-    const roomId = await getServices().leaderboard.getLeaderboardData(limit);
-    res.json(roomId);
+    const leaderboardData = await getServices().leaderboard.getLeaderboardData(limit);
+    res.json(leaderboardData);
   } catch (e) {
     next(e);
   }
@@ -18,8 +18,8 @@ leaderboardRouter.get("/", async (req, res, next) => {
 leaderboardRouter.get("/stats", async (req, res, next) => {
   try {
     const user = req.user as User;
-    const roomId = await getServices().leaderboard.getStats(user);
-    res.json(roomId);
+    const playerStats = await getServices().leaderboard.getStats(user);
+    res.json(playerStats);
   } catch (e) {
     next(e);
   }
