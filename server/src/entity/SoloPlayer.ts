7b5283a5c53c1ce99bd46020a1ec873da20aa3af
tkawamura02 <tkawamura02@gmail.com>
2023-05-26 11:51:24 -0700

    feat: add solo player entity
    Co-authored-by: saachibm <saachibm@users.noreply.github.com>
    Co-authored-by: sabrinanel3 <sabrinanel3@users.noreply.github.com>
    Co-authored-by: sgfost <sgfost@users.noreply.github.com>

---
+++ server/src/entity/SoloPlayer.ts
@@ -0,0 +1,32 @@
+import { Column, Entity, ManyToOne, OneToOne, PrimaryGeneratedColumn, CreateDateColumn, JoinColumn } from "typeorm";
+import { Role, ROLES } from "@port-of-mars/shared/types";
+import { User } from "./User";
+import { SoloGame } from "@port-of-mars/server/entity/SoloGame";
+
+@Entity()
+export class SoloPlayer {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @ManyToOne(type => User, user => user.players, { nullable: false })
+  user!: User;
+
+  @Column()
+  userId!: number;
+
+  @Column({ default: "" })
+  playerIp!: string;
+
+  @OneToOne(type => SoloGame, game => game.player, { nullable: false })
+  @JoinColumn()
+  game!: SoloGame;
+
+  @Column()
+  gameId!: number;
+
+  @Column("int", { nullable: true })
+  points!: number | null;
+
+  @CreateDateColumn()
+  dateCreated!: Date;
+}
