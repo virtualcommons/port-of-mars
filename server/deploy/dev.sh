e6c232d7f642e30864cf736eaf9388e30bfc3f0c
sgfost <46429375+sgfost@users.noreply.github.com>
2023-06-02 12:29:27 -0700

    build: replace wait-for-it.sh with pg_isready
    startup scripts are no longer needed and pg_isready is just given as a
    healthcheck command in the compose file
    
    resolves #864

--- server/deploy/dev.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -o errexit
-
-yarn install
-./deploy/wait-for-it.sh db:5432 -- echo "db ready"
-exec yarn start
--- server/deploy/prod.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-./wait-for-it.sh db:5432 -- echo "db ready"
-exec yarn start:prod
