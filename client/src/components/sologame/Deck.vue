6086c4643c2e3a32d2f04d57c06d96616dec8cb4
tkawamura02 <tkawamura02@gmail.com>
2023-07-14 10:14:28 -0700

    refactor deck

---
+++ client/src/components/sologame/Deck.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h4> {{ deck }} </h4>
+  </div>
+</template>
+  
+<script lang="ts">
+import { Vue, Component, Prop } from "vue-property-decorator";
+import { EventCardData } from "@port-of-mars/shared/sologame";
+  
+@Component({})
+export default class Deck extends Vue {
+  @Prop() deck!: EventCardData;
+}
+</script>
+  
+<style lang="scss"></style>
