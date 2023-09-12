da48d5bb56e0bc2e359aecd55e4bee83f7a07641
tkawamura02 <tkawamura02@gmail.com>
2023-09-11 18:29:32 -0700

    feat: add deck component

--- client/src/components/sologame/Deck.vue
+++ client/src/components/sologame/Deck.vue
@@ -1,17 +1,19 @@
 <template>
   <div>
-    <h4> {{ deck }} </h4>
+    <h4>{{ event.displayName }}</h4>
+    <p><i>{{ event.flavorText }}</i></p>
+    <p>{{ event.effectText }}</p>
   </div>
 </template>
-  
+
 <script lang="ts">
 import { Vue, Component, Prop } from "vue-property-decorator";
 import { EventCardData } from "@port-of-mars/shared/sologame";
-  
+
 @Component({})
-export default class Deck extends Vue {
-  @Prop() deck!: EventCardData;
+export default class EventCard extends Vue {
+  @Prop() event!: EventCardData;
 }
 </script>
-  
+
 <style lang="scss"></style>
--- client/src/views/SoloGame.vue
+++ client/src/views/SoloGame.vue
@@ -30,9 +30,20 @@ investment input - https://bootstrap-vue.org/docs/components/form-spinbutton
     <b-row class="w-100 flex-shrink-1 p-2" no-gutters>
       <b-col cols="12" class="content-container"
         >SYSTEM HEALTH: {{ state.systemHealth }}
-
-        <SegmentedBar :min="0" :max="25" v-model="state.systemHealth" />
-        {{ tempVal }}
+        <b-col class="statusbar-outer" no-gutters>
+          <b-col class="statusbar-inner" no-gutters></b-col>
+          <b-img
+            v-bind="systemHealthIconProps"
+            :src="require(`@port-of-mars/client/assets/icons/systemHealth.svg`)"
+            rounded="circle"
+            left
+            class="mr-1 mt-2"
+            alt="System Health"
+            contain
+            height="50px"
+            width="75px"
+          />
+        </b-col>
       </b-col>
     </b-row>
     <b-row class="w-100 flex-grow-1" no-gutters>
@@ -54,34 +65,40 @@ investment input - https://bootstrap-vue.org/docs/components/form-spinbutton
         </b-row>
         <b-row class="w-100 p-2" no-gutters>
           <b-col cols="4" class="content-container">
-            <div>
+            <div>  
               <div v-for="event in state.roundEventCards" :key="event.codeName">
                 <EventCard :event="event" />
               </div>
             </div>
           </b-col>
           <b-col cols="8" class="content-container">
-            DECK: 
             <span v-if="state.treatmentParams.isEventDeckKnown">
-              TRUE
-              <div v-for="deck in state.eventCardDeck" :key="deck.codeName">
-                <Deck :event="deck" />
-              </div>
+                <div v-for="event in state.eventCardDeck" :key="event.codeName">
+                  <Deck :event="event" />
+                </div>
             </span>
-            <span v-else>FALSE</span>
+            <span v-else>
+              FALSE
+            </span>
           </b-col>
           <b-row class="w-25 flex-shrink-1 p-2" no-gutters>
             <b-col cols="12" class="content-container">
-              <SegmentedBar
-                :min="0"
-                :max="state.player.resources"
+              <b-form-spinbutton
+                id="invest-input"
                 v-model="pendingSystemHealthInvestment"
-                :asInput="true"
-              />
-
-              <b-button @click="handleInvestButtonClick" :disabled="!state.canInvest"
-                >Invest in System Health</b-button
+                min="0"
+                :max="state.player.resources"
               >
+                <template #decrement>
+                  <b-icon-dash scale="1.00" color="#f1e0c5"></b-icon-dash>
+                </template>
+                <template #increment>
+                  <b-icon-plus scale="1.00" color="#f1e0c5"></b-icon-plus>
+                </template>
+              </b-form-spinbutton>
+              <p>{{ pendingSystemHealthInvestment }}</p>
+              <b-button @click="handleInvestButtonClick" :disabled="!state.canInvest"
+                >Invest in System Health</b-button>
             </b-col>
           </b-row>
         </b-row>
@@ -97,9 +114,7 @@ import { SoloGameRequestAPI } from "@port-of-mars/client/api/sologame/request";
 import { applySoloGameServerResponses } from "@port-of-mars/client/api/sologame/response";
 import { EventCardData, SOLO_ROOM_NAME } from "@port-of-mars/shared/sologame";
 import EventCard from "@port-of-mars/client/components/sologame/EventCard.vue";
-import HealthBar from "@port-of-mars/client/components/sologame/HealthBar.vue";
-import SystemHealth from "@port-of-mars/client/components/game/static/systemhealth/SystemHealth.vue";
-import SegmentedBar from "@port-of-mars/client/components/global/SegmentedBar.vue";
+import Deck from "@port-of-mars/client/components/sologame/Deck.vue";
 
 export interface SoloGameState {
   timeRemaining: number;
@@ -127,9 +142,7 @@ export interface SoloGameState {
   name: "sologame",
   components: {
     EventCard,
-    HealthBar,
-    SystemHealth,
-    SegmentedBar,
+    Deck,
   },
 })
 export default class Game extends Vue {
@@ -138,8 +151,6 @@ export default class Game extends Vue {
   hasApi: boolean = false;
 
   pendingSystemHealthInvestment = 0;
-
-  tempVal = 0;
 
   // FIXME: move this to a vuex store after splitting up the multiplayer game and
   // onboarding/etc. stores
