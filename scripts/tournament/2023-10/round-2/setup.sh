14e829598f95719db840387458086b41ef499d4d
Allen Lee <alee@users.noreply.github.com>
2023-11-27 17:30:27 -0700

    feat: add round 2

---
+++ scripts/tournament/2023-10/round-2/setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+CLI_CMD="${1:-cli}" # in prod invoke with `cli:prod` e.g., `$ ./setup.sh cli:prod`
+
+## create Fall 2023 Mars Madness tournament Round 2 structure
+
+yarn ${CLI_CMD} tournament round create --introSurveyUrl=https://asu.co1.qualtrics.com/jfe/form/SV_9N65NiZ7uJjxEIl --exitSurveyUrl=https://asu.co1.qualtrics.com/jfe/form/SV_6FNhPbsBuybTjEN --announcement="ROUND 2 IS NOW OPEN. Eligible players will be explicitly invited to participate via email, so keep an eye on those inboxes! If you've been sent an invitation, please sign in and complete all pre-mission onboarding tasks before joining the lobby during the scheduled launch time to compete."
+# single launch time, Wednesday November 29th, 2023 at 8PM MST
+yarn ${CLI_CMD} tournament round date --date="2023-11-29T20:00:00-07:00";
