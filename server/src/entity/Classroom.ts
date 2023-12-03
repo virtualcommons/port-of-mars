b837745e2925f566f037ecb0aac6bd99639b8c8e
saachibm <123594505+saachibm@users.noreply.github.com>
2023-12-03 11:48:07 -0700

    feat: add supporting entity definitions
    - a Teacher can have multiple Classrooms
    - a Classroom can have multiple Students
    - Students are one-to-one with Users which lets them participate in Games
    - Teachers are one-to-one with Users which lets them sign into the
      website (may need to revisit this part of the schema)
    
    Co-authored-by: Tkawamura02 <tkawamura02@gmail.com>
    Co-authored-by: Allen Lee <alee@users.noreply.github.com>
    Co-authored-by: Scott Foster <sgfost@users.noreply.github.com>
    Co-authored-by: Sabrina Nelson <sabrinanel3@users.noreply.github.com>

---
+++ server/src/entity/Classroom.ts
@@ -0,0 +1,21 @@
+import { Column, Entity, ManyToOne, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { Student } from "./Student";
+import { Teacher } from "./Teacher";
+
+@Entity()
+export class Classroom {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @OneToMany(type => Student, student => student.classroom)
+  students!: Student;
+
+  @ManyToOne(type => Teacher, teacher => teacher.classrooms)
+  teacher!: Teacher;
+
+  @Column()
+  teacherId!: number;
+
+  @Column()
+  authToken!: string;
+}
