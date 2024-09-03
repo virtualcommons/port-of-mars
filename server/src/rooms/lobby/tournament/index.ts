8fdcbdb99b00ae46e1bd4712742cbd22f1464bdd
sgfost <sgfost@asu.edu>
2024-09-03 16:46:13 -0700

    fix: set client accepted game invitation to avoid delay
    group.clients and state.clients are separate objects in the lobby, so
    setting accepted on state.clients did not satisfy the condition of
    group.allClientsAccepted so the timeout would always expire

--- server/src/rooms/lobby/tournament/index.ts
+++ server/src/rooms/lobby/tournament/index.ts
@@ -108,13 +108,13 @@ export class TournamentLobbyRoom extends LobbyRoom<TournamentLobbyRoomState> {
 
   onAcceptInvitation(client: Client, message: AcceptInvitation) {
     logger.trace(`client ${client.auth.username} accepted invitation to join a game`);
-    this.state.setClientAccepted(client.auth.username);
     const group = this.groupManager.getClientPendingGroup(client);
     // check that all clients in the group have accepted the invitation
     if (!group) {
       logger.warn(`no client group found for client ${client.auth.username}`);
       return;
     }
+    group.setClientAccepted(client.auth.username);
     if (group.allClientsAccepted()) {
       group.clearForceMoveTimeout();
       this.removeGroupFromLobby(group);
